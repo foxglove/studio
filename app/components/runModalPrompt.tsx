012b7bf343570ad7e3551727ee63eef608b84aff
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-02-23 13:54:15 -0600

    Enable eslint-plugin-react-hooks and fix/suppress errors

---
+++ app/components/runModalPrompt.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import Modal from "@foxglove-studio/app/components/Modal";
+import Button from "@foxglove-studio/app/components/Button";
+import renderToBody from "@foxglove-studio/app/components/renderToBody";
+
+const ModalContent = styled.div`
+  overflow-y: auto;
+  padding: 25px;
+  padding-top: 48px;
+  width: 300px;
+`;
+
+const ModalActions = styled.div`
+  padding-top: 10px;
+  text-align: right;
+`;
+
+function ModalPrompt({
+  initialValue,
+  onComplete,
+}: {
+  initialValue: string;
+  onComplete: (value: string | undefined) => void;
+}) {
+  const [value, setValue] = useState(initialValue);
+
+  return (
+    <Modal onRequestClose={() => onComplete(undefined)}>
+      <ModalContent>
+        <div>
+          <input
+            style={{ width: "100%" }}
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+        <ModalActions>
+          <Button onClick={() => onComplete(undefined)}>Cancel</Button>
+          <Button primary={true} onClick={() => onComplete(value)}>
+            OK
+          </Button>
+        </ModalActions>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export function runModalPrompt(initialValue: string): Promise<string | undefined> {
+  return new Promise((resolve) => {
+    const modal = renderToBody(
+      <ModalPrompt
+        initialValue={initialValue}
+        onComplete={(value) => {
+          modal.remove();
+          resolve(value);
+        }}
+      />,
+    );
+  });
+}
