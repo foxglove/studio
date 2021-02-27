06070b1ae0eddc45e1d3eb8ad0a1adfb5b2918f9
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-02-26 18:42:58 -0600

    Add DMG background image

---
+++ electron-forge.config.js
@@ -0,0 +1,39 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+module.exports = {
+  packagerConfig: {
+    dir: ".webpack",
+    asar: true,
+    icon: "resources/icon/icon.icns",
+    name: "Foxglove Studio",
+  },
+  makers: [
+    {
+      name: "@electron-forge/maker-deb",
+      platforms: ["linux"],
+      config: {
+        scripts: {
+          postinst: "resources/linux/deb/postinst",
+        },
+      },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {
+        background: "resources/dmg-background/background.png",
+        contents: (opts) => [
+          { x: 144, y: 170, type: "file", path: opts.appPath },
+          { x: 390, y: 170, type: "link", path: "/Applications" },
+        ],
+        additionalDMGOptions: {
+          window: {
+            size: { width: 540, height: 380 },
+          },
+        },
+      },
+    },
+  ],
+};
