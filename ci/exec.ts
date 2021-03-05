f826901d39d25f954cc79ad08860ba8a76a163e8
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-05 17:00:35 -0600

    fixes

--- ci/exec.ts
+++ ci/exec.ts
@@ -1,11 +1,10 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import execa, from "execa";
-import "colors";
+import execa from "execa";
 
 export default function exec(program: string, args: string[]): Promise<unknown> {
-  console.log(`::group::\$${program} ${args}`); // eslint-disable-line no-restricted-syntax
+  console.log(`::group::$${program} ${args}`); // eslint-disable-line no-restricted-syntax
   return execa(program, args, { stdio: "inherit" }).finally(() => {
     console.log("::endgroup::"); // eslint-disable-line no-restricted-syntax
   });
