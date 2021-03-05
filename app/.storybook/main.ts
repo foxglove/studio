50533b0360dd2fac928b24de22822ba2f214c04c
Adrian Macneil <adrian@foxglove.dev>
2021-03-04 19:37:21 -0800

    testing webpack config

--- app/.storybook/main.ts
+++ app/.storybook/main.ts
@@ -15,12 +15,65 @@ module.exports = {
   // Storybook's config is required, for instance the HtmlWebpackPlugin that they
   // use to generate the main iframe page.
   webpackFinal: (config: Configuration): Configuration => {
-    const rendererConfig = makeConfig(null, { mode: "development" });
-    return {
-      ...config,
-      resolve: rendererConfig.resolve,
-      module: rendererConfig.module,
-      plugins: [...(config.plugins ?? []), ...(rendererConfig.plugins ?? [])],
+    console.log(JSON.stringify(config, null, "  "));
+
+    config.resolve ||= {};
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      zlib: require.resolve("browserify-zlib"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      fs: false,
+      "@blueprintjs/core": false,
+      "@blueprintjs/icons": false,
     };
+
+    config.module ||= {};
+    config.module.rules = [
+      // ...(config.module?.rules ?? []),
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            onlyCompileBundledFiles: true,
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        loader: "style-loader",
+        sideEffects: true,
+      },
+      {
+        test: /\.s?css$/,
+        oneOf: [
+          {
+            test: /\.module\./,
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[path][name]-[sha512:hash:base32:5]--[local]",
+              },
+              sourceMap: true,
+            },
+          },
+          { loader: "css-loader", options: { sourceMap: true } },
+        ],
+      },
+      { test: /\.scss$/, loader: "sass-loader", options: { sourceMap: true } },
+    ];
+
+    return config;
+
+    // const rendererConfig = makeConfig(null, { mode: "development" });
+    // return {
+    //   ...config,
+    //   resolve: rendererConfig.resolve,
+    //   module: rendererConfig.module,
+    //   plugins: [...(config.plugins ?? []), ...(rendererConfig.plugins ?? [])],
+    // };
   },
 };
