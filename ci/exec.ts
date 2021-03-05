72d32aa93cab8805dd80e1c11847ea2e5d1134fd
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-05 17:11:53 -0600

    Use @actions instead of execa and manual grouping

--- ci/exec.ts
+++ ci/exec.ts
@@ -1,11 +1,11 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import execa from "execa";
+import { group } from "@actions/core";
+import { exec } from "@actions/exec";
 
-export default function exec(program: string, args: string[]): Promise<unknown> {
-  console.log(`::group::$${program} ${args}`); // eslint-disable-line no-restricted-syntax
-  return execa(program, args, { stdio: "inherit" }).finally(() => {
-    console.log("::endgroup::"); // eslint-disable-line no-restricted-syntax
+export default async function (program: string, args: string[]): Promise<void> {
+  await group(`$ ${program} ${args.join(" ")}`, async () => {
+    await exec(program, args);
   });
 }
