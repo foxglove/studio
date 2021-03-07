afec7ae8b68c883c09c04cd06200e620b7712ecf
John Hurliman <jhurliman@jhurliman.org>
2021-03-06 20:26:12 -0800

    Add integration test, turn on Windows build in CI

---
+++ test/startup.test.ts
@@ -0,0 +1,50 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import path from "path";
+import { Application } from "spectron";
+
+jest.setTimeout(10000);
+
+const electronPath = path.join(__dirname, "..", "node_modules", ".bin", "electron");
+const appPath = path.join(__dirname, "..", ".webpack");
+
+const app = new Application({
+  path: electronPath,
+  args: [appPath],
+});
+
+beforeAll(async () => {
+  await app.start();
+});
+
+afterAll(async () => {
+  if (!app.isRunning()) {
+    return;
+  }
+
+  await app.stop();
+  expect(app.isRunning()).toBe(false);
+});
+
+async function waitForAppMounted(appInstance: Application) {
+  for (;;) {
+    const logs = await appInstance.client.getRenderProcessLogs();
+    for (const log of logs as { level: "INFO" | "SEVERE" | "WARNING"; message?: string }[]) {
+      if (log.level === "SEVERE") {
+        throw new Error(log.message);
+      }
+      const message = log.message;
+      if (typeof message === "string" && message.includes("App rendered")) {
+        return;
+      }
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+}
+
+it("should start with no errors", async () => {
+  await waitForAppMounted(app);
+});
