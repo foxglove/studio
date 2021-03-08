24c07d74a508206701726ca22d00f67e2e99beb7
Roman Shtylman <roman@foxglove.dev>
2021-03-08 09:06:25 -0800

    Update resources/notarize.cjs
    Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

--- resources/notarize.cjs
+++ resources/notarize.cjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d01665b8fca05e70abed4ad3639d669fe224616548926d853a1179dd23091def
-size 978
+oid sha256:48c6add77f266cd1652ea063c6018202524604f87b1fd1d7bc29c6701eb010cf
+size 1173
