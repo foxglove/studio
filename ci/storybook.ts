e2f46bb35388f10cc2f899a6a3639dcc3647bf71
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-09 20:07:40 -0600

    Enable local storycap runs

--- ci/storybook.ts
+++ ci/storybook.ts
@@ -8,52 +8,73 @@ import exec from "./exec";
 //  2. capture screenshots (via storycap)
 //  3. upload & compare screenshots (via reg-suit)
 
-await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "build-storybook"]);
-await exec("yarn", [
-  "workspace",
-  "@foxglove-studio/app",
-  "run",
-  "storycap",
-  "http://localhost:9001",
+const local = process.argv.includes("--local");
+const headless = !process.argv.includes("--headful");
 
-  // Use http-server instead of start-storybook since any build errors would be raised during
-  // the build above, rather than waiting several minutes for it to build inside the storycap command
-  "--serverCmd",
-  "yarn http-server storybook-static -p 9001",
-
-  "--outDir",
-  "storybook-screenshots",
-
-  "--puppeteerLaunchConfig",
-  JSON.stringify({
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--user-agent=PuppeteerTestingChrome/88.",
-    ],
-  }),
-]);
-
-const publishArgs: string[] = [];
-
-// If this is a PR, then by default github actions has us on a merge commit.
-// Checkout the feature branch and commit to make reg-suit happy.
-const prBranch = process.env.GITHUB_HEAD_REF ?? "";
-if (prBranch.length > 0) {
-  await exec("git", ["fetch", "origin", prBranch]);
-  await exec("git", ["checkout", "-B", prBranch, `refs/remotes/origin/${prBranch}`]);
-  // Only set GitHub status checks on PR branches, not main branch
-  publishArgs.push("-n");
+if (process.argv.includes("build")) {
+  await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "build-storybook"]);
 }
 
-await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "reg-suit", "sync-expected"]);
-await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "reg-suit", "compare"]);
-await exec("yarn", [
-  "workspace",
-  "@foxglove-studio/app",
-  "run",
-  "reg-suit",
-  "publish",
-  ...publishArgs,
-]);
+if (process.argv.includes("capture")) {
+  await exec("yarn", [
+    "workspace",
+    "@foxglove-studio/app",
+    "run",
+    "storycap",
+    "http://localhost:9001",
+
+    "--verbose",
+
+    "--include",
+    "**/*bayer*",
+
+    // Use http-server instead of start-storybook since any build errors would be raised during
+    // the build above, rather than waiting several minutes for it to build inside the storycap command
+    "--serverCmd",
+    "yarn http-server storybook-static -p 9001",
+
+    ...(local ? ["--parallel", "1"] : []),
+
+    "--outDir",
+    "storybook-screenshots",
+
+    "--puppeteerLaunchConfig",
+    JSON.stringify({
+      headless,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--user-agent=PuppeteerTestingChrome/88.",
+      ],
+    }),
+  ]);
+}
+
+if (process.argv.includes("publish")) {
+  const publishArgs: string[] = [];
+
+  // If this is a PR, then by default github actions has us on a merge commit.
+  // Checkout the feature branch and commit to make reg-suit happy.
+  const prBranch = process.env.GITHUB_HEAD_REF ?? "";
+  if (prBranch.length > 0) {
+    await exec("git", ["fetch", "origin", prBranch]);
+    await exec("git", ["checkout", "-B", prBranch, `refs/remotes/origin/${prBranch}`]);
+    // Only set GitHub status checks on PR branches, not main branch
+    publishArgs.push("-n");
+  }
+
+  await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "reg-suit", "sync-expected"]);
+  await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "reg-suit", "compare"]);
+  await exec("yarn", [
+    "workspace",
+    "@foxglove-studio/app",
+    "run",
+    "reg-suit",
+    "publish",
+    ...publishArgs,
+  ]);
+}
+
+// Local runs don't always exit gracefully :(
+process.exit(0);
