0204eb2e7d5a1562cf48d5b5ece6f77f4c24381f
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-10 11:03:22 -0600

    Real arg parsing

--- ci/storybook.ts
+++ ci/storybook.ts
@@ -1,22 +1,60 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
+import {
+  ArgumentParser,
+  ONE_OR_MORE,
+  RawDescriptionHelpFormatter,
+  ArgumentTypeError,
+} from "argparse";
+
 import exec from "./exec";
 
-// Runs storybook in CI:
-//  1. build storybook
-//  2. capture screenshots (via storycap)
-//  3. upload & compare screenshots (via reg-suit)
+enum Command {
+  build = "build",
+  capture = "capture",
+  publish = "publish",
+}
 
-const local = process.argv.includes("--local");
-const headless = !process.argv.includes("--headful");
-const verbose = process.argv.includes("verbose");
+const parser = new ArgumentParser({
+  formatter_class: RawDescriptionHelpFormatter,
+  description: `
+Run storybook, capture screenshots, and upload with reg-suit.
 
-if (process.argv.includes("build")) {
+Use commands:
+  build - Run build-storybook
+  capture - Run storycap
+  publish - Run reg-suit and publish results to GitHub
+`.trim(),
+});
+parser.add_argument("--local", {
+  action: "store_true",
+  help: "Disable parallel execution for local debugging",
+});
+parser.add_argument("--headful", {
+  action: "store_true",
+  help: "Disable headless mode for local debugging",
+});
+parser.add_argument("--verbose", { action: "store_true", help: "Enable storycap verbose output" });
+parser.add_argument("command", {
+  nargs: ONE_OR_MORE,
+  help: "Sub-commands to run",
+  type: (arg: string): Command => {
+    const command: Command | undefined = Command[arg as Command];
+    if (command !== undefined) {
+      return command;
+    }
+    throw new ArgumentTypeError(`Unrecognized command ${arg}`);
+  },
+});
+
+const args = parser.parse_args();
+
+if (args.command.includes(Command.build)) {
   await exec("yarn", ["workspace", "@foxglove-studio/app", "run", "build-storybook"]);
 }
 
-if (process.argv.includes("capture")) {
+if (args.command.includes(Command.capture)) {
   await exec("yarn", [
     "workspace",
     "@foxglove-studio/app",
@@ -24,21 +62,21 @@ if (process.argv.includes("capture")) {
     "storycap",
     "http://localhost:9001",
 
-    ...(verbose ? ["--verbose"] : []),
+    ...(args.verbose ? ["--verbose"] : []),
 
     // Use http-server instead of start-storybook since any build errors would be raised during
     // the build above, rather than waiting several minutes for it to build inside the storycap command
     "--serverCmd",
     "yarn http-server storybook-static -p 9001",
 
-    ...(local ? ["--parallel", "1"] : []),
+    ...(args.local ? ["--parallel", "1"] : []),
 
     "--outDir",
     "storybook-screenshots",
 
     "--puppeteerLaunchConfig",
     JSON.stringify({
-      headless,
+      headless: args.headless,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
@@ -49,7 +87,7 @@ if (process.argv.includes("capture")) {
   ]);
 }
 
-if (process.argv.includes("publish")) {
+if (args.command.includes(Command.publish)) {
   const publishArgs: string[] = [];
 
   // If this is a PR, then by default github actions has us on a merge commit.
