ad4d01fbbf82c3c881426a15e1af639613e9967d
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-15 11:41:26 -0500

    Convert "==/!= null" to "==/!= undefined"

--- app/components/MessagePathSyntax/useCachedGetMessagePathDataItems.ts
+++ app/components/MessagePathSyntax/useCachedGetMessagePathDataItems.ts
@@ -129,7 +129,7 @@ function filterMatches(filter: MessagePathFilter, value: any) {
   let currentValue = value;
   for (const name of filter.path) {
     currentValue = getField(currentValue, name);
-    if (currentValue == null) {
+    if (currentValue == undefined) {
       return false;
     }
   }
@@ -283,7 +283,7 @@ export function getMessagePathDataItems(
       for (let i = startIdx; i <= Math.min(endIdx, arrayLength - 1); i++) {
         const index = i >= 0 ? i : arrayLength + i;
         const arrayElement = getIndex(value, index);
-        if (arrayElement == null) {
+        if (arrayElement == undefined) {
           continue;
         }
         // Ideally show something like `/topic.object[:]{some_id=123}` for the path, but fall
