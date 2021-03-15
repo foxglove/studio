9d01cce50cb2ed8bfe413a4a7ac7ecc74e1d0e79
John Hurliman <jhurliman@jhurliman.org>
2021-03-15 09:40:03 -0700

    Move .d.ts file to typings/ folder

--- packages/ros1-nodejs/src/XmlRpcNode.ts
+++ packages/ros1-nodejs/src/XmlRpcNode.ts
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="xmlrpc-rosnodejs.d.ts" />
+/// <reference path="../typings/xmlrpc-rosnodejs.d.ts" />
 
 import type {
   XmlRpcClient,
