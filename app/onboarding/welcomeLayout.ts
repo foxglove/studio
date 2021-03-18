7789e2676bc5e890c3d65901414d17151f91aabd
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-03-18 10:17:42 -1000

    Improve welcome layout

--- app/onboarding/welcomeLayout.ts
+++ app/onboarding/welcomeLayout.ts
@@ -6,38 +6,117 @@ import { PanelsState } from "@foxglove-studio/app/reducers/panels";
 const state: PanelsState = {
   layout: {
     first: {
-      first: "StateTransitions!3485dnq",
-      second: "DiagnosticStatusPanel!1m51jv5",
+      first: {
+        first: "Plot!2srxf7j",
+        second: {
+          first: "StateTransitions!3v5kkkn",
+          second: "3D Panel!3vhcz8n",
+          direction: "column",
+          splitPercentage: 29.709659628112195,
+        },
+        direction: "column",
+        splitPercentage: 22.05128205128205,
+      },
+      second: {
+        first: "ImageViewPanel!4evmpi6",
+        second: {
+          first: "DiagnosticSummary!2ttktu0",
+          second: "DiagnosticStatusPanel!2yh7rts",
+          direction: "column",
+          splitPercentage: 35.6573705179283,
+        },
+        direction: "column",
+        splitPercentage: 25.748502994011975,
+      },
       direction: "row",
+      splitPercentage: 52.86975717439294,
     },
-    second: "3D Panel!3vhcz8n",
+    second: "onboarding.welcome!31b1ehz",
     direction: "row",
+    splitPercentage: 66.03498542274052,
   },
   savedProps: {
-    "StateTransitions!3485dnq": {
+    "Plot!2srxf7j": {
       paths: [
         {
-          value: "/radar/points.height",
+          value: "/radar/tracks.tracks[:]{number==$track_id}.accel",
+          enabled: true,
+          timestampMethod: "receiveTime",
+        },
+        {
+          value: "/radar/tracks.tracks[:]{number==$track_id}.rate",
+          enabled: true,
+          timestampMethod: "receiveTime",
+        },
+      ],
+      minYValue: "",
+      maxYValue: "",
+      showLegend: false,
+      xAxisVal: "timestamp",
+    },
+    "StateTransitions!3v5kkkn": {
+      paths: [
+        {
+          value: "/radar/tracks.tracks[:]{number==$track_id}.moving",
+          timestampMethod: "receiveTime",
+        },
+        {
+          value: "/radar/tracks.tracks[:]{number==$track_id}.status",
           timestampMethod: "receiveTime",
         },
       ],
     },
     "3D Panel!3vhcz8n": {
-      checkedKeys: ["name:Topics", "t:/radar/points"],
-      expandedKeys: ["name:Topics"],
-      followTf: "radar",
+      checkedKeys: ["name:Topics", "t:/velodyne_points", "t:/radar/points"],
+      expandedKeys: ["name:Topics", "t:/tf"],
+      followTf: "velodyne",
       cameraState: {
         targetOffset: [0, 0, 0],
+        thetaOffset: 1.6227449919298258,
+        distance: 51.57407475453429,
+        perspective: true,
+        phi: 1.044534412955465,
+        fovy: 0.7853981633974483,
+        near: 0.01,
+        far: 5000,
       },
       modifiedNamespaceTopics: [],
       pinTopics: false,
-      settingsByKey: {},
+      settingsByKey: {
+        "t:/velodyne_points": {
+          colorMode: {
+            mode: "rainbow",
+            colorField: "intensity",
+          },
+          pointShape: "circle",
+          pointSize: 2,
+        },
+      },
       autoSyncCameraState: false,
       autoTextBackgroundColor: true,
-      followOrientation: false,
+    },
+    "ImageViewPanel!4evmpi6": {
+      cameraTopic: "/image_raw",
+      enabledMarkerTopics: [],
+      customMarkerTopicOptions: [],
+      scale: 0.2,
+      transformMarkers: true,
+      synchronize: false,
+      mode: "other",
+      zoomPercentage: 27.500000000000004,
+      offset: [0, 0],
+    },
+    "DiagnosticStatusPanel!2yh7rts": {
+      topicToRender: "/diagnostics",
+      collapsedSections: [],
+      selectedHardwareId: "Velodyne HDL-32E",
+      splitFraction: 0.7048054919908466,
+      selectedName: "velodyne_nodelet_manager: velodyne_packets topic status",
     },
   },
-  globalVariables: {},
+  globalVariables: {
+    track_id: 25,
+  },
   userNodes: {},
   linkedGlobalVariables: [],
   playbackConfig: {
