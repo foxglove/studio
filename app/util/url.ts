a894288f9ef9d60e84d9071ba22daeda7b69d81e
John Hurliman <jhurliman@jhurliman.org>
2021-03-22 14:18:53 -0700

    Fix missing import

--- app/util/url.ts
+++ app/util/url.ts
@@ -2,6 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { URL } from "whatwg-url";
+
 export function parseInputUrl(str?: string): URL | undefined {
   if (str == undefined || str.length === 0) {
     return undefined;
