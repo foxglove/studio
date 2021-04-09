5592828f657219e5bf294580690530b1c87183a6
John Hurliman <jhurliman@jhurliman.org>
2021-04-09 08:56:21 -0700

    Convert RosGraph to <CytoscapeComponent>

--- app/panels/TopicGraph/index.tsx
+++ app/panels/TopicGraph/index.tsx
@@ -11,64 +11,81 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
+import Cytoscape from "cytoscape";
+import CytoscapeCola from "cytoscape-cola";
 import { useCallback } from "react";
 import CytoscapeComponent from "react-cytoscapejs";
-import styled from "styled-components";
 
 import EmptyState from "@foxglove-studio/app/components/EmptyState";
 import { useMessagePipeline } from "@foxglove-studio/app/components/MessagePipeline";
 import Panel from "@foxglove-studio/app/components/Panel";
 import PanelToolbar from "@foxglove-studio/app/components/PanelToolbar";
-import SelectableTimestamp from "@foxglove-studio/app/components/SelectableTimestamp";
-import clipboard from "@foxglove-studio/app/util/clipboard";
-import { formatDuration } from "@foxglove-studio/app/util/formatTime";
-import { subtractTimes, toSec } from "@foxglove-studio/app/util/time";
 
-const STableContainer = styled.div`
-  overflow-y: auto;
-  overflow-x: hidden;
-`;
+const STYLESHEET: Cytoscape.Stylesheet[] = [
+  {
+    selector: 'node[type="node"]',
+    style: {
+      content: "data(label)",
+      shape: "rectangle",
+      "background-color": "blue",
+      "font-size": "16px",
+      "text-valign": "center",
+      "text-halign": "center",
+      "text-outline-color": "#000",
+      "text-outline-width": "2px",
+      color: "#fff",
+    },
+  },
+  {
+    selector: 'node[type="topic"]',
+    style: {
+      content: "data(label)",
+      shape: "diamond",
+      "background-color": "green",
+      "font-size": "16px",
+      "text-outline-color": "#000",
+      "text-outline-width": "2px",
+      color: "#fff",
+    },
+  },
+  {
+    selector: 'node[type="service"]',
+    style: {
+      content: "data(label)",
+      shape: "ellipse",
+      "background-color": "red",
+      "font-size": "16px",
+      "text-valign": "center",
+      "text-halign": "center",
+      "text-outline-color": "#000",
+      "text-outline-width": "2px",
+      color: "#fff",
+    },
+  },
+];
 
-const STable = styled.div`
-  max-width: 100%;
-  min-width: 400px;
-  overflow: auto;
-`;
+const LAYOUT = ({
+  name: "cola",
+  animate: true,
+  refresh: 1,
+  maxSimulationTime: 1000,
+  nodeDimensionsIncludeLabels: true,
+  avoidOverlap: true,
+  handleDisconnected: true,
+} as unknown) as Cytoscape.LayoutOptions;
 
-const SRow = styled.div`
-  &:nth-child(even) {
-    background: #333;
+Cytoscape.use(CytoscapeCola);
+
+function unionInto<T>(dest: Set<T>, ...iterables: Set<T>[]): void {
+  for (const iterable of iterables) {
+    for (const item of iterable) {
+      dest.add(item);
+    }
   }
-`;
-
-const SCell = styled.div`
-  border: 0;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  font-size: 14px;
-  line-height: 1.6;
-  width: 33%;
-  display: inline-block;
-  padding: 2px 8px;
-  white-space: nowrap;
-`;
-
-const SHeader = styled.div`
-  font-size: 14px;
-  border-bottom: #333 solid 2px;
-`;
-
-const STitle = styled.div`
-  padding: 2px 8px;
-`;
-
-const SHeaderItem = styled.div`
-  overflow: hidden;
-  white-space: nowrap;
-`;
+}
 
 function TopicGraph() {
-  const { publishedTopics, subscribedTopics } = useMessagePipeline(
+  const { publishedTopics, subscribedTopics, services } = useMessagePipeline(
     useCallback(
       ({ playerState: { activeData } }) =>
         activeData
@@ -81,7 +98,7 @@ function TopicGraph() {
       [],
     ),
   );
-  if (publishedTopics == undefined || subscribedTopics == undefined) {
+  if (publishedTopics == undefined) {
     return (
       <>
         <PanelToolbar floating />
@@ -90,13 +107,68 @@ function TopicGraph() {
     );
   }
 
-  const elements = [
-    { data: { id: "one", label: "Node 1" }, position: { x: 0, y: 0 } },
-    { data: { id: "two", label: "Node 2" }, position: { x: 100, y: 0 } },
-    { data: { source: "one", target: "two", label: "Edge from Node1 to Node2" } },
-  ];
+  const elements: cytoscape.ElementDefinition[] = [];
 
-  return <CytoscapeComponent elements={elements}></CytoscapeComponent>;
+  const nodeIds = new Set<string>();
+  const topicIds = new Set<string>();
+  const serviceIds = new Set<string>();
+  publishedTopics.forEach((curNodes, topic) => {
+    unionInto(nodeIds, curNodes);
+    topicIds.add(topic);
+  });
+  if (subscribedTopics != undefined) {
+    subscribedTopics.forEach((curNodes, topic) => {
+      unionInto(nodeIds, curNodes);
+      topicIds.add(topic);
+    });
+  }
+  if (services != undefined) {
+    services.forEach((curNodes, topic) => {
+      unionInto(nodeIds, curNodes);
+      serviceIds.add(topic);
+    });
+  }
+
+  for (const node of nodeIds) {
+    elements.push({ data: { id: `n:${node}`, label: node, type: "node" } });
+  }
+  for (const topic of topicIds) {
+    elements.push({ data: { id: `t:${topic}`, label: topic, type: "topic" } });
+  }
+  for (const service of serviceIds) {
+    elements.push({ data: { id: `s:${service}`, label: service, type: "service" } });
+  }
+
+  for (const [topic, publishers] of publishedTopics.entries()) {
+    for (const node of publishers) {
+      elements.push({ data: { source: `n:${node}`, target: `t:${topic}` } });
+    }
+  }
+
+  if (subscribedTopics != undefined) {
+    for (const [topic, subscribers] of subscribedTopics.entries()) {
+      for (const node of subscribers) {
+        elements.push({ data: { source: `t:${topic}`, target: `n:${node}` } });
+      }
+    }
+  }
+
+  if (services != undefined) {
+    for (const [service, providers] of services.entries()) {
+      for (const node of providers) {
+        elements.push({ data: { source: `n:${node}`, target: `s:${service}` } });
+      }
+    }
+  }
+
+  return (
+    <CytoscapeComponent
+      elements={elements}
+      style={{ width: "100%", height: "100%" }}
+      stylesheet={STYLESHEET}
+      layout={LAYOUT}
+    ></CytoscapeComponent>
+  );
 }
 
 TopicGraph.panelType = "TopicGraph";
