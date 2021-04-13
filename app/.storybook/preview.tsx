fa9f194fc09a60fe6e2c9d2dab636f89344ac65a
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-13 12:16:58 -1000

    Add WindowGeometryContext to all stories

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import waitForFonts from "@foxglove-studio/app/util/waitForFonts";
 import { withMockSubscribeToNewsletter } from "./__mocks__/subscribeToNewsletter";
 import { Story, StoryContext } from "@storybook/react";
 import ThemeProvider from "@foxglove-studio/app/theme/ThemeProvider";
+import WindowGeometryContext from "@foxglove-studio/app/context/WindowGeometryContext";
 
 import "@foxglove-studio/app/styles/global.scss";
 import "./styles.scss";
@@ -16,6 +17,14 @@ function withTheme(Story: Story, { parameters }: StoryContext) {
   );
 }
 
+function withWindowGeometry(Story: Story) {
+  return (
+    <WindowGeometryContext.Provider value={{ insetToolbar: false }}>
+      <Story />
+    </WindowGeometryContext.Provider>
+  );
+}
+
 export const loaders = [
   async () => {
     // These loaders are run once for each story when you switch between stories,
@@ -27,7 +36,7 @@ export const loaders = [
   },
 ];
 
-export const decorators = [withTheme, withMockSubscribeToNewsletter];
+export const decorators = [withTheme, withWindowGeometry, withMockSubscribeToNewsletter];
 
 export const parameters = {
   // Disable default padding around the page body
