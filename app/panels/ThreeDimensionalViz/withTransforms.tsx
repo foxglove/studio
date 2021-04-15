258d4a81e411fd8e698b3c22e4abb937bf2c4b6f
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-14 15:43:13 -1000

    Some Node Playground usability fixes

--- app/panels/ThreeDimensionalViz/withTransforms.tsx
+++ app/panels/ThreeDimensionalViz/withTransforms.tsx
@@ -62,11 +62,38 @@ function withTransforms<Props extends any>(ChildComponent: React.ComponentType<P
       // that topic) and transform frames (coordinate frames)
       for (const topic in frame) {
         for (const msg of frame[topic] as Message[]) {
-          const frameId: string | undefined = isBobject(msg.message)
-            ? msg.message.header?.().frame_id?.()
-            : msg.message.header?.frame_id;
-          if (frameId != undefined) {
-            transforms.register(frameId);
+          {
+            const frameId: string | undefined = isBobject(msg.message)
+              ? msg.message.header?.().frame_id?.()
+              : msg.message.header?.frame_id;
+            if (frameId != undefined) {
+              transforms.register(frameId);
+              continue;
+            }
+          }
+          // A hack specific to MarkerArray messages, which don't themselves have headers, but individual markers do.
+          if (isBobject(msg.message)) {
+            const markers = msg.message.markers?.();
+            if (!markers) {
+              continue;
+            }
+            for (const marker of markers) {
+              const frameId = marker.header?.().frame_id?.();
+              if (frameId != undefined) {
+                transforms.register(frameId);
+              }
+            }
+          } else {
+            const markers = msg.message.markers;
+            if (!markers) {
+              continue;
+            }
+            for (const marker of markers) {
+              const frameId = marker.header?.frame_id;
+              if (frameId != undefined) {
+                transforms.register(frameId);
+              }
+            }
           }
         }
       }
