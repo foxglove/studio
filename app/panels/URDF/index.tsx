1d11439def04552b2eed0a8363a0d0e3bfa3fbc8
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-16 08:21:54 -1000

    WIP

--- app/panels/URDF/index.tsx
+++ app/panels/URDF/index.tsx
@@ -1,128 +1,118 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { useResizeDetector } from "react-resize-detector";
-import { useUpdate } from "react-use";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { URDFRobot } from "urdf-loader/src/URDFClasses";
 
-import Flex from "@foxglove-studio/app/components/Flex";
 import Panel from "@foxglove-studio/app/components/Panel";
 import PanelToolbar from "@foxglove-studio/app/components/PanelToolbar";
 import { useRobotModels } from "@foxglove-studio/app/context/RobotModelContext";
+import useCleanup from "@foxglove-studio/app/hooks/useCleanup";
+
+class Renderer {
+  private scene = new THREE.Scene();
+  private camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+  private controls?: OrbitControls;
+  private renderer?: THREE.WebGLRenderer;
+  private ambientLight: THREE.HemisphereLight;
+  private dirLight: THREE.DirectionalLight;
+  private models: readonly URDFRobot[] = [];
+
+  private frame?: number;
+
+  constructor() {
+    this.camera.position.z = 5;
+
+    this.ambientLight = new THREE.HemisphereLight("#666666", "#000");
+    this.ambientLight.groundColor.lerp(this.ambientLight.color, 0.5);
+    this.ambientLight.intensity = 0.8;
+    this.ambientLight.position.set(0, 1, 0);
+    this.scene.add(this.ambientLight);
+
+    this.dirLight = new THREE.DirectionalLight(0xffffff);
+    this.scene.add(this.dirLight);
+    this.scene.add(this.dirLight.target);
+  }
+
+  setCanvas(canvas: HTMLCanvasElement) {
+    this.renderer = new THREE.WebGLRenderer({ canvas });
+    this.renderer.setClearColor(0xffffff);
+    this.renderer.setClearAlpha(0.5);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.controls = new OrbitControls(this.camera, canvas);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25;
+    this.controls.enableZoom = true;
+    this.controls.addEventListener("change", () => {
+      if (this.frame == undefined) {
+        this.frame = requestAnimationFrame(() => this.render());
+      }
+    });
+  }
+
+  setSize(width: number, height: number) {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer?.setSize(width, height);
+  }
+
+  setModels(models: readonly URDFRobot[]) {
+    for (const model of this.models) {
+      this.scene.remove(model);
+    }
+    this.models = models;
+    for (const model of this.models) {
+      this.scene.add(model);
+    }
+  }
+
+  render() {
+    if (this.frame != undefined) {
+      cancelAnimationFrame(this.frame);
+      this.frame = undefined;
+    }
+    this.dirLight.position.copy(this.camera.position);
+    this.renderer?.render(this.scene, this.camera);
+  }
+
+  dispose() {
+    if (this.frame != undefined) {
+      cancelAnimationFrame(this.frame);
+      this.frame = undefined;
+    }
+  }
+}
 
 function URDF() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | ReactNull>(ReactNull);
   const { ref: resizeRef, width, height } = useResizeDetector();
-  console.log("canvas size", { width, height, canvas });
   const { models } = useRobotModels();
-  const scene = useMemo(() => new THREE.Scene(), []);
-  const camera = useMemo(() => new THREE.PerspectiveCamera(75, 1, 0.1, 1000), []);
-  const controls = useMemo(() => canvas && new OrbitControls(camera, canvas), [camera, canvas]);
 
-  const renderer = useMemo(() => canvas && new THREE.WebGLRenderer({ canvas }), [canvas]);
+  const [renderer] = useState(() => new Renderer());
+  useCleanup(() => renderer.dispose());
 
-  const forceUpdate = useUpdate();
-  const frameRef = useRef<number | undefined>();
-  useEffect(() => {
-    if (renderer) {
-      renderer.setClearColor(0xffffff);
-      renderer.setClearAlpha(0.5);
-      renderer.shadowMap.enabled = true;
-      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-      renderer.outputEncoding = THREE.sRGBEncoding;
+  useLayoutEffect(() => {
+    if (canvas) {
+      renderer.setCanvas(canvas);
     }
-
-    camera.position.z = 5; // + Math.sin(performance.now());
-    if (controls) {
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.25;
-      controls.enableZoom = true;
-      // controls.autoRotate = true;
-      controls.addEventListener("change", (e) => {
-        if (frameRef.current == undefined) {
-          frameRef.current = requestAnimationFrame(() => forceUpdate());
-        }
-      });
-    }
-  }, [camera, controls, forceUpdate, renderer]);
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  // cube.position.z = 15;
+  }, [canvas, renderer]);
 
   useLayoutEffect(() => {
     if (width != undefined && height != undefined) {
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-      renderer?.setSize(width, height);
+      renderer.setSize(width, height);
     }
-  }, [width, height, camera, renderer]);
+  }, [width, height, renderer]);
 
   useLayoutEffect(() => {
-    frameRef.current = undefined;
-    scene.clear();
+    renderer.setModels(models);
+  }, [renderer, models]);
 
-    const ambientLight = new THREE.HemisphereLight("#666666", "#000");
-    ambientLight.groundColor.lerp(ambientLight.color, 0.5);
-    ambientLight.intensity = 0.5;
-    ambientLight.position.set(0, 1, 0);
-    scene.add(ambientLight);
-
-    // Light setup
-    const dirLight = new THREE.DirectionalLight(0xffffff);
-    dirLight.position.set(4, 10, 1);
-    dirLight.shadow.mapSize.width = 2048;
-    dirLight.shadow.mapSize.height = 2048;
-    dirLight.shadow.normalBias = 0.001;
-    dirLight.castShadow = true;
-    scene.add(dirLight);
-    scene.add(dirLight.target);
-
-    const world = new THREE.Object3D();
-
-    const plane = new THREE.Mesh(
-      new THREE.PlaneBufferGeometry(40, 40),
-      new THREE.ShadowMaterial({ side: THREE.DoubleSide, transparent: true, opacity: 0.5 }),
-    );
-    plane.rotation.x = -Math.PI / 2;
-    plane.position.y = -0.5;
-    plane.receiveShadow = true;
-    plane.scale.set(10, 10, 10);
-    scene.add(plane);
-    scene.add(world);
-    // world.add(cube);
-
-    // scene.add(cube);
-    if (models.length > 0) {
-      scene.add(...models);
-      console.log("models", models[0]);
-      // models[0]?.traverse((c: any) => {
-      //   if (c.isMesh) {
-      //     c.castShadow = true;
-      //     c.receiveShadow = true;
-
-      //     if (c.material) {
-      //       const mats = (Array.isArray(c.material) ? c.material : [c.material]).map((m: any) => {
-      //         if (m instanceof THREE.MeshBasicMaterial) {
-      //           m = new THREE.MeshPhongMaterial();
-      //         }
-
-      //         if (m.map) {
-      //           m.map.encoding = THREE.GammaEncoding;
-      //         }
-
-      //         return m;
-      //       });
-      //       c.material = mats.length === 1 ? mats[0] : mats;
-      //     }
-      //   }
-      // });
-    }
-    controls?.update();
-    renderer?.render(scene, camera);
+  useLayoutEffect(() => {
+    renderer.render();
   });
 
   return (
