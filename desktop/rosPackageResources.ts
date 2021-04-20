9ac11f48214f531ef87ecaa9f41193cd7399507f
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-20 10:25:07 -1000

    Fix fs/promises import

--- desktop/rosPackageResources.ts
+++ desktop/rosPackageResources.ts
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 import { protocol } from "electron";
-import fs from "fs/promises";
+import { promises as fs } from "fs";
 import path from "path";
 import { PNG } from "pngjs";
 import UTIF from "utif";
