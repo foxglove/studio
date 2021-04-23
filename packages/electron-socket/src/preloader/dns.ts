c8f559bb5feee690ad3d4de292d2fa3cdbe0bc4e
Adrian Macneil <adrian@foxglove.dev>
2021-04-23 09:25:21 -0700

    fix tsconfig

--- packages/electron-socket/src/preloader/dns.ts
+++ packages/electron-socket/src/preloader/dns.ts
@@ -2,12 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-/* eslint-disable @typescript-eslint/triple-slash-reference */
-
-/// <reference types="node" />
-/// <reference path="../../typings/dns-packet/index.d.ts" />
-/// <reference path="../../typings/multicast-dns/index.d.ts" />
-
 import dns from "dns";
 import type packet from "dns-packet";
 import MulticastDns from "multicast-dns";
