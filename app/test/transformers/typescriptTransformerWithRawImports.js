94bcffa138f7c4673af18207bd5d2a52537e3900
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-23 12:46:01 -1000

    Fix jsdom environment

--- app/test/transformers/typescriptTransformerWithRawImports.js
+++ app/test/transformers/typescriptTransformerWithRawImports.js
@@ -2,33 +2,28 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import fs from "fs";
-import { Transformer } from "@jest/transform";
-// import {} from "@jest/create-cache-key-function";
+/* eslint-disable @typescript-eslint/no-var-requires */
+const babelJest = require("babel-jest").default;
+const fs = require("fs");
 
 // look for `?raw` import statements
 // re-write these into `const variable = "string source";`;
 const importRegEx = /^import (.*) from "(.*)\?raw";$/gm;
-const importReplacer = (_: unknown, p1: string, p2: string) => {
+const importReplacer = (_, p1, p2) => {
   const resolved = require.resolve(p2);
   const rawFile = fs.readFileSync(resolved, { encoding: "utf-8" });
   return `const ${p1} = ${JSON.stringify(rawFile.toString())};`;
 };
 
-function rewriteSource(source: string) {
+function rewriteSource(source) {
   return source.replace(importRegEx, importReplacer);
 }
 
-const transformer: Transformer = {
-  process(sourceText, sourcePath, options) {
-    return rewriteSource(sourceText);
+module.exports = {
+  process(sourceText, ...args) {
+    return babelJest.process(rewriteSource(sourceText), ...args);
   },
-  // FIXME
-  // getCacheKey(
-  //   sourceText, sourcePath, options,
-  // ) {
-  //   cryptod
-  //   return transformer.getCacheKey(rewriteSource(source), filePath, options);
-  // },
+  getCacheKey(sourceText, ...args) {
+    return babelJest.getCacheKey(rewriteSource(sourceText), ...args);
+  },
 };
-module.exports = transformer;
