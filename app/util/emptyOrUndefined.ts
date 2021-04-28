d5d36b081dc24769fd4b93e1c294edadf2170efc
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-27 16:23:16 -1000

    comment

--- app/util/emptyOrUndefined.ts
+++ app/util/emptyOrUndefined.ts
@@ -2,6 +2,9 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// Theoretically we could provide isEmptyOrUndefined as a type guard returning `str is "" |
+// undefined`, but this doesn't work: https://github.com/microsoft/TypeScript/issues/31156
+
 export function isNonEmptyOrUndefined(str: string | undefined): str is string {
   return str != undefined && str.length !== 0;
 }
