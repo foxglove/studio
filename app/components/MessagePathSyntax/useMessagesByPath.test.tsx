be9838f326c109309f8a64a2d62335c4f7f90672
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-05-20 12:44:41 -1000

    Fix useMessagesByPath tests

--- app/components/MessagePathSyntax/useMessagesByPath.test.tsx
+++ app/components/MessagePathSyntax/useMessagesByPath.test.tsx
@@ -13,13 +13,15 @@
 //   You may not use this file except in compliance with the License.
 
 import { act, renderHook } from "@testing-library/react-hooks";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useLayoutEffect, useState } from "react";
 
-import { setGlobalVariables } from "@foxglove/studio-base/actions/panels";
+import { MessageDataItemsByPath } from "@foxglove/studio-base/components/MessagePathSyntax/useCachedGetMessagePathDataItems";
 import useMessagesByPath from "@foxglove/studio-base/components/MessagePathSyntax/useMessagesByPath";
 import MockMessagePipelineProvider from "@foxglove/studio-base/components/MessagePipeline/MockMessagePipelineProvider";
-import createRootReducer from "@foxglove/studio-base/reducers";
-import configureStore from "@foxglove/studio-base/store/configureStore.testing";
+import useGlobalVariables, {
+  GlobalVariables,
+} from "@foxglove/studio-base/hooks/useGlobalVariables";
+import CurrentLayoutProvider from "@foxglove/studio-base/providers/CurrentLayoutProvider";
 
 import * as fixture from "./fixture";
 
@@ -38,41 +40,58 @@ type TestProps = {
 } & Partial<
   Pick<
     React.ComponentProps<typeof MockMessagePipelineProvider>,
-    "topics" | "datatypes" | "messages" | "activeData" | "store"
+    "topics" | "datatypes" | "messages" | "activeData"
   >
 >;
 
-function makeMessagePipelineWrapper() {
+function makeMessagePipelineWrapper(initialGlobalVariables?: GlobalVariables) {
   const setSubscriptions = jest.fn();
+
+  function SetInitialGlobalVariables({ children }: { children: JSX.Element }) {
+    const { setGlobalVariables } = useGlobalVariables();
+    const [initialized, setInitialized] = useState(initialGlobalVariables == undefined);
+    useLayoutEffect(() => {
+      if (!initialized && initialGlobalVariables != undefined) {
+        setGlobalVariables(initialGlobalVariables);
+        setInitialized(true);
+      }
+    }, [initialized, setGlobalVariables]);
+    return initialized ? children : ReactNull;
+  }
+
   const wrapper = ({
     children,
     topics = [],
     datatypes = {},
     messages = [],
     activeData,
-    store,
   }: PropsWithChildren<TestProps>) => (
-    <MockMessagePipelineProvider
-      topics={topics}
-      datatypes={datatypes}
-      messages={messages}
-      setSubscriptions={setSubscriptions}
-      activeData={activeData}
-      store={store}
-    >
-      {children}
-    </MockMessagePipelineProvider>
+    <CurrentLayoutProvider>
+      <SetInitialGlobalVariables>
+        <MockMessagePipelineProvider
+          topics={topics}
+          datatypes={datatypes}
+          messages={messages}
+          setSubscriptions={setSubscriptions}
+          activeData={activeData}
+        >
+          {children}
+        </MockMessagePipelineProvider>
+      </SetInitialGlobalVariables>
+    </CurrentLayoutProvider>
   );
   return { setSubscriptions, wrapper };
 }
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const callback = ({ paths, historySize }: TestProps) => useMessagesByPath(paths, historySize);
+const Hooks = ({ paths, historySize }: TestProps) => ({
+  messagesByPath: useMessagesByPath(paths, historySize),
+  setGlobalVariables: useGlobalVariables().setGlobalVariables,
+});
 
 describe("useMessagesByPath", () => {
   it("(un)subscribes based on `topics`", () => {
     const { setSubscriptions, wrapper } = makeMessagePipelineWrapper();
-    const { unmount, rerender } = renderHook(callback, {
+    const { unmount, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps: {
         paths: ["/some/topic", "/some/other/topic"],
@@ -102,7 +121,7 @@ describe("useMessagesByPath", () => {
   it("does not filter out non-existing topics", () => {
     // Initial mount. Note that we haven't received any topics yet.
     const { setSubscriptions, wrapper } = makeMessagePipelineWrapper();
-    const { unmount, rerender } = renderHook(callback, {
+    const { unmount, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps: {
         paths: ["/some/topic"],
@@ -135,7 +154,7 @@ describe("useMessagesByPath", () => {
       topics: singleTopic,
       historySize: 1,
     };
-    const { result, rerender, unmount } = renderHook(callback, {
+    const { result, rerender, unmount } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
@@ -143,7 +162,9 @@ describe("useMessagesByPath", () => {
     rerender({ ...initialProps, historySize: 2, messages: [...fixture.messages] });
     unmount();
 
-    expect(result.all).toEqual([
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([
       { "/some/topic": [] },
       { "/some/topic": [] },
       {
@@ -165,7 +186,7 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0], fixture.messages[1]],
     };
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
@@ -175,7 +196,9 @@ describe("useMessagesByPath", () => {
     // (since bufferSize=2).
     rerender({ ...initialProps, messages: [fixture.messages[2]] });
 
-    expect(result.all).toEqual([
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([
       { "/some/topic": [queriedMessage(0), queriedMessage(1)] },
       { "/some/topic": [queriedMessage(1), queriedMessage(2)] },
     ]);
@@ -189,7 +212,7 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0]],
     };
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
@@ -198,7 +221,9 @@ describe("useMessagesByPath", () => {
     // Do the seek, and make sure we clear things out.
     rerender({ ...initialProps, messages: [], activeData: { lastSeekTime: 1 } });
 
-    expect(result.all).toEqual([{ "/some/topic": [queriedMessage(0)] }, { "/some/topic": [] }]);
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([{ "/some/topic": [queriedMessage(0)] }, { "/some/topic": [] }]);
   });
 
   it("returns the same when passing in a topic twice", () => {
@@ -208,16 +233,18 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0], fixture.messages[1]],
     };
-    const { result: result1 } = renderHook(callback, {
+    const { result: result1 } = renderHook(Hooks, {
       wrapper,
       initialProps: { ...initialProps, paths: ["/some/topic"] },
     });
-    const { result: result2 } = renderHook(callback, {
+    const { result: result2 } = renderHook(Hooks, {
       wrapper,
       initialProps: { ...initialProps, paths: ["/some/topic", "/some/topic"] },
     });
 
-    expect(result1.all).toEqual(result2.all);
+    expect(
+      result1.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual(result2.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)));
   });
 
   it("lets you drill down in a path", () => {
@@ -228,11 +255,13 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0], fixture.messages[1]],
     };
-    const { result } = renderHook(callback, {
+    const { result } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
-    expect(result.all).toEqual([
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([
       {
         "/some/topic.index": [
           {
@@ -259,7 +288,7 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0]],
     };
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
@@ -267,7 +296,9 @@ describe("useMessagesByPath", () => {
     // Add a new path, and we should get another call with the same data
     rerender({ ...initialProps, paths: ["/some/topic", "/some/other/topic"] });
 
-    expect(result.all).toEqual([
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([
       { "/some/topic": [queriedMessage(0)] },
       { "/some/topic": [queriedMessage(0)], "/some/other/topic": [] },
     ]);
@@ -284,7 +315,7 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0]],
     };
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
@@ -292,7 +323,9 @@ describe("useMessagesByPath", () => {
     // Change an existing path, and we should restore the data from the previous path on the same topic
     rerender({ ...initialProps, paths: ["/some/topic.index"] });
 
-    expect(result.all).toEqual([
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([
       { "/some/topic": [queriedMessage(0)] },
       {
         "/some/topic.index": [
@@ -330,29 +363,26 @@ describe("useMessagesByPath", () => {
       },
     };
     it("updates queriedData when a global variable changes", () => {
-      const store = configureStore(createRootReducer());
-
-      store.dispatch(setGlobalVariables({ foo: 0 }));
-
-      const { wrapper } = makeMessagePipelineWrapper();
+      const { wrapper } = makeMessagePipelineWrapper({ foo: 0 });
       const initialProps = {
         paths: ["/some/topic.bars[:]{index==$foo}.baz"],
-        store,
         topics: [{ name: "/some/topic", datatype: "dtype/Foo" }],
         datatypes: exampleDatatypes,
         messages: [message],
       };
-      const { result, unmount } = renderHook(callback, {
+      const { result, unmount } = renderHook(Hooks, {
         wrapper,
         initialProps,
       });
 
       // when $foo changes to 1, queriedData.value should change to 11
       act(() => {
-        store.dispatch(setGlobalVariables({ foo: 1 }));
+        result.current.setGlobalVariables({ foo: 1 });
       });
 
-      expect(result.all).toEqual([
+      expect(
+        result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+      ).toEqual([
         {
           "/some/topic.bars[:]{index==$foo}.baz": [
             {
@@ -377,7 +407,7 @@ describe("useMessagesByPath", () => {
 
   it("supports changing a path for a previously-existing topic that no longer exists", () => {
     const { wrapper } = makeMessagePipelineWrapper();
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps: {
         paths: ["/some/topic"],
@@ -389,10 +419,9 @@ describe("useMessagesByPath", () => {
 
     rerender({ topics: [], datatypes: {}, messages: [], paths: ["/some/topic.index"] });
 
-    expect(result.all).toEqual([
-      { "/some/topic": [queriedMessage(0)] },
-      { "/some/topic.index": [] },
-    ]);
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([{ "/some/topic": [queriedMessage(0)] }, { "/some/topic.index": [] }]);
   });
 
   it("ignores messages from non-subscribed topics", () => {
@@ -406,18 +435,20 @@ describe("useMessagesByPath", () => {
       datatypes: fixture.datatypes,
       messages: [fixture.messages[0]],
     };
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps,
     });
     rerender({ ...initialProps, messages: [fixture.messages[1], fixture.messages[2]] });
 
-    expect(result.all).toEqual([{ "/some/other/topic": [] }, { "/some/other/topic": [] }]);
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([{ "/some/other/topic": [] }, { "/some/other/topic": [] }]);
   });
 
   it("return the same itemsByPath (identity) if the MessageHistory props did not change but children changed", () => {
     const { wrapper } = makeMessagePipelineWrapper();
-    const { result, rerender } = renderHook(callback, {
+    const { result, rerender } = renderHook(Hooks, {
       wrapper,
       initialProps: {
         paths: ["/some/topic"],
@@ -427,10 +458,11 @@ describe("useMessagesByPath", () => {
       },
     });
     rerender();
-    expect(result.all).toEqual([
-      { "/some/topic": [queriedMessage(0)] },
-      { "/some/topic": [queriedMessage(0)] },
-    ]);
-    expect(result.all[0]).toBe(result.all[1]);
+    expect(
+      result.all.map((item) => (item instanceof Error ? undefined : item.messagesByPath)),
+    ).toEqual([{ "/some/topic": [queriedMessage(0)] }, { "/some/topic": [queriedMessage(0)] }]);
+    expect((result.all[0] as { messagesByPath: MessageDataItemsByPath }).messagesByPath).toBe(
+      (result.all[1] as { messagesByPath: MessageDataItemsByPath }).messagesByPath,
+    );
   });
 });
