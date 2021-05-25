336c915af9085e15f7a06593b400d27c56708389
John Hurliman <jhurliman@jhurliman.org>
2021-05-25 15:07:39 -0700

    Update template to match latest @foxglove/studio API

--- packages/@foxglove/fox/template/src/ExamplePanel.tsx
+++ packages/@foxglove/fox/template/src/ExamplePanel.tsx
@@ -1,13 +1,3 @@
-// persisted panel state
-type Config = {
-  storedValue?: number;
-};
-
-type Props = {
-  config: Config;
-  saveConfig: (config: Config) => void;
-};
-
-export function ExamplePanel(props: Props): JSX.Element {
-  return <div>Stored value is: {props.config.storedValue}</div>;
+export function ExamplePanel(): JSX.Element {
+  return <div>Hello, world!</div>;
 }
