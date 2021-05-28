eed23116c36a199db103d0d4d73777cd7383bab0
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-05-28 07:09:58 -1000

    Upgrade yarn to 3.0.0-rc.2

--- .yarn/yarn-wrapper.js
+++ .yarn/yarn-wrapper.js
@@ -4,7 +4,7 @@
 const path = require("path");
 const fs = require("fs");
 
-const REAL_YARN = path.join(__dirname, "releases", "yarn-sources.cjs");
+const REAL_YARN = path.join(__dirname, "releases", "yarn-3.0.0-rc.2.cjs");
 
 try {
   if (fs.statSync(REAL_YARN).size < 10000) {
