fd56a857bf20d0b6fe9b95cb16f16ba9b49cc7c7
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-06-15 18:37:34 -1000

    lint

--- packages/studio-base/src/providers/CurrentLayoutProvider/CurrentLayoutState.ts
+++ packages/studio-base/src/providers/CurrentLayoutProvider/CurrentLayoutState.ts
@@ -2,7 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 import { isEqual } from "lodash";
-import moment from "moment";
 import { getNodeAtPath } from "react-mosaic-component";
 import { v4 as uuidv4 } from "uuid";
 
