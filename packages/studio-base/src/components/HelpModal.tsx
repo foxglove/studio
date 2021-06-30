df05a951ac021ed9a4a69f8eb7a842d9f7124da7
Andrew Carr <andrew@2metres.com>
2021-07-01 08:41:10 +1000

    revert close icon to absolute position to prior style

--- packages/studio-base/src/components/HelpModal.tsx
+++ packages/studio-base/src/components/HelpModal.tsx
@@ -1,15 +1,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//   Copyright 2018-2021 Cruise LLC
-//
-//   This source code is licensed under the Apache License, Version 2.0,
-//   found at http://www.apache.org/licenses/LICENSE-2.0
-//   You may not use this file except in compliance with the License.
 
 import { IconButton, Modal, makeStyles, useTheme } from "@fluentui/react";
 import { PropsWithChildren, ReactElement } from "react";
@@ -21,19 +12,19 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing.l1,
   },
   header: {
-    backgroundColor: theme.semanticColors.bodyBackground,
     display: "flex",
     flexDirection: "space-between",
     justifyContent: "flex-end",
-    padding: theme.spacing.s1,
-    position: "sticky",
+    padding: theme.spacing.l1,
+    position: "absolute",
     top: 0,
+    right: 0,
   },
 }));
 
 export default function HelpModal({
-  onRequestClose,
   children,
+  onRequestClose,
 }: PropsWithChildren<{ onRequestClose: () => void }>): ReactElement {
   const classes = useStyles();
   const theme = useTheme();
