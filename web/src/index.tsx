0b2d3e28c6eb5a1ead184d9bae9a16f9e5bfbf3f
Esther Weon <esther.s.weon@gmail.com>
2021-07-19 12:50:49 -0700

    Add banner for unsupported browser

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,14 +3,63 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { init as initSentry } from "@sentry/browser";
+import { useState } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
 import Logger from "@foxglove/log";
-import { installDevtoolsFormatters, overwriteFetch, waitForFonts } from "@foxglove/studio-base";
 
-import Root from "./Root";
+import "@foxglove/studio-base/styles/global.scss";
 
 const log = Logger.getLogger(__filename);
+const MINIMUM_CHROME_VERSION = 91;
+const StyledBanner = styled.div`
+  text-align: center;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  padding: 10px;
+  background-color: rgba(99, 102, 241, 0.9);
+  z-index: 100;
+`;
+
+const VersionBanner = function ({ isChrome }: { isChrome: boolean }) {
+  const [showBanner, setShowBanner] = useState(true);
+
+  if (!showBanner) {
+    return ReactNull;
+  }
+
+  const prompt = isChrome
+    ? `Update Chrome to version ${MINIMUM_CHROME_VERSION}+ to continue.`
+    : `You're using an unsupported browser. Use Chrome ${MINIMUM_CHROME_VERSION}+ to continue.`;
+  const fixText = isChrome ? "Update Chrome" : "Download Chrome";
+  const continueText = isChrome
+    ? "Continue with unsupported version"
+    : "Continue with unsupported browser";
+
+  return (
+    <StyledBanner>
+      <div>
+        <p>{prompt} </p>
+        {isChrome ? undefined : (
+          <p>
+            Check out our browser support progress in{" "}
+            <a href="https://github.com/foxglove/studio/issues/1422">this GitHub issue</a>.
+          </p>
+        )}
+      </div>
+
+      <div style={{ paddingTop: "20px" }}>
+        <a href="https://www.google.com/chrome/" target="_blank" rel="noreferrer">
+          <button>{fixText}</button>
+        </a>
+        <button onClick={() => setShowBanner(false)}>{continueText}</button>
+      </div>
+    </StyledBanner>
+  );
+};
 
 log.debug("initializing");
 
@@ -30,22 +79,58 @@ if (typeof process.env.SENTRY_DSN === "string") {
   });
 }
 
-installDevtoolsFormatters();
-overwriteFetch();
-
 const rootEl = document.getElementById("root");
 if (!rootEl) {
   throw new Error("missing #root element");
 }
 
 async function main() {
+  const chromeMatch = navigator.userAgent.match(/Chrome\/(\d+)\./);
+  const chromeVersion = chromeMatch ? parseInt(chromeMatch[1] ?? "", 10) : 0;
+  const isChrome = chromeVersion !== 0;
+
+  if (!isChrome) {
+    ReactDOM.render(
+      <>
+        <VersionBanner isChrome={false} />
+      </>,
+      rootEl,
+      () => {
+        // Integration tests look for this console log to indicate the app has rendered once
+        log.debug("App rendered");
+      },
+    );
+    return;
+  }
+
+  const { installDevtoolsFormatters, overwriteFetch, waitForFonts } = await import(
+    "@foxglove/studio-base"
+  );
+  installDevtoolsFormatters();
+  overwriteFetch();
   // consider moving waitForFonts into App to display an app loading screen
   await waitForFonts();
 
-  ReactDOM.render(<Root />, rootEl, () => {
-    // Integration tests look for this console log to indicate the app has rendered once
-    log.debug("App rendered");
-  });
+  const { Root } = await import("./Root");
+  if (chromeVersion >= MINIMUM_CHROME_VERSION) {
+    ReactDOM.render(<Root />, rootEl, () => {
+      // Integration tests look for this console log to indicate the app has rendered once
+      log.debug("App rendered");
+    });
+    return;
+  }
+
+  ReactDOM.render(
+    <>
+      <VersionBanner isChrome={true} />
+      <Root />
+    </>,
+    rootEl,
+    () => {
+      // Integration tests look for this console log to indicate the app has rendered once
+      log.debug("App rendered");
+    },
+  );
 }
 
 void main();
