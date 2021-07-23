a8d324663f686c0e0941b7b1b9b3f8afcee06f01
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-07-22 20:59:32 -0700

    fix test

--- packages/studio-base/src/providers/CurrentLayoutProvider/index.test.tsx
+++ packages/studio-base/src/providers/CurrentLayoutProvider/index.test.tsx
@@ -148,6 +148,7 @@ describe("CurrentLayoutProvider", () => {
           {
             name: "unnamed",
             data: persistedState.panels,
+            permission: "creator_write",
           },
         ],
       ]);
