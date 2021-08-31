4847834084c398611eb5225e197088ab06322dc2
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-08-30 23:39:42 -0700

    help i'm trapped in a screenshot factory

--- packages/studio-base/src/panels/LegacyPlot/index.stories.tsx
+++ packages/studio-base/src/panels/LegacyPlot/index.stories.tsx
@@ -162,7 +162,7 @@ export function CustomMinMaxWindow(): JSX.Element {
 
 CustomMinMaxVal.parameters = { useReadySignal: true };
 export function CustomMinMaxVal(): JSX.Element {
-  const readySignal = useReadySignal({ count: 2 });
+  const readySignal = useReadySignal({ count: 1 });
   return (
     <PanelSetup fixture={fixture}>
       <TwoDimensionalPlot
@@ -210,7 +210,7 @@ export function WithTooltip(): JSX.Element {
 
 ShowResetAfterHorizontalZoom.parameters = { useReadySignal: true };
 export function ShowResetAfterHorizontalZoom(): JSX.Element {
-  const readySignal = useReadySignal({ count: 2 });
+  const readySignal = useReadySignal({ count: 1 });
   return (
     <PanelSetup
       fixture={fixture}
@@ -227,7 +227,7 @@ export function ShowResetAfterHorizontalZoom(): JSX.Element {
 }
 ShowResetAfterVerticalZoom.parameters = { useReadySignal: true };
 export function ShowResetAfterVerticalZoom(): JSX.Element {
-  const readySignal = useReadySignal({ count: 4 });
+  const readySignal = useReadySignal({ count: 3 });
   return (
     <PanelSetup
       fixture={fixture}
@@ -247,7 +247,7 @@ export function ShowResetAfterVerticalZoom(): JSX.Element {
 }
 ShowResetZoom.parameters = { useReadySignal: true };
 export function ShowResetZoom(): JSX.Element {
-  const readySignal = useReadySignal({ count: 4 });
+  const readySignal = useReadySignal({ count: 3 });
   return (
     <PanelSetup
       fixture={fixture}
@@ -268,7 +268,7 @@ export function ShowResetZoom(): JSX.Element {
 
 ResetZoom.parameters = { useReadySignal: true };
 export function ResetZoom(): JSX.Element {
-  const readySignal = useReadySignal({ count: 4 });
+  const readySignal = useReadySignal({ count: 3 });
   return (
     <PanelSetup
       fixture={fixture}
