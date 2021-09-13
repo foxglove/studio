2a19b695a4a7a2a824cac459cdec857bc63480d6
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-09-13 15:49:57 -0700

    Upgrade `@foxglove/eslint-plugin` and fix `no-boolean-parameters`

--- packages/studio-base/src/panels/ImageView/index.tsx
+++ packages/studio-base/src/panels/ImageView/index.tsx
@@ -149,14 +149,19 @@ const canTransformMarkersByTopic = (topic: string) => !topic.includes("rect");
 
 // Group image topics by the first component of their name
 
-function renderEmptyState(
-  cameraTopic: string,
-  markerTopics: string[],
-  shouldSynchronize: boolean,
+function renderEmptyState({
+  cameraTopic,
+  markerTopics,
+  shouldSynchronize,
+  messagesByTopic,
+}: {
+  cameraTopic: string;
+  markerTopics: string[];
+  shouldSynchronize: boolean;
   messagesByTopic: {
     [topic: string]: readonly MessageEvent<unknown>[];
-  },
-) {
+  };
+}) {
   if (cameraTopic === "") {
     return (
       <SEmptyStateWrapper>
@@ -211,6 +216,7 @@ function renderEmptyState(
 }
 
 function useOptionallySynchronizedMessages(
+  // eslint-disable-next-line @foxglove/no-boolean-parameters
   shouldSynchronize: boolean,
   topics: readonly PanelAPI.RequestedTopic[],
 ) {
@@ -617,7 +623,12 @@ function ImageView(props: Props) {
       {toolbar}
       {/* If rendered, EmptyState will hide the always-present ImageCanvas */}
       {showEmptyState &&
-        renderEmptyState(cameraTopic, enabledMarkerTopics, shouldSynchronize, messagesByTopic)}
+        renderEmptyState({
+          cameraTopic,
+          markerTopics: enabledMarkerTopics,
+          shouldSynchronize,
+          messagesByTopic,
+        })}
       {/* Always render the ImageCanvas because it's expensive to unmount and start up. */}
       {imageMessageToRender && (
         <ImageCanvas
