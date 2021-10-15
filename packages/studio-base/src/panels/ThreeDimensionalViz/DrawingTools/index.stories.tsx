a7e5d2edd7b020a7122037b381588ae5d6717e6a
Andrew Carr <andrew@2metres.com>
2021-10-15 11:29:48 +1100

    Fix up stories to match real use cases

--- packages/studio-base/src/panels/ThreeDimensionalViz/DrawingTools/index.stories.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/DrawingTools/index.stories.tsx
@@ -15,6 +15,7 @@ import { storiesOf } from "@storybook/react";
 import { PolygonBuilder } from "regl-worldview";
 
 import { pointsToPolygons } from "@foxglove/studio-base/panels/ThreeDimensionalViz/utils/drawToolUtils";
+import { colors } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 import DrawingTools, { POLYGON_TAB_TYPE } from "./index";
 
@@ -31,9 +32,13 @@ const polygons = pointsToPolygons([
   ],
 ]);
 
-const containerStyle = {
-  margin: 8,
-  display: "inline-block",
+const containerStyle: CSSProperties = {
+  padding: 8,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  backgroundColor: colors.DARK,
+  height: "100%",
 };
 
 const DEFAULT_PROPS = {
