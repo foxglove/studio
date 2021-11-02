Lichtblick-Suite/lichtblick

commit aaacaa824a52c93a13608f8787eefd3c36dab522

Move Player creation out of PlayerManager (#1513)
User-Facing Changes
None

Description
This change moves player creation out of PlayerManager and into individual factory instances. Each factory is responsible for the initialization of players of that specific type.

This change purposefully does not change the UX around selecting data sources. This change keeps file/directory dialog and prompt control within PlayerManager but changes the logic to use settings/hints from the selected data source factory.

Fixes: #776

packages/studio-base/src/Workspace.tsx
@@ -210,12 +210,9 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
     if (isMounted()) {
       setSelectedLayoutId(newLayout.id);
       if (props.demoBagUrl) {
-        selectSource(
-          { name: "Demo Bag", type: "ros1-remote-bagfile" },
-          {
-            url: props.demoBagUrl,
-          },
-        );
+        selectSource("ros1-remote-bagfile", {
+          url: props.demoBagUrl,
+        });
       }
     }
   }, [layoutStorage, isMounted, setSelectedLayoutId, props.demoBagUrl, selectSource]);
@@ -313,12 +310,9 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
         } else {
           previousFiles.current = otherFiles;
         }
-        selectSource(
-          { name: "ROS 1 Bag File (local)", type: "ros1-local-bagfile" },
-          {
-            files: previousFiles.current,
-          },
-        );
+        selectSource("ros1-local-bagfile", {
+          files: previousFiles.current,
+        });
       }
     },
     [addToast, extensionLoader, loadFromFile, selectSource],
@@ -355,10 +349,8 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
           return;
         }
         selectSource(
-          {
-            name: "ROS 1 Bag File (HTTP)",
-            type: "ros1-remote-bagfile",
-          },
+          "ros1-remote-bagfile",
+
           { url: bagUrl },
         );
       } else if (type === "foxglove-data-platform") {
@@ -378,13 +370,7 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
           log.warn(`Missing or invalid timestamp(s) in ${url}`);
           return;
         }
-        selectSource(
-          {
-            name: "Foxglove Data Platform",
-            type: "foxglove-data-platform",
-          },
-          { start, end, seekTo, deviceId },
-        );
+        selectSource("foxglove-data-platform", { start, end, seekTo, deviceId });
       } else {
         log.warn(`Unknown deep link type ${url}`);
       }

desktop/renderer/Root.tsx
@@ -8,7 +8,7 @@ import {
   App,
   ErrorBoundary,
   MultiProvider,
-  PlayerSourceDefinition,
+  IDataSourceFactory,
   ThemeProvider,
   UserProfileLocalStorageProvider,
   StudioToastProvider,
@@ -17,6 +17,14 @@ import {
   ConsoleApi,
   ConsoleApiContext,
   ConsoleApiRemoteLayoutStorageProvider,
+  Ros1LocalBagDataSourceFactory,
+  Ros2LocalBagDataSourceFactory,
+  RosbridgeDataSourceFactory,
+  VelodyneDataSourceFactory,
+  Ros1RemoteBagDataSourceFactory,
+  Ros1SocketDataSourceFactory,
+  Ros2SocketDataSourceFactory,
+  FoxgloveDataPlatformDataSourceFactory,
 } from "@foxglove/studio-base";
 
 import { Desktop } from "../common/types";
@@ -31,43 +39,18 @@ const DEMO_BAG_URL = "https://storage.googleapis.com/foxglove-public-assets/demo
 
 const desktopBridge = (global as unknown as { desktopBridge: Desktop }).desktopBridge;
 
-export default function Root(): ReactElement {
-  const playerSources: PlayerSourceDefinition[] = [
-    {
-      name: "ROS 1",
-      type: "ros1-socket",
-    },
-    {
-      name: "ROS 1 Rosbridge",
-      type: "rosbridge-websocket",
-    },
-    {
-      name: "ROS 1 Bag (local)",
-      type: "ros1-local-bagfile",
-    },
-    {
-      name: "ROS 1 Bag (remote)",
-      type: "ros1-remote-bagfile",
-    },
-    {
-      name: "ROS 2",
-      type: "ros2-socket",
-      badgeText: "beta",
-    },
-    {
-      name: "ROS 2 Rosbridge",
-      type: "rosbridge-websocket",
-    },
-    {
-      name: "ROS 2 Bag (local)",
-      type: "ros2-local-bagfile",
-    },
-    {
-      name: "Velodyne LIDAR",
-      type: "velodyne-device",
-    },
-  ];
+const dataSources: IDataSourceFactory[] = [
+  new Ros1SocketDataSourceFactory(),
+  new Ros1LocalBagDataSourceFactory(),
+  new Ros1RemoteBagDataSourceFactory(),
+  new Ros2SocketDataSourceFactory(),
+  new Ros2LocalBagDataSourceFactory(),
+  new RosbridgeDataSourceFactory(),
+  new VelodyneDataSourceFactory(),
+  new FoxgloveDataPlatformDataSourceFactory(),
+];
 
+export default function Root(): ReactElement {
   const api = useMemo(() => new ConsoleApi(process.env.FOXGLOVE_API_URL!), []);
 
   const providers = [
@@ -93,7 +76,7 @@ export default function Root(): ReactElement {
       <CssBaseline>
         <ErrorBoundary>
           <MultiProvider providers={providers}>
-            <App demoBagUrl={DEMO_BAG_URL} deepLinks={deepLinks} availableSources={playerSources} />
+            <App demoBagUrl={DEMO_BAG_URL} deepLinks={deepLinks} availableSources={dataSources} />
           </MultiProvider>
         </ErrorBoundary>
       </CssBaseline>

packages/studio-base/src/App.tsx
@@ -16,7 +16,7 @@ import AnalyticsProvider from "@foxglove/studio-base/context/AnalyticsProvider";
 import { AssetsProvider } from "@foxglove/studio-base/context/AssetsContext";
 import { HoverValueProvider } from "@foxglove/studio-base/context/HoverValueContext";
 import ModalHost from "@foxglove/studio-base/context/ModalHost";
-import { PlayerSourceDefinition } from "@foxglove/studio-base/context/PlayerSelectionContext";
+import { IDataSourceFactory } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import { UserNodeStateProvider } from "@foxglove/studio-base/context/UserNodeStateContext";
 import CurrentLayoutProvider from "@foxglove/studio-base/providers/CurrentLayoutProvider";
 import ExtensionMarketplaceProvider from "@foxglove/studio-base/providers/ExtensionMarketplaceProvider";
@@ -31,7 +31,7 @@ type AppProps = {
    * on first launch and not subsequent launches.
    */
   loadWelcomeLayout?: boolean;
-  availableSources: PlayerSourceDefinition[];
+  availableSources: IDataSourceFactory[];
   demoBagUrl?: string;
   deepLinks?: string[];
 };

packages/studio-base/src/components/ConnectionList/index.tsx
@@ -12,7 +12,7 @@ import {
 import NotificationModal from "@foxglove/studio-base/components/NotificationModal";
 import ModalContext from "@foxglove/studio-base/context/ModalContext";
 import {
-  PlayerSourceDefinition,
+  IDataSourceFactory,
   usePlayerSelection,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import { useConfirm } from "@foxglove/studio-base/hooks/useConfirm";
@@ -51,7 +51,7 @@ export default function ConnectionList(): JSX.Element {
   const theme = useTheme();
 
   const onSourceClick = useCallback(
-    (source: PlayerSourceDefinition) => {
+    (source: IDataSourceFactory) => {
       if (source.disabledReason != undefined) {
         void confirm({
           title: "Unsupported Connection",
@@ -62,7 +62,7 @@ export default function ConnectionList(): JSX.Element {
         return;
       }
 
-      selectSource(source);
+      selectSource(source.id);
     },
     [confirm, selectSource],
   );
@@ -95,31 +95,13 @@ export default function ConnectionList(): JSX.Element {
           : playerName}
       </Text>
       {availableSources.map((source) => {
-        let iconName: RegisteredIconNames;
-        switch (source.type) {
-          case "ros1-local-bagfile":
-            iconName = "OpenFile";
-            break;
-          case "ros2-local-bagfile":
-            iconName = "OpenFolder";
-            break;
-          case "ros1-socket":
-          case "ros2-socket":
-            iconName = "studio.ROS";
-            break;
-          case "rosbridge-websocket":
-            iconName = "Flow";
-            break;
-          case "foxglove-data-platform":
-          case "ros1-remote-bagfile":
-            iconName = "FileASPX";
-            break;
-          case "velodyne-device":
-            iconName = "GenericScan";
-            break;
+        if (source.hidden === true) {
+          return ReactNull;
         }
+
+        const iconName: RegisteredIconNames = source.iconName as RegisteredIconNames;
         return (
-          <div key={source.name}>
+          <div key={source.id}>
             <ActionButton
               styles={{
                 root: {
@@ -138,7 +120,7 @@ export default function ConnectionList(): JSX.Element {
               }}
               onClick={() => onSourceClick(source)}
             >
-              {source.name}
+              {source.displayName}
               {source.badgeText && <span className={styles.badge}>{source.badgeText}</span>}
             </ActionButton>
           </div>

packages/studio-base/src/components/NativeFileMenuPlayerSelection.tsx
@@ -19,14 +19,14 @@ export function NativeFileMenuPlayerSelection(): ReactElement {
     }
 
     for (const item of availableSources) {
-      nativeAppMenu.addFileEntry(item.name, () => {
-        selectSource(item);
+      nativeAppMenu.addFileEntry(item.displayName, () => {
+        selectSource(item.id);
       });
     }
 
     return () => {
       for (const item of availableSources) {
-        nativeAppMenu.removeFileEntry(item.name);
+        nativeAppMenu.removeFileEntry(item.displayName);
       }
     };
   }, [availableSources, nativeAppMenu, selectSource]);

packages/studio-base/src/context/PlayerSelectionContext.ts
@@ -4,61 +4,51 @@
 
 import { createContext, useContext } from "react";
 
-type SourceTypes =
-  | "foxglove-data-platform"
-  | "ros1-local-bagfile"
-  | "ros2-local-bagfile"
-  | "ros1-socket"
-  | "ros2-socket"
-  | "rosbridge-websocket"
-  | "ros1-remote-bagfile"
-  | "velodyne-device";
+import { PromptOptions } from "@foxglove/studio-base/hooks/usePrompt";
+import { Player, PlayerMetricsCollectorInterface } from "@foxglove/studio-base/players/types";
+import ConsoleApi from "@foxglove/studio-base/services/ConsoleApi";
 
-export type PlayerSourceDefinition = {
-  name: string;
-  type: SourceTypes;
+export type DataSourceFactoryInitializeArgs = {
+  metricsCollector: PlayerMetricsCollectorInterface;
+  unlimitedMemoryCache: boolean;
+  rosHostname?: string;
+  folder?: FileSystemDirectoryHandle;
+  file?: File;
+  url?: string;
+  consoleApi?: ConsoleApi;
+} & Record<string, unknown>;
+
+export interface IDataSourceFactory {
+  id: string;
+  displayName: string;
+  iconName?: RegisteredIconNames;
   disabledReason?: string | JSX.Element;
   badgeText?: string;
-};
+  hidden?: boolean;
 
-type FileSourceParams = {
-  files?: File[];
-};
+  // If data source initialization supports "Open File" workflow, this property lists the supported
+  // file types
+  supportedFileTypes?: string[];
 
-type FolderSourceParams = {
-  folder?: string;
-};
+  supportsOpenDirectory?: boolean;
 
-type HttpSourceParams = {
-  url?: string;
-};
+  promptOptions?: (previousValue?: string) => PromptOptions;
 
-type FoxgloveDataPlatformSourceParams = {
-  start?: string;
-  end?: string;
-  seekTo?: string;
-  deviceId?: string;
-};
-
-type SpecializedPlayerSource<T extends SourceTypes> = Omit<PlayerSourceDefinition, "type"> & {
-  type: T;
-};
-
-interface SelectSourceFunction {
-  (definition: SpecializedPlayerSource<"ros1-local-bagfile">, params?: FileSourceParams): void;
-  (definition: SpecializedPlayerSource<"ros2-local-bagfile">, params?: FolderSourceParams): void;
-  (definition: SpecializedPlayerSource<"ros1-remote-bagfile">, params?: HttpSourceParams): void;
-  (
-    definition: SpecializedPlayerSource<"foxglove-data-platform">,
-    params?: FoxgloveDataPlatformSourceParams,
-  ): void;
-  (definition: PlayerSourceDefinition, params?: never): void;
+  // Initialize a player.
+  initialize: (args: DataSourceFactoryInitializeArgs) => Player | undefined;
 }
 
-// PlayerSelection provides the user with a select function and the items to select
+/**
+ * PlayerSelectionContext exposes the available data sources and a function to set the current data source
+ */
 export interface PlayerSelection {
-  selectSource: SelectSourceFunction;
-  availableSources: PlayerSourceDefinition[];
+  selectSource: (sourceId: string, args?: Record<string, unknown>) => void;
+
+  /** Currently selected data source */
+  selectedSource?: IDataSourceFactory;
+
+  /** List of available data sources */
+  availableSources: IDataSourceFactory[];
 }
 
 const PlayerSelectionContext = createContext<PlayerSelection>({

packages/studio-base/src/dataSources/FoxgloveDataPlatformDataSourceFactory.tsx
@@ -1,0 +1,53 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import {
+  IDataSourceFactory,
+  DataSourceFactoryInitializeArgs,
+} from "@foxglove/studio-base/context/PlayerSelectionContext";
+import FoxgloveDataPlatformPlayer from "@foxglove/studio-base/players/FoxgloveDataPlatformPlayer";
+import { Player } from "@foxglove/studio-base/players/types";
+
+type FoxgloveDataPlatformOptions = {
+  start: string;
+  end: string;
+  seek?: string;
+  deviceId: string;
+};
+
+class FoxgloveDataPlatformDataSourceFactory implements IDataSourceFactory {
+  id = "foxglove-data-platform";
+  displayName = "Foxglove Data Platform";
+  iconName: IDataSourceFactory["iconName"] = "FileASPX";
+  hidden = true;
+
+  initialize(args: DataSourceFactoryInitializeArgs): Player | undefined {
+    if (!args.consoleApi) {
+      return;
+    }
+
+    // This could benefit from schema validation rather than casting
+    const start = args.start as string | undefined;
+    const end = args.end as string | undefined;
+    const deviceId = args.deviceId as string | undefined;
+    if (!start || !end || !deviceId) {
+      return;
+    }
+
+    const params: FoxgloveDataPlatformOptions = {
+      start,
+      end,
+      deviceId,
+      seek: args.seek as string | undefined,
+    };
+
+    return new FoxgloveDataPlatformPlayer({
+      params,
+      consoleApi: args.consoleApi,
+      metricsCollector: args.metricsCollector,
+    });
+  }
+}
+
+export default FoxgloveDataPlatformDataSourceFactory;

foxglove/studio

commit daa14098e9dbb809fe8945f2024552b774f191dc

fix: drag & drop initializes a source with file arg
The player manager failed to properly detect the files argument provided when selecting a data source via drag & drop. This was a regression introduced by https://github.com/foxglove/studio/pull/1513. This fixes the regression and also expands drag & drop support to any data source that supports files.

Fixes: #2061

packages/studio-base/src/Workspace.tsx
@@ -11,6 +11,7 @@
 //   You may not use this file except in compliance with the License.
 
 import { Link, makeStyles, Stack, Text, useTheme } from "@fluentui/react";
+import { extname } from "path";
 import {
   useState,
   useEffect,
@@ -138,9 +139,6 @@ function Variables() {
   );
 }
 
-// file types we support for drag/drop
-const allowedDropExtensions = [".bag", ".foxe", ".urdf", ".xacro"];
-
 type WorkspaceProps = {
   loadWelcomeLayout?: boolean;
   demoBagUrl?: string;
@@ -156,10 +154,21 @@ const selectPlayerProblems = ({ playerState }: MessagePipelineContext) => player
 export default function Workspace(props: WorkspaceProps): JSX.Element {
   const classes = useStyles();
   const containerRef = useRef<HTMLDivElement>(ReactNull);
-  const { selectSource } = usePlayerSelection();
+  const { availableSources, selectSource } = usePlayerSelection();
   const playerPresence = useMessagePipeline(selectPlayerPresence);
   const playerCapabilities = useMessagePipeline(selectPlayerCapabilities);
   const playerProblems = useMessagePipeline(selectPlayerProblems);
+
+  // file types we support for drag/drop
+  const allowedDropExtensions = useMemo(() => {
+    const extensions = [".foxe", ".urdf", ".xacro"];
+    for (const source of availableSources) {
+      if (source.supportedFileTypes) {
+        extensions.push(...source.supportedFileTypes);
+      }
+    }
+    return extensions;
+  }, [availableSources]);
 
   const supportsAccountSettings = useContext(ConsoleApiContext) != undefined;
 
@@ -264,16 +273,12 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
     })();
   });
 
-  // previously loaded files are tracked so support the "add bag" feature which loads a second bag
-  // file when the user presses shift during a drag/drop
-  const previousFiles = useRef<File[]>([]);
-
   const { loadFromFile } = useAssets();
 
   const extensionLoader = useExtensionLoader();
 
   const openFiles = useCallback(
-    async (files: FileList, { shiftPressed }: { shiftPressed: boolean }) => {
+    async (files: FileList) => {
       const otherFiles: File[] = [];
       for (const file of files) {
         // electron extends File with a `path` field which is not available in browsers
@@ -305,24 +310,31 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
       }
 
       if (otherFiles.length > 0) {
-        if (shiftPressed) {
-          previousFiles.current = previousFiles.current.concat(otherFiles);
-        } else {
-          previousFiles.current = otherFiles;
+        // Look for a source that supports the dragged file extensions
+        for (const source of availableSources) {
+          const filteredFiles = otherFiles.filter((file) => {
+            const ext = extname(file.name);
+            return source.supportedFileTypes?.includes(ext);
+          });
+
+          // select the first source that has files that match the supported extensions
+          if (filteredFiles.length > 0) {
+            selectSource(source.id, {
+              files: otherFiles,
+            });
+            break;
+          }
         }
-        selectSource("ros1-local-bagfile", {
-          files: previousFiles.current,
-        });
       }
     },
-    [addToast, extensionLoader, loadFromFile, selectSource],
+    [addToast, availableSources, extensionLoader, loadFromFile, selectSource],
   );
 
   // files the main thread told us to open
   const filesToOpen = useElectronFilesToOpen();
   useEffect(() => {
     if (filesToOpen) {
-      void openFiles(filesToOpen, { shiftPressed: false });
+      void openFiles(filesToOpen);
     }
   }, [filesToOpen, openFiles]);
 
@@ -380,8 +392,8 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
   }, [props.deepLinks, selectSource]);
 
   const dropHandler = useCallback(
-    ({ files, shiftPressed }: { files: FileList; shiftPressed: boolean }) => {
-      void openFiles(files, { shiftPressed });
+    ({ files }: { files: FileList }) => {
+      void openFiles(files);
     },
     [openFiles],
   );

packages/studio-base/src/components/PlayerManager.tsx
@@ -219,15 +219,19 @@ export default function PlayerManager(props: PropsWithChildren<PlayerManagerProp
       const supportedFileTypes = foundSource.supportedFileTypes;
       if (supportedFileTypes != undefined) {
         try {
-          const [fileHandle] = await showOpenFilePicker({
-            types: [
-              {
-                description: foundSource.displayName,
-                accept: { "application/octet-stream": supportedFileTypes },
-              },
-            ],
-          });
-          const file = await fileHandle.getFile();
+          let file = (args?.files as File[] | undefined)?.[0];
+
+          if (!file) {
+            const [fileHandle] = await showOpenFilePicker({
+              types: [
+                {
+                  description: foundSource.displayName,
+                  accept: { "application/octet-stream": supportedFileTypes },
+                },
+              ],
+            });
+            file = await fileHandle.getFile();
+          }
 
           const newPlayer = foundSource.initialize({
             file,