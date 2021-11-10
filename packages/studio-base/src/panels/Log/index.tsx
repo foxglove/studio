af6b97aa870557069d12a46b18e352ea4e39b426
Miles Egan <miles@plastaq.com>
2021-11-10 15:43:57 -0500

    Remove unneeded padding at top of log window.

--- packages/studio-base/src/panels/Log/index.tsx
+++ packages/studio-base/src/panels/Log/index.tsx
@@ -102,8 +102,8 @@ const LogPanel = React.memo(({ config, saveConfig }: Props) => {
       <Stack grow>
         <LogList
           items={filteredMessages}
-          renderRow={({ item, style, key, index, ref }) => (
-            <div ref={ref} key={key} style={index === 0 ? { ...style, paddingTop: 36 } : style}>
+          renderRow={({ item, style, key, ref }) => (
+            <div ref={ref} key={key} style={style}>
               <LogMessage msg={item.message} />
             </div>
           )}
