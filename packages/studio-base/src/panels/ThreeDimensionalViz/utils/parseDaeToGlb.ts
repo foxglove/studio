ec8caac45f2431c2690c5065af2e03de3e48a355
John Hurliman <jhurliman@jhurliman.org>
2021-12-16 22:25:37 -0800

    Catch GLTFExporter errors

--- packages/studio-base/src/panels/ThreeDimensionalViz/utils/parseDaeToGlb.ts
+++ packages/studio-base/src/panels/ThreeDimensionalViz/utils/parseDaeToGlb.ts
@@ -2,7 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
+import type { Object3D } from "three";
+import { GLTFExporter, GLTFExporterOptions } from "three/examples/jsm/exporters/GLTFExporter";
 import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader";
 
 import { parseGLB } from "@foxglove/regl-worldview";
@@ -11,6 +12,16 @@ import { MeshPrimitive } from "@foxglove/studio-base/panels/ThreeDimensionalViz/
 
 const UNSIGNED_INT = 5125;
 const DEFAULT_COLOR = [36 / 255, 142 / 255, 255 / 255, 1];
+
+// The type declaration for GLTFExporter.parse is not correct in THREE.js 0.135.0
+type FixedExporter = {
+  parse: (
+    input: Object3D,
+    onCompleted: (gltf: object) => void,
+    onError: (err: Error) => void,
+    options: GLTFExporterOptions,
+  ) => void;
+};
 
 export async function parseDaeToGlb(buffer: ArrayBuffer): Promise<GlbModel> {
   const loader = new ColladaLoader();
@@ -29,14 +40,15 @@ export async function parseDaeToGlb(buffer: ArrayBuffer): Promise<GlbModel> {
   });
 
   const exporter = new GLTFExporter();
-  return await new Promise((resolve) => {
-    exporter.parse(
+  return await new Promise((resolve, reject) => {
+    (exporter as FixedExporter).parse(
       collada.scene,
       async (glbBuffer) => {
         const glb = (await parseGLB(glbBuffer)) as GlbModel;
         patchGlb(glb);
         resolve(glb);
       },
+      reject,
       {
         embedImages: false,
         binary: true,
