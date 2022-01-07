f27b9bd2d3c5850f406b5a7ea26b3017ab815677
Roman Shtylman <roman@foxglove.dev>
2022-01-07 07:38:07 -0800

    Fix transform loading from URDF robot models in 3d panel
    URDF models may contain joints. These joints represent transforms between different frames of the robot. 3D panel is able to add this frame information to the transform tree.
    
    This change fixes update behavior in the 3d panel when loading URDF models with joints. Previously, the UrdfBuilder would add the joint transforms directly to the transform tree. This approach did not inform the react loop to run or for the transform tree to invalidate itself and create a new instance (as required by hook dependencies).
    
    Instead, UrdfBuilder is now an event emitter that emits new transforms. The 3d panel components register event handlers and pass along the urdf transform information back through useTransforms. All other uses of the transform tree should be immutable. Only useTransforms can mutate the transform tree.
    
    Fixes: #2199

--- packages/studio-base/src/panels/ThreeDimensionalViz/TopicTree/types.ts
+++ packages/studio-base/src/panels/ThreeDimensionalViz/TopicTree/types.ts
@@ -13,7 +13,7 @@
 
 import { Color } from "@foxglove/regl-worldview";
 import { TOPIC_DISPLAY_MODES } from "@foxglove/studio-base/panels/ThreeDimensionalViz/TopicTree/constants";
-import { CoordinateFrame } from "@foxglove/studio-base/panels/ThreeDimensionalViz/transforms";
+import { IImmutableTransformTree } from "@foxglove/studio-base/panels/ThreeDimensionalViz/transforms";
 import { Topic } from "@foxglove/studio-base/players/types";
 import { Namespace } from "@foxglove/studio-base/types/Messages";
 
@@ -74,9 +74,7 @@ export type UseSceneBuilderAndTransformsDataInput = {
     errorsByTopic: { [topicName: string]: string[] };
   };
   staticallyAvailableNamespacesByTopic: NamespacesByTopic;
-  transforms: {
-    frames(): ReadonlyMap<string, CoordinateFrame>;
-  };
+  transforms: IImmutableTransformTree;
 };
 
 export type SceneErrorsByKey = {
