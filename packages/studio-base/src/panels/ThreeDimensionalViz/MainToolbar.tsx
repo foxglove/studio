94cfe8019afc8c3aff12c11326283256a53b63eb
Miles Egan <miles@foxglove.dev>
2022-01-25 14:18:09 -0700

    Fix lint errors.

--- packages/studio-base/src/panels/ThreeDimensionalViz/MainToolbar.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/MainToolbar.tsx
@@ -2,7 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Text } from "@fluentui/react";
 import RulerIcon from "@mdi/svg/svg/ruler.svg";
 import Video3dIcon from "@mdi/svg/svg/video-3d.svg";
 import AddLocationIcon from "@mui/icons-material/AddLocation";
@@ -27,7 +26,6 @@ import {
   InteractionStateProps,
   PublishClickType,
 } from "@foxglove/studio-base/panels/ThreeDimensionalViz/InteractionState";
-import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 type Props = InteractionStateProps & {
   debug: boolean;
