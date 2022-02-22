e1b4eb42e9fbbc70e409a90cba034f32ae079939
Andrew Carr <andrew@foxglove.dev>
2022-02-22 13:08:46 +1100

    Fix up missing Stack style

--- packages/studio-base/src/components/SidebarContent.tsx
+++ packages/studio-base/src/components/SidebarContent.tsx
@@ -14,6 +14,7 @@ import { useTooltip } from "@foxglove/studio-base/components/Tooltip";
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     display: "flex",
+    flexDirection: "column",
     flex: "auto",
     gap: theme.spacing(1),
     height: "100%",
