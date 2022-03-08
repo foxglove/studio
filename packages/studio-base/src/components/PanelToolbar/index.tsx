Lichtblick-Suite/lichtblick

commit 5cfa90396ca9b9c6b52594ae1d60c9d07308d467

Show all available annotation topics in the annotations dropdown (#2877)
This change updates the Image panel annotations dropdown to show all available annotation topics. The "related" topics appear first. Those are the ones that are under the same "camera namespace" (i.e same topic prefix). Then the remaining topics after.

This change also switches the Dropdown component to the MUI Select component for the dropdown.

Fixes: #1675

Co-authored-by: Andrew Carr <andrew@foxglove.dev>

packages/studio-base/src/components/PanelToolbar/index.tsx
@@ -58,11 +58,7 @@ const useStyles = makeStyles((theme) => ({
       width: "100%",
       zIndex: 5000,
       backgroundColor: "transparent",
-      pointerEvents: "none",
 
-      "*": {
-        pointerEvents: "auto",
-      },
       "&.hasChildren": {
         left: 0,
         backgroundColor: theme.palette.neutralLighterAlt,

packages/studio-base/src/panels/ImageView/index.tsx
@@ -12,46 +12,28 @@
 //   You may not use this file except in compliance with the License.
 
 import { useTheme } from "@fluentui/react";
-import CheckboxBlankOutlineIcon from "@mdi/svg/svg/checkbox-blank-outline.svg";
-import CheckboxMarkedIcon from "@mdi/svg/svg/checkbox-marked.svg";
-import CloseIcon from "@mdi/svg/svg/close.svg";
-import MenuDownIcon from "@mdi/svg/svg/menu-down.svg";
 import WavesIcon from "@mdi/svg/svg/waves.svg";
 import { Stack, Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import cx from "classnames";
-import { uniq } from "lodash";
 import { useEffect, useState, useMemo, useCallback, useRef } from "react";
 
-import { useShallowMemo } from "@foxglove/hooks";
 import { useDataSourceInfo } from "@foxglove/studio-base/PanelAPI";
-import Autocomplete from "@foxglove/studio-base/components/Autocomplete";
-import Dropdown from "@foxglove/studio-base/components/Dropdown";
-import DropdownItem from "@foxglove/studio-base/components/Dropdown/DropdownItem";
 import Icon from "@foxglove/studio-base/components/Icon";
-import { LegacyButton } from "@foxglove/studio-base/components/LegacyStyledComponents";
-import { Item, SubMenu } from "@foxglove/studio-base/components/Menu";
 import { useMessagePipeline } from "@foxglove/studio-base/components/MessagePipeline";
 import Panel from "@foxglove/studio-base/components/Panel";
 import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
 import inScreenshotTests from "@foxglove/studio-base/stories/inScreenshotTests";
 import { PanelConfigSchema } from "@foxglove/studio-base/types/panels";
 import { mightActuallyBePartial } from "@foxglove/studio-base/util/mightActuallyBePartial";
-import naturalSort from "@foxglove/studio-base/util/naturalSort";
 import { getTopicsByTopicName } from "@foxglove/studio-base/util/selectors";
 import { formatTimeRaw } from "@foxglove/studio-base/util/time";
-import toggle from "@foxglove/studio-base/util/toggle";
 
-import { ImageCanvas, ImageEmptyState, Toolbar, TopicTimestamp } from "./components";
+import { ImageCanvas, ImageEmptyState, Toolbar, TopicDropdown, TopicTimestamp } from "./components";
 import { useCameraInfo, ANNOTATION_DATATYPES, useImagePanelMessages } from "./hooks";
 import helpContent from "./index.help.md";
 import { NORMALIZABLE_IMAGE_DATATYPES } from "./lib/normalizeMessage";
-import {
-  getCameraNamespace,
-  getRelatedMarkerTopics,
-  getMarkerOptions,
-  groupTopics,
-} from "./lib/util";
+import { getRelatedMarkerTopics, getMarkerOptions } from "./lib/util";
 import type { Config, SaveImagePanelConfig, PixelData } from "./types";
 
 type Props = {
@@ -66,10 +48,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     flex: "1 1 auto",
     alignItems: "center",
     overflow: "hidden",
-
-    button: {
-      margin: theme.spacing(0.125, 0.5, 0.125, 0),
-    },
+    gap: theme.spacing(0.5),
   },
   bottomBar: {
     transition: "opacity 0.1s ease-in-out",
@@ -130,63 +109,12 @@ const BottomBar = ({ children }: { children?: React.ReactNode }) => {
   );
 };
 
-const ToggleComponent = ({
-  text,
-  disabled = false,
-  dataTest,
-}: {
-  text: string;
-  disabled?: boolean;
-  dataTest?: string;
-}) => {
-  const classes = useStyles();
-  return (
-    <LegacyButton
-      style={{ maxWidth: "100%", padding: "4px 8px" }}
-      className={cx(classes.toggleButton, { disabled })}
-      data-test={dataTest}
-    >
-      <span className={classes.dropdownTitle}>{text}</span>
-      <Icon style={{ marginLeft: 4 }}>
-        <MenuDownIcon style={{ width: 14, height: 14, opacity: 0.5 }} />
-      </Icon>
-    </LegacyButton>
-  );
-};
-
 const canTransformMarkersByTopic = (topic: string) => !topic.includes("rect");
-
-const AddTopic = ({
-  onSelectTopic,
-  topics,
-}: {
-  onSelectTopic: (arg0: string) => void;
-  topics: string[];
-}) => {
-  return (
-    <div style={{ padding: "8px 12px", height: "31px" }}>
-      <Autocomplete
-        placeholder="Add topic"
-        items={topics}
-        onSelect={onSelectTopic}
-        getItemValue={(s) => String(s)}
-        getItemText={(s) => String(s)}
-      />
-    </div>
-  );
-};
-
-const NO_CUSTOM_OPTIONS: string[] = [];
 
 function ImageView(props: Props) {
   const classes = useStyles();
   const { config, saveConfig } = props;
-  const {
-    cameraTopic,
-    enabledMarkerTopics,
-    transformMarkers,
-    customMarkerTopicOptions = NO_CUSTOM_OPTIONS,
-  } = config;
+  const { cameraTopic, enabledMarkerTopics, transformMarkers } = config;
   const theme = useTheme();
   const { topics } = useDataSourceInfo();
   const cameraTopicFullObject = useMemo(
@@ -195,17 +123,8 @@ function ImageView(props: Props) {
   );
   const [activePixelData, setActivePixelData] = useState<PixelData | undefined>();
 
-  // Namespaces represent marker topics based on the camera topic prefix (e.g. "/camera_front_medium")
-  const { allCameraNamespaces, imageTopicsByNamespace, allImageTopics } = useMemo(() => {
-    const imageTopics = topics.filter(({ datatype }) =>
-      NORMALIZABLE_IMAGE_DATATYPES.includes(datatype),
-    );
-    const topicsByNamespace = groupTopics(imageTopics);
-    return {
-      allImageTopics: imageTopics,
-      imageTopicsByNamespace: topicsByNamespace,
-      allCameraNamespaces: [...topicsByNamespace.keys()],
-    };
+  const allImageTopics = useMemo(() => {
+    return topics.filter(({ datatype }) => NORMALIZABLE_IMAGE_DATATYPES.includes(datatype));
   }, [topics]);
 
   // If no cameraTopic is selected, automatically select the first available image topic
@@ -218,32 +137,22 @@ function ImageView(props: Props) {
     }
   }, [allImageTopics, config, saveConfig]);
 
-  const defaultAvailableMarkerTopics = useMemo(
-    () => getMarkerOptions(cameraTopic, topics, allCameraNamespaces, ANNOTATION_DATATYPES),
-    [cameraTopic, topics, allCameraNamespaces],
+  const relatedAnnotationTopics = useMemo(
+    () => getMarkerOptions(cameraTopic, topics, ANNOTATION_DATATYPES),
+    [cameraTopic, topics],
   );
 
-  const availableAndEnabledMarkerTopics = useShallowMemo(
-    uniq([
-      ...defaultAvailableMarkerTopics,
-      ...customMarkerTopicOptions,
-      ...enabledMarkerTopics,
-    ]).sort(),
-  );
-
-  const onToggleMarkerName = useCallback(
-    (markerTopic: string) => {
-      saveConfig({ enabledMarkerTopics: toggle(enabledMarkerTopics, markerTopic) });
-    },
-    [saveConfig, enabledMarkerTopics],
-  );
+  const allAnnotationTopics = useMemo(() => {
+    return topics
+      .filter((topic) => (ANNOTATION_DATATYPES as readonly string[]).includes(topic.datatype))
+      .map((topic) => topic.name);
+  }, [topics]);
 
   const onChangeCameraTopic = useCallback(
     (newCameraTopic: string) => {
       const newAvailableMarkerTopics = getMarkerOptions(
         newCameraTopic,
         topics,
-        allCameraNamespaces,
         ANNOTATION_DATATYPES,
       );
 
@@ -258,81 +167,31 @@ function ImageView(props: Props) {
         enabledMarkerTopics: newEnabledMarkerTopics,
       });
     },
-    [topics, allCameraNamespaces, enabledMarkerTopics, saveConfig],
+    [topics, enabledMarkerTopics, saveConfig],
   );
   const imageTopicDropdown = useMemo(() => {
-    const cameraNamespace = getCameraNamespace(cameraTopic);
+    const items = allImageTopics.map((topic) => {
+      return {
+        name: topic.name,
+        selected: topic.name === cameraTopic,
+      };
+    });
 
-    if (imageTopicsByNamespace.size === 0) {
-      return (
-        <Dropdown
-          btnClassname={classes.dropdown}
-          toggleComponent={
-            <ToggleComponent
-              dataTest={"topics-dropdown"}
-              text={cameraTopic ? cameraTopic : "No image topics"}
-              disabled
-            />
-          }
-        />
-      );
+    function onChange(newTopics: string[]) {
+      const newTopic = newTopics[0];
+      if (newTopic) {
+        onChangeCameraTopic(newTopic);
+      }
     }
 
-    const items = [...imageTopicsByNamespace.keys()].sort().map((namespace) => {
-      const imageTopics = imageTopicsByNamespace.get(namespace);
-      if (!imageTopics) {
-        return ReactNull;
-      }
+    const title = cameraTopic
+      ? cameraTopic
+      : items.length === 0
+      ? "No camera topics"
+      : "Select a camera topic";
 
-      // If a namespace only contains itself as an entry, just render that item instead of a submenu.
-      if (imageTopics.length === 1 && imageTopics[0]?.name === namespace) {
-        return (
-          <DropdownItem key={namespace} value={namespace}>
-            {namespace}
-          </DropdownItem>
-        );
-      }
-
-      imageTopics.sort(naturalSort("name"));
-
-      return (
-        <SubMenu
-          direction="right"
-          key={namespace}
-          text={namespace}
-          checked={namespace === cameraNamespace}
-          dataTest={namespace.substr(1)}
-        >
-          {imageTopics.map((topic) => {
-            return (
-              <DropdownItem key={topic.name} value={topic.name}>
-                <Item
-                  checked={topic.name === cameraTopic}
-                  onClick={() => onChangeCameraTopic(topic.name)}
-                >
-                  {topic.name}
-                </Item>
-              </DropdownItem>
-            );
-          })}
-        </SubMenu>
-      );
-    });
-    return (
-      <Dropdown
-        toggleComponent={
-          <ToggleComponent
-            dataTest={"topics-dropdown"}
-            text={cameraTopic.length > 0 ? cameraTopic : "Select a topic"}
-          />
-        }
-        value={cameraTopic}
-        onChange={(value) => onChangeCameraTopic(value)}
-      >
-        {items}
-      </Dropdown>
-    );
-  }, [cameraTopic, classes.dropdown, imageTopicsByNamespace, onChangeCameraTopic]);
+    return <TopicDropdown multiple={false} title={title} items={items} onChange={onChange} />;
+  }, [cameraTopic, allImageTopics, onChangeCameraTopic]);
 
   const cameraInfo = useCameraInfo(cameraTopic);
 
@@ -344,87 +203,46 @@ function ImageView(props: Props) {
     synchronize: shouldSynchronize,
   });
 
-  const addTopicsMenu = useMemo(
-    () => (
-      <AddTopic
-        topics={topics
-          .map(({ name }) => name)
-          .filter((topic) => !availableAndEnabledMarkerTopics.includes(topic))}
-        onSelectTopic={(topic) =>
-          saveConfig({
-            enabledMarkerTopics: [...enabledMarkerTopics, topic],
-            customMarkerTopicOptions: [...customMarkerTopicOptions, topic],
-          })
-        }
-      />
-    ),
-    [
-      topics,
-      availableAndEnabledMarkerTopics,
-      saveConfig,
-      enabledMarkerTopics,
-      customMarkerTopicOptions,
-    ],
-  );
-
   const markerDropdown = useMemo(() => {
-    return (
-      <Dropdown
-        dataTest={"markers-dropdown"}
-        closeOnChange={false}
-        onChange={onToggleMarkerName}
-        value={enabledMarkerTopics}
-        text={availableAndEnabledMarkerTopics.length > 0 ? "Markers" : "No markers"}
-        btnClassname={classes.dropdown}
-      >
-        {availableAndEnabledMarkerTopics.map((topic) => (
-          <Item
-            {...{ value: topic }}
-            icon={
-              enabledMarkerTopics.includes(topic) ? (
-                <CheckboxMarkedIcon />
-              ) : (
-                <CheckboxBlankOutlineIcon />
-              )
-            }
-            key={topic}
-            className={classes.dropdownItem}
-          >
-            <span style={{ display: "inline-block", marginRight: "15px" }}>{topic}</span>
-            {customMarkerTopicOptions.includes(topic) && (
-              <Icon
-                style={{ position: "absolute", right: "10px" }}
-                onClick={() =>
-                  saveConfig({
-                    enabledMarkerTopics: enabledMarkerTopics.filter(
-                      (topicOption) => topicOption !== topic,
-                    ),
-                    customMarkerTopicOptions: customMarkerTopicOptions.filter(
-                      (topicOption) => topicOption !== topic,
-                    ),
-                  })
-                }
-              >
-                <CloseIcon />
-              </Icon>
-            )}
-          </Item>
-        ))}
-        {addTopicsMenu}
-      </Dropdown>
-    );
-  }, [
-    addTopicsMenu,
-    availableAndEnabledMarkerTopics,
-    classes.dropdown,
-    classes.dropdownItem,
-    customMarkerTopicOptions,
-    enabledMarkerTopics,
-    onToggleMarkerName,
-    saveConfig,
-  ]);
+    const allSet = new Set(allAnnotationTopics);
+    const enabledAnnotationTopics = new Set(enabledMarkerTopics);
 
-  //const imageMessage = messagesByTopic[cameraTopic]?.[0];
+    const dropdownTopics = [];
+
+    // Related topics come first since those are more likely to be what the user wants to show
+    for (const topic of relatedAnnotationTopics) {
+      dropdownTopics.push({
+        name: topic,
+        selected: enabledAnnotationTopics.has(topic),
+      });
+
+      allSet.delete(topic);
+    }
+
+    // Then add all the other available annotation topics
+    for (const topic of allSet) {
+      dropdownTopics.push({
+        name: topic,
+        selected: enabledAnnotationTopics.has(topic),
+      });
+    }
+
+    function onChange(activeTopics: string[]) {
+      saveConfig({
+        enabledMarkerTopics: activeTopics,
+      });
+    }
+
+    return (
+      <TopicDropdown
+        multiple={true}
+        title="Annotations"
+        items={dropdownTopics}
+        onChange={onChange}
+      />
+    );
+  }, [allAnnotationTopics, enabledMarkerTopics, relatedAnnotationTopics, saveConfig]);
+
   const lastImageMessageRef = useRef(image);
   if (image) {
     lastImageMessageRef.current = image;
@@ -539,7 +357,6 @@ function ImageView(props: Props) {
 
 const defaultConfig: Config = {
   cameraTopic: "",
-  customMarkerTopicOptions: [],
   enabledMarkerTopics: [],
   mode: "fit",
   pan: { x: 0, y: 0 },

packages/studio-base/src/panels/ImageView/components/TopicDropdown.stories.tsx
@@ -1,0 +1,58 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { Box } from "@mui/material";
+import { action } from "@storybook/addon-actions";
+import { Story } from "@storybook/react";
+
+import { TopicDropdown } from "./TopicDropdown";
+
+export default {
+  title: "panels/ImageView/TopicDropdown",
+  component: TopicDropdown,
+};
+
+export const NoTopics: Story = (_args) => {
+  return (
+    <Box padding={2}>
+      <TopicDropdown open multiple={false} title="Title" items={[]} onChange={action("onChange")} />
+    </Box>
+  );
+};
+
+export const OneTopic: Story = (_args) => {
+  return (
+    <Box padding={2}>
+      <TopicDropdown
+        open
+        multiple={false}
+        title="Title"
+        items={[
+          { name: "/foobar", selected: false },
+          { name: "/another", selected: true },
+          { name: "/final", selected: false },
+        ]}
+        onChange={action("onChange")}
+      />
+    </Box>
+  );
+};
+
+export const MultipleTopic: Story = (_args) => {
+  return (
+    <Box padding={2}>
+      <TopicDropdown
+        open
+        multiple={true}
+        title="Title"
+        items={[
+          { name: "/foobar", selected: false },
+          { name: "/another", selected: true },
+          { name: "/final", selected: false },
+        ]}
+        onChange={action("onChange")}
+      />
+    </Box>
+  );
+};

packages/studio-base/src/panels/ImageView/components/TopicDropdown.tsx
@@ -1,0 +1,88 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {
+  MenuItem,
+  Select,
+  ListItemText,
+  SelectChangeEvent,
+  Radio,
+  ListSubheader,
+  SelectProps,
+} from "@mui/material";
+import { useMemo } from "react";
+
+type TopicDropdownItem = {
+  name: string;
+  selected: boolean;
+};
+
+type Props = {
+  title: string;
+  items: TopicDropdownItem[];
+  multiple: boolean;
+  size?: SelectProps["size"];
+  open?: boolean;
+
+  onChange: (activeTopics: string[]) => void;
+};
+
+export function TopicDropdown(props: Props): JSX.Element {
+  const { items, onChange, multiple, title, size = "small" } = props;
+
+  const selectedTopics = useMemo<string[]>(() => {
+    return items.filter((item) => item.selected).map((item) => item.name);
+  }, [items]);
+
+  const handleChange = (event: SelectChangeEvent<string[]>) => {
+    const {
+      target: { value },
+    } = event;
+    onChange(typeof value === "string" ? [value] : value);
+  };
+
+  return (
+    <>
+      <Select
+        value={selectedTopics}
+        disabled={items.length === 0}
+        displayEmpty
+        renderValue={(_selected) => title}
+        title={title}
+        size={size}
+        onChange={handleChange}
+        multiple={multiple}
+        open={props.open}
+        MenuProps={{
+          contentEditable: multiple,
+          MenuListProps: {
+            dense: true,
+            subheader: multiple ? <ListSubheader>Select multiple topics</ListSubheader> : undefined,
+          },
+        }}
+      >
+        {items.length === 0 && (
+          <MenuItem disabled value="">
+            <ListItemText
+              primary="No topics"
+              primaryTypographyProps={{ variant: "inherit", color: "text.secondary" }}
+            />
+          </MenuItem>
+        )}
+        {items.map((item) => (
+          <MenuItem key={item.name} value={item.name}>
+            <Radio
+              checked={selectedTopics.includes(item.name)}
+              size="small"
+              edge="start"
+              checkedIcon={<CheckCircleIcon />}
+            />
+            <ListItemText primary={item.name} primaryTypographyProps={{ variant: "inherit" }} />
+          </MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+}

packages/studio-base/src/panels/ImageView/components/index.ts
@@ -5,4 +5,5 @@
 export * from "./ImageCanvas";
 export * from "./ImageEmptyState";
 export * from "./Toolbar";
+export * from "./TopicDropdown";
 export * from "./TopicTimestamp";

packages/studio-base/src/panels/ImageView/hooks/index.ts
@@ -2,5 +2,5 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-export { useCameraInfo } from "./useCameraInfo";
+export * from "./useCameraInfo";
 export { useImagePanelMessages, ANNOTATION_DATATYPES } from "./useImagePanelMessages";

packages/studio-base/src/panels/ImageView/index.stories.tsx
@@ -59,76 +59,6 @@ export const TopicButNoDataSourceHoveredLight = Object.assign(
   { parameters: { colorScheme: "light" } },
 );
 
-function AvailableTopicsStory({
-  cameraTopic,
-  openMarkersMenu = false,
-}: {
-  cameraTopic: string;
-  openMarkersMenu?: boolean;
-}): React.ReactElement {
-  const onMount = useHoverOnPanel(() => {
-    const button = document.querySelector(
-      openMarkersMenu ? "[data-test~='markers-dropdown']" : "[data-test~='topics-dropdown']",
-    );
-    if (!button) {
-      throw new Error("missing mouse event target");
-    }
-    TestUtils.Simulate.click(button);
-  });
-  return (
-    <PanelSetup
-      onMount={onMount}
-      fixture={{
-        topics: [
-          { name: "/foo_image", datatype: "sensor_msgs/Image" },
-          { name: "/bar_image/compressed", datatype: "sensor_msgs/Image" },
-          { name: "/baz_image/compressed", datatype: "sensor_msgs/Image" },
-          { name: "/baz_image/image_rect_color", datatype: "sensor_msgs/Image" },
-          { name: "/baz_image/markers", datatype: "visualization_msgs/ImageMarker" },
-        ],
-        frame: {},
-      }}
-    >
-      <ImageView overrideConfig={{ ...ImageView.defaultConfig, cameraTopic }} />
-    </PanelSetup>
-  );
-}
-
-export const AvailableTopicsNoneSelected = (): React.ReactElement => (
-  <AvailableTopicsStory cameraTopic="" />
-);
-AvailableTopicsNoneSelected.parameters = { colorScheme: "dark" };
-export const AvailableTopicsNoneSelectedLight = Object.assign(
-  AvailableTopicsNoneSelected.bind(undefined),
-  { parameters: { colorScheme: "light" } },
-);
-
-export const AvailableTopicsChildSelected = (): React.ReactElement => (
-  <AvailableTopicsStory cameraTopic="/foo_image" />
-);
-AvailableTopicsChildSelected.parameters = { colorScheme: "dark" };
-export const AvailableTopicsChildSelectedLight = Object.assign(
-  AvailableTopicsChildSelected.bind(undefined),
-  { parameters: { colorScheme: "light" } },
-);
-
-export const AvailableTopicsDescendentSelected = (): React.ReactElement => (
-  <AvailableTopicsStory cameraTopic="/baz_image/compressed" />
-);
-AvailableTopicsDescendentSelected.parameters = { colorScheme: "dark" };
-export const AvailableTopicsDescendentSelectedLight = Object.assign(
-  AvailableTopicsDescendentSelected.bind(undefined),
-  { parameters: { colorScheme: "light" } },
-);
-
-export const AvailableTopicsMarkers = (): React.ReactElement => (
-  <AvailableTopicsStory openMarkersMenu cameraTopic="/baz_image/compressed" />
-);
-AvailableTopicsMarkers.parameters = { colorScheme: "dark" };
-export const AvailableTopicsMarkersLight = Object.assign(AvailableTopicsMarkers.bind(undefined), {
-  parameters: { colorScheme: "light" },
-});
-
 export function Settings(): JSX.Element {
   return (
     <SchemaEditor

packages/studio-base/src/panels/ImageView/lib/util.test.ts
@@ -18,7 +18,6 @@ import {
   getCameraInfoTopic,
   getMarkerOptions,
   getRelatedMarkerTopics,
-  groupTopics,
   buildMarkerData,
   getCameraNamespace,
 } from "./util";
@@ -51,15 +50,12 @@ describe("ImageView", () => {
       { name: "/camera_rear_medium/marker4", datatype: "vision_msgs/ImageMarker" }, // not included because it's for a different camera
       { name: "/unknown_camera/marker5", datatype: "vision_msgs/ImageMarker" },
     ];
-    const allCameraNamespaces = ["/some_camera_topic", "/camera_rear_medium"];
     it("filters and sorts topics relevant to this camera", () => {
       expect(
-        getMarkerOptions(
-          "/some_camera_topic/image_rect_color",
-          allMarkerTopics,
-          allCameraNamespaces,
-          ["visualization_msgs/ImageMarker", "vision_msgs/ImageMarker"],
-        ),
+        getMarkerOptions("/some_camera_topic/image_rect_color", allMarkerTopics, [
+          "visualization_msgs/ImageMarker",
+          "vision_msgs/ImageMarker",
+        ]),
       ).toEqual(["/some_camera_topic/marker1", "/some_camera_topic/marker2"]);
     });
   });
@@ -102,64 +98,6 @@ describe("ImageView", () => {
     });
     it("Returns undefined when encountering a single level topic", () => {
       expect(getCameraNamespace("/camera_back_left")).toEqual(undefined);
-    });
-  });
-
-  describe("groupTopics", () => {
-    const topic = (name: string) => ({ name, datatype: "dummy" });
-
-    it("groups by camera name", () => {
-      expect(
-        groupTopics([
-          topic("/camera_1/foo"),
-          topic("/camera_2/foo"),
-          topic("/camera_1/bar"),
-          topic("/weird_topic"),
-        ]),
-      ).toEqual(
-        new Map([
-          ["/camera_1", [topic("/camera_1/foo"), topic("/camera_1/bar")]],
-          ["/camera_2", [topic("/camera_2/foo")]],
-          ["/weird_topic", [topic("/weird_topic")]],
-        ]),
-      );
-    });
-
-    it("puts /old topics under the correct camera", () => {
-      expect(
-        groupTopics([
-          topic("/camera_1/foo"),
-          topic("/old/camera_2/foo"),
-          topic("/old/camera_1/bar"),
-          topic("/camera_2/old/foo"),
-        ]),
-      ).toEqual(
-        new Map([
-          ["/camera_1", [topic("/camera_1/foo"), topic("/old/camera_1/bar")]],
-          ["/camera_2", [topic("/old/camera_2/foo"), topic("/camera_2/old/foo")]],
-        ]),
-      );
-    });
-
-    it("Separates /studio_source_2 topics", () => {
-      expect(
-        groupTopics([
-          topic("/camera_1/foo"),
-          topic("/camera_1/bar"),
-          topic("/studio_source_2/camera_1/foo"),
-          topic("/studio_source_2/camera_1/bar"),
-          topic("/studio_source_2/camera_2/foo"),
-        ]),
-      ).toEqual(
-        new Map([
-          ["/camera_1", [topic("/camera_1/foo"), topic("/camera_1/bar")]],
-          [
-            "/studio_source_2/camera_1",
-            [topic("/studio_source_2/camera_1/foo"), topic("/studio_source_2/camera_1/bar")],
-          ],
-          ["/studio_source_2/camera_2", [topic("/studio_source_2/camera_2/foo")]],
-        ]),
-      );
     });
   });
 

foxglove/studio

commit 6579ad219a31a842927c7b9efafe47e6d811bbb3

Fixes clicking in the upper area of a panel
When using a floating toolbar, the hidden toolbar would prevent clicking the upper area of a panel. This regressed in https://github.com/foxglove/studio/pull/2877. This change fixes the bug by setting the width of the toolbar to 100% for toolbars with children and no set width for no children. This lets the panel toolbar container size to the controls rather than across the entire panel.

Fixes: #2987

packages/studio-base/src/components/PanelToolbar/index.tsx
@@ -55,11 +55,14 @@ const useStyles = makeStyles((theme) => ({
       // leave some room for possible scrollbar
       paddingRight: 8,
       top: 0,
-      width: "100%",
       zIndex: 5000,
       backgroundColor: "transparent",
 
       "&.hasChildren": {
+        // If the toolbar has children - set the width to 100% to takeup the entire panel width
+        // If the toolbar does not have children, then the width should be only the controls
+        // so the div does not interfere with other panel elements.
+        width: "100%",
         left: 0,
         backgroundColor: theme.palette.neutralLighterAlt,
       },