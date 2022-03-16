6e41630630b0f038f7b3e1987b763097cd49f980
Miles Egan <miles@foxglove.dev>
2022-03-16 09:07:39 -0600

    Don't update url unless we have a stable player

--- packages/studio-base/src/hooks/useStateToURLSynchronization.ts
+++ packages/studio-base/src/hooks/useStateToURLSynchronization.ts
@@ -15,7 +15,7 @@ import {
 } from "@foxglove/studio-base/context/CurrentLayoutContext";
 import { usePlayerSelection } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import useDeepMemo from "@foxglove/studio-base/hooks/useDeepMemo";
-import { PlayerCapabilities } from "@foxglove/studio-base/players/types";
+import { PlayerCapabilities, PlayerPresence } from "@foxglove/studio-base/players/types";
 import {
   AppURLState,
   encodeAppURLState,
@@ -28,6 +28,7 @@ const selectCanSeek = (ctx: MessagePipelineContext) =>
 const selectCurrentTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.currentTime;
 const selectUrlState = (ctx: MessagePipelineContext) => ctx.playerState.urlState;
 const selectLayoutId = (layoutState: LayoutState) => layoutState.selectedLayout?.id;
+const selectPlayerPresence = (ctx: MessagePipelineContext) => ctx.playerState.presence;
 
 /**
  * Syncs our current player, layout and other state with the URL in the address bar.
@@ -35,10 +36,13 @@ const selectLayoutId = (layoutState: LayoutState) => layoutState.selectedLayout?
 export function useStateToURLSynchronization(): void {
   const canSeek = useMessagePipeline(selectCanSeek);
   const currentTime = useMessagePipeline(selectCurrentTime);
-  const urlState = useMessagePipeline(selectUrlState);
+  const playerUrlState = useMessagePipeline(selectUrlState);
   const layoutId = useCurrentLayoutSelector(selectLayoutId);
-  const stableUrlState = useDeepMemo(urlState);
+  const stablePlayerUrlState = useDeepMemo(playerUrlState);
+  const playerPresence = useMessagePipeline(selectPlayerPresence);
   const { selectedSource } = usePlayerSelection();
+
+  const stablePlayerRef = useRef(false);
 
   // unsavedAppStateRef contains the app state that will be saved to the url
   // It starts out as the current url state values
@@ -67,14 +71,17 @@ export function useStateToURLSynchronization(): void {
     maxWait: 500,
   });
 
-  // During startup, many of the values (selectedSource, layoutId, etc) start out undefined.
-  // We don't want their initial undefined state to clear the url state only to add it back
-  // immediately. Conversely, if a value goes from being defined to undefined, we want to clear
-  // the url state value.
-  //
-  // referenceAppStateRef lets us accomplish this. We start with an empty state and as the values
-  // differ from our reference values, we update the unsavedAppState
-  const referenceAppStateRef = useRef<AppURLState>({});
+  // Mark our player state as unstable when a new source is selected.
+  useEffect(() => {
+    stablePlayerRef.current = false;
+  }, [selectedSource]);
+
+  // Wait until the player is present to switch player state back to stable.
+  useEffect(() => {
+    if (playerPresence === PlayerPresence.PRESENT) {
+      stablePlayerRef.current = true;
+    }
+  }, [playerPresence]);
 
   useEffect(() => {
     // Electron has its own concept of what the app URL is. If we want to do anything
@@ -84,32 +91,25 @@ export function useStateToURLSynchronization(): void {
       return;
     }
 
+    // Don't update url unless we have a stable player state.
+    if (!stablePlayerRef.current) {
+      return;
+    }
+
     const unsavedAppState = getUnsavedAppState();
-
-    if (referenceAppStateRef.current.layoutId !== layoutId) {
-      unsavedAppState.layoutId = layoutId;
-    }
-
-    if (referenceAppStateRef.current.ds !== selectedSource?.id) {
-      unsavedAppState.ds = selectedSource?.id;
-    }
-
-    if (referenceAppStateRef.current.dsParams !== stableUrlState) {
-      unsavedAppState.dsParams = stableUrlState;
-    }
-
-    const time = canSeek ? currentTime : undefined;
-    if (referenceAppStateRef.current.time !== time) {
-      unsavedAppState.time = time;
-    }
-
-    referenceAppStateRef.current = {
-      layoutId,
-      ds: selectedSource?.id,
-      dsParams: stableUrlState,
-      time,
-    };
+    unsavedAppState.layoutId = layoutId;
+    unsavedAppState.ds = selectedSource?.id;
+    unsavedAppState.dsParams = stablePlayerUrlState;
+    unsavedAppState.time = canSeek ? currentTime : undefined;
 
     queueUpdateUrl();
-  }, [canSeek, currentTime, layoutId, queueUpdateUrl, selectedSource, stableUrlState]);
+  }, [
+    canSeek,
+    currentTime,
+    layoutId,
+    playerPresence,
+    queueUpdateUrl,
+    selectedSource,
+    stablePlayerUrlState,
+  ]);
 }
