3c8e63130f12f0d8cb56e990f8c93b761fc5fcfd
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-03-16 11:33:44 -0700

    unused import

--- packages/studio-base/src/panels/ThreeDimensionalViz/commands/PoseMarkers.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/commands/PoseMarkers.tsx
@@ -10,7 +10,7 @@
 //   This source code is licensed under the Apache License, Version 2.0,
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
-import { mat4, quat, vec3 } from "gl-matrix";
+import { mat4, vec3 } from "gl-matrix";
 import { memo, ReactElement } from "react";
 
 import {
