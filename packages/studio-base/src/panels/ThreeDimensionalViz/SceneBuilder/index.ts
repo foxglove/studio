Lichtblick-Suite/lichtblick

commit f389e84e797a35b9026ef62ec06c63d61c93cc37

Add frame lock option for OccupancyGrid topics (#3024)
**User-Facing Changes**
Add a new topic settings modal for OccupancyGrid topics. The modal introduces a `Frame lock` option. When the `Frame lock` option is enabled, occupancy grids are placed within the 3d scene using transformations from the _current time_ rather than the `header.stamp` time as is the default behavior.

**Description**
Adding a frame lock option to occupancy grid topics allows users who publish occupancy grids as static maps to publish their message infrequently while still having the grid display correctly relative to other visual elements as new transforms are received.

Fixes: #3018

Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

packages/studio-base/src/panels/ThreeDimensionalViz/SceneBuilder/index.ts
@@ -584,6 +584,8 @@ export default class SceneBuilder implements MarkerProvider {
     const type = 101;
     const name = `${topic}/${type}`;
 
+    const { frameLocked } = this._settingsByKey[`t:${topic}`] as { frameLocked?: boolean };
+
     const { header, info, data } = message;
     if (info.width * info.height !== data.length) {
       this._setTopicError(
@@ -609,7 +611,7 @@ export default class SceneBuilder implements MarkerProvider {
       type,
       name,
       pose: clonePose(info.origin),
-      frame_locked: false,
+      frame_locked: frameLocked ?? false,
       interactionData: { topic, originalMessage: message },
     };
 

packages/studio-base/src/panels/ThreeDimensionalViz/TopicSettingsEditor/OccupancyGridSettingsEditor.stories.tsx
@@ -1,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { ReactElement } from "react";
+
+import OccupancyGridSettingsEditor from "./OccupancyGridSettingsEditor";
+
+export default {
+  title: "panels/ThreeDimensionalViz/TopicSettingsEditor/OccupancyGridSettingsEditor",
+  component: OccupancyGridSettingsEditor,
+};
+
+export function Default(): ReactElement {
+  return (
+    <OccupancyGridSettingsEditor
+      onFieldChange={() => undefined}
+      onSettingsChange={() => undefined}
+      settings={{}}
+    />
+  );
+}

packages/studio-base/src/panels/ThreeDimensionalViz/TopicSettingsEditor/OccupancyGridSettingsEditor.tsx
@@ -1,0 +1,47 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright 2019-2021 Cruise LLC
+//
+//   This source code is licensed under the Apache License, Version 2.0,
+//   found at http://www.apache.org/licenses/LICENSE-2.0
+//   You may not use this file except in compliance with the License.
+
+import { Checkbox, FormControlLabel, Stack } from "@mui/material";
+
+import { Marker, MarkerArray } from "@foxglove/studio-base/types/Messages";
+
+import { TopicSettingsEditorProps } from ".";
+import { SDescription } from "./common";
+
+type GenericSettings = {
+  frameLocked?: boolean;
+};
+
+export default function OccupancyGridSettingsEditor(
+  props: TopicSettingsEditorProps<Marker | MarkerArray, GenericSettings>,
+): JSX.Element {
+  const { settings = {}, onFieldChange } = props;
+  return (
+    <Stack flex="auto">
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={settings.frameLocked === true}
+            onChange={(_ev, checked) => onFieldChange("frameLocked", checked)}
+          />
+        }
+        label="Frame lock"
+      />
+      <SDescription>
+        When disabled, the grid will be positioned in the 3D scene by transforming it using its
+        <code>header.stamp</code> time. When enabled, the grid will be “locked” to the current
+        position of its <code>header.frame_id</code> and will move when the frame moves.
+      </SDescription>
+    </Stack>
+  );
+}

packages/studio-base/src/panels/ThreeDimensionalViz/TopicSettingsEditor/index.tsx
@@ -19,6 +19,7 @@ import { TopicSettingsEditorProps } from "@foxglove/studio-base/panels/ThreeDime
 import { FOXGLOVE_GRID_DATATYPE, URDF_DATATYPE } from "@foxglove/studio-base/util/globalConstants";
 
 import MarkerSettingsEditor from "./MarkerSettingsEditor";
+import OccupancyGridSettingsEditor from "./OccupancyGridSettingsEditor";
 import PointCloudSettingsEditor from "./PointCloudSettingsEditor";
 import PoseSettingsEditor from "./PoseSettingsEditor";
 
@@ -56,6 +57,9 @@ export function topicSettingsEditorForDatatype(datatype: string):
     ["nav_msgs/Path", MarkerSettingsEditor],
     ["nav_msgs/msg/Path", MarkerSettingsEditor],
     ["ros.nav_msgs.Path", MarkerSettingsEditor],
+    ["nav_msgs/OccupancyGrid", OccupancyGridSettingsEditor],
+    ["nav_msgs/msg/OccupancyGrid", OccupancyGridSettingsEditor],
+    ["ros.nav_msgs.OccupancyGrid", OccupancyGridSettingsEditor],
   ]);
 
   return editors.get(datatype) as

foxglove/studio

commit 07d1839e0966103f228e355718f7664f2f410500

Fix destructing an undefined value for occupancy grids
Bug introduced in https://github.com/foxglove/studio/pull/3024

packages/studio-base/src/panels/ThreeDimensionalViz/SceneBuilder/index.ts
@@ -584,7 +584,7 @@ export default class SceneBuilder implements MarkerProvider {
     const type = 101;
     const name = `${topic}/${type}`;
 
-    const { frameLocked } = this._settingsByKey[`t:${topic}`] as { frameLocked?: boolean };
+    const { frameLocked } = (this._settingsByKey[`t:${topic}`] ?? {}) as { frameLocked?: boolean };
 
     const { header, info, data } = message;
     if (info.width * info.height !== data.length) {