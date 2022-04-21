f31b127ef53a5d73a88b0d3ee3d0fc68eb8cdeb5
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-04-20 19:59:25 -0500

    cleanup

--- web/src/services/IdbLayoutStorage.ts
+++ web/src/services/IdbLayoutStorage.ts
@@ -2,7 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-// import * as IdbKeyval from "idb-keyval";
 import * as IDB from "idb/with-async-ittr";
 
 import Log from "@foxglove/log";
