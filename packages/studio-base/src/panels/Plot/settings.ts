b4cb07e550c1eef664b90b8d0369b3e2205a5a15
Miles Egan <miles@foxglove.dev>
2022-04-25 06:54:03 -0500

    Convert plot panel settings to new sidebar

---
+++ packages/studio-base/src/panels/Plot/settings.ts
@@ -0,0 +1,75 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { SettingsTreeNode } from "@foxglove/studio-base/components/SettingsTreeEditor/types";
+
+import { PlotConfig } from "./types";
+
+export function buildSettingsTree(config: PlotConfig): SettingsTreeNode {
+  /*
+    const defaultConfig: PlotConfig = {
+    title: undefined,
+    paths: [{ value: "", enabled: true, timestampMethod: "receiveTime" }],
+    minYValue: "",
+    maxYValue: "",
+    showXAxisLabels: true,
+    showYAxisLabels: true,
+    showLegend: true,
+    legendDisplay: "floating",
+    showPlotValuesInLegend: false,
+    isSynced: true,
+    xAxisVal: "timestamp",
+    sidebarDimension: defaultSidebarDimension,
+  };
+    */
+  return {
+    fields: {
+      title: { label: "Title", input: "string", value: config.title },
+      isSynced: { label: "Sync with other plots", input: "boolean", value: config.isSynced },
+      legendDisplay: {
+        label: "Legend position",
+        input: "select",
+        value: config.legendDisplay,
+        options: [
+          { value: "floating", label: "Floating" },
+          { value: "left", label: "Left" },
+          { value: "top", label: "Top" },
+        ],
+      },
+      showPlotValuesInLegend: {
+        label: "Show plot values in legend",
+        input: "boolean",
+        value: config.showPlotValuesInLegend,
+      },
+      showXAxisLabels: {
+        label: "Show X axis labels",
+        input: "boolean",
+        value: config.showXAxisLabels,
+      },
+      showYAxisLabels: {
+        label: "Show Y axis labels",
+        input: "boolean",
+        value: config.showYAxisLabels,
+      },
+      minYValue: {
+        label: "Y min",
+        input: "number",
+        value: Number(config.minYValue),
+        placeholder: "auto",
+      },
+      maxYValue: {
+        label: "Y max",
+        input: "number",
+        value: Number(config.maxYValue),
+        placeholder: "auto",
+      },
+      followingViewWidth: {
+        label: "X range in seconds (for timestamp plots only)",
+        input: "number",
+        placeholder: "auto",
+        value: config.followingViewWidth,
+      },
+    },
+  };
+}
