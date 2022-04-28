83e32d4682e46b6ff1ca2e41ceaa295c8949dc47
Miles Egan <miles@foxglove.dev>
2022-04-28 10:39:18 -0500

    Memoize topic builder

--- packages/studio-base/src/panels/ThreeDeeRender/settings.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/settings.ts
@@ -2,6 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { memoize } from "lodash";
+
 import { CameraState, DEFAULT_CAMERA_STATE } from "@foxglove/regl-worldview";
 import { Topic } from "@foxglove/studio";
 import {
@@ -74,6 +76,107 @@ export type SettingsTreeOptions = {
   pclFieldsByTopic: Map<string, string[]>;
 };
 
+function buildTopicNode(
+  topicConfigOrTopicName: unknown,
+  topic: Topic,
+  pclFieldsByTopic: Map<string, string[]>,
+): undefined | SettingsTreeNode {
+  const { datatype } = topic;
+  if (
+    !SUPPORTED_DATATYPES.has(datatype) ||
+    TF_DATATYPES.has(datatype) ||
+    TF_DATATYPES.has(datatype)
+  ) {
+    return;
+  }
+
+  type SettingsTreeNodeWithFields = SettingsTreeNode & { fields: SettingsTreeFields };
+  const node: SettingsTreeNodeWithFields = { label: topic.name, fields: {} };
+  const topicConfig = typeof topicConfigOrTopicName === "string" ? {} : topicConfigOrTopicName;
+
+  if (MARKER_DATATYPES.has(datatype) || MARKER_ARRAY_DATATYPES.has(datatype)) {
+    const cur = topicConfig as Partial<TopicSettingsMarker> | undefined;
+    const color = cur?.color;
+    node.fields.color = { label: "Color", input: "color", value: color };
+  } else if (OCCUPANCY_GRID_DATATYPES.has(datatype)) {
+    const cur = topicConfig as Partial<TopicSettingsOccupancyGrid> | undefined;
+    const frameLock = cur?.frameLock ?? false;
+    node.fields.frameLock = { label: "Frame lock", input: "boolean", value: frameLock };
+  } else if (POINTCLOUD_DATATYPES.has(datatype)) {
+    const cur = topicConfig as Partial<TopicSettingsPointCloud2> | undefined;
+    const pclFields = pclFieldsByTopic.get(topic.name) ?? POINTCLOUD_REQUIRED_FIELDS;
+    const pointSize = cur?.pointSize;
+    const pointShape = cur?.pointShape ?? "circle";
+    const decayTime = cur?.decayTime;
+    const colorBy = cur?.colorBy ?? bestColorByField(pclFields);
+    const colorByOptions = pclFields.map((field) => ({ label: field, value: field }));
+    const flatColor = cur?.flatColor ?? "#ffffff";
+    const valueMin = cur?.valueMin;
+    const valueMax = cur?.valueMax;
+    const colorMap = cur?.colorMap ?? "turbo";
+    // const gradient = cur?.gradient;
+
+    node.fields.pointSize = {
+      label: "Point size",
+      input: "number",
+      value: pointSize,
+      placeholder: "2",
+    };
+    node.fields.pointShape = {
+      label: "Point shape",
+      input: "select",
+      options: POINT_SHAPE_OPTIONS,
+      value: pointShape,
+    };
+    node.fields.decayTime = {
+      label: "Decay time",
+      input: "number",
+      value: decayTime,
+      step: 0.5,
+      placeholder: "0 seconds",
+    };
+    node.fields.colorBy = {
+      label: "Color by",
+      input: "select",
+      options: [{ label: "None", value: "none" }].concat(colorByOptions),
+      value: colorBy,
+    };
+    if (!colorBy || colorBy === "none") {
+      node.fields.flatColor = { label: "Flat color", input: "color", value: flatColor };
+    } else {
+      node.fields.valueMin = {
+        label: "Value min",
+        input: "number",
+        value: valueMin,
+        placeholder: "auto",
+      };
+      node.fields.valueMax = {
+        label: "Value max",
+        input: "number",
+        value: valueMax,
+        placeholder: "auto",
+      };
+      node.fields.colorMap = {
+        label: "Color map",
+        input: "select",
+        options: [
+          { label: "Turbo", value: "turbo" },
+          { label: "Rainbow", value: "rainbow" },
+          { label: "Gradient", value: "gradient" },
+        ],
+        value: colorMap,
+      };
+      if (colorMap === "gradient") {
+        // node.fields.gradient = { label: "Gradient", input: "gradient", value: gradient };
+      }
+    }
+  }
+
+  return node;
+}
+
+const memoBuildTopicNode = memoize(buildTopicNode);
+
 export function buildSettingsTree(options: SettingsTreeOptions): SettingsTreeNode {
   const { config, coordinateFrames, followTf, topics, pclFieldsByTopic } = options;
   const { cameraState } = config;
@@ -82,98 +185,13 @@ export function buildSettingsTree(options: SettingsTreeOptions): SettingsTreeNod
 
   const sortedTopics = sorted(topics, (a, b) => a.name.localeCompare(b.name));
   for (const topic of sortedTopics) {
-    const { datatype } = topic;
-    if (
-      !SUPPORTED_DATATYPES.has(datatype) ||
-      TF_DATATYPES.has(datatype) ||
-      TF_DATATYPES.has(datatype)
-    ) {
-      continue;
+    // We key our memoized function by the first argument. Since the config
+    // maybe be undefined we use the config or the topic name.
+    const topicConfig = config.topics[topic.name] ?? topic.name;
+    const newNode = memoBuildTopicNode(topicConfig, topic, pclFieldsByTopic);
+    if (newNode) {
+      topicsChildren[topic.name] = newNode;
     }
-
-    type SettingsTreeNodeWithFields = SettingsTreeNode & { fields: SettingsTreeFields };
-    const node: SettingsTreeNodeWithFields = { label: topic.name, fields: {} };
-    const topicConfig = config.topics[topic.name];
-
-    if (MARKER_DATATYPES.has(datatype) || MARKER_ARRAY_DATATYPES.has(datatype)) {
-      const cur = topicConfig as Partial<TopicSettingsMarker> | undefined;
-      const color = cur?.color;
-      node.fields.color = { label: "Color", input: "color", value: color };
-    } else if (OCCUPANCY_GRID_DATATYPES.has(datatype)) {
-      const cur = topicConfig as Partial<TopicSettingsOccupancyGrid> | undefined;
-      const frameLock = cur?.frameLock ?? false;
-      node.fields.frameLock = { label: "Frame lock", input: "boolean", value: frameLock };
-    } else if (POINTCLOUD_DATATYPES.has(datatype)) {
-      const cur = topicConfig as Partial<TopicSettingsPointCloud2> | undefined;
-      const pclFields = pclFieldsByTopic.get(topic.name) ?? POINTCLOUD_REQUIRED_FIELDS;
-      const pointSize = cur?.pointSize;
-      const pointShape = cur?.pointShape ?? "circle";
-      const decayTime = cur?.decayTime;
-      const colorBy = cur?.colorBy ?? bestColorByField(pclFields);
-      const colorByOptions = pclFields.map((field) => ({ label: field, value: field }));
-      const flatColor = cur?.flatColor ?? "#ffffff";
-      const valueMin = cur?.valueMin;
-      const valueMax = cur?.valueMax;
-      const colorMap = cur?.colorMap ?? "turbo";
-      // const gradient = cur?.gradient;
-
-      node.fields.pointSize = {
-        label: "Point size",
-        input: "number",
-        value: pointSize,
-        placeholder: "2",
-      };
-      node.fields.pointShape = {
-        label: "Point shape",
-        input: "select",
-        options: POINT_SHAPE_OPTIONS,
-        value: pointShape,
-      };
-      node.fields.decayTime = {
-        label: "Decay time",
-        input: "number",
-        value: decayTime,
-        step: 0.5,
-        placeholder: "0 seconds",
-      };
-      node.fields.colorBy = {
-        label: "Color by",
-        input: "select",
-        options: [{ label: "None", value: "none" }].concat(colorByOptions),
-        value: colorBy,
-      };
-      if (!colorBy || colorBy === "none") {
-        node.fields.flatColor = { label: "Flat color", input: "color", value: flatColor };
-      } else {
-        node.fields.valueMin = {
-          label: "Value min",
-          input: "number",
-          value: valueMin,
-          placeholder: "auto",
-        };
-        node.fields.valueMax = {
-          label: "Value max",
-          input: "number",
-          value: valueMax,
-          placeholder: "auto",
-        };
-        node.fields.colorMap = {
-          label: "Color map",
-          input: "select",
-          options: [
-            { label: "Turbo", value: "turbo" },
-            { label: "Rainbow", value: "rainbow" },
-            { label: "Gradient", value: "gradient" },
-          ],
-          value: colorMap,
-        };
-        if (colorMap === "gradient") {
-          // node.fields.gradient = { label: "Gradient", input: "gradient", value: gradient };
-        }
-      }
-    }
-
-    topicsChildren[topic.name] = node;
   }
 
   // prettier-ignore
