7d75b8f89e142c82d8cdced6e4a261d0ccf4680a
Miles Egan <miles@foxglove.dev>
2022-05-20 12:46:53 -0500

    Require login for DP sources

--- packages/studio-base/src/Workspace.tsx
+++ packages/studio-base/src/Workspace.tsx
@@ -53,6 +53,7 @@ import Preferences from "@foxglove/studio-base/components/Preferences";
 import RemountOnValueChange from "@foxglove/studio-base/components/RemountOnValueChange";
 import Sidebar, { SidebarItem } from "@foxglove/studio-base/components/Sidebar";
 import { SidebarContent } from "@foxglove/studio-base/components/SidebarContent";
+import { SignInFormModal } from "@foxglove/studio-base/components/SignInFormModal";
 import Stack from "@foxglove/studio-base/components/Stack";
 import { URLStateSyncAdapter } from "@foxglove/studio-base/components/URLStateSyncAdapter";
 import { useAssets } from "@foxglove/studio-base/context/AssetsContext";
@@ -74,6 +75,7 @@ import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import useAddPanel from "@foxglove/studio-base/hooks/useAddPanel";
 import { useCalloutDismissalBlocker } from "@foxglove/studio-base/hooks/useCalloutDismissalBlocker";
 import useElectronFilesToOpen from "@foxglove/studio-base/hooks/useElectronFilesToOpen";
+import { useInitialDeepLinkState } from "@foxglove/studio-base/hooks/useInitialDeepLinkState";
 import useNativeAppMenuEvent from "@foxglove/studio-base/hooks/useNativeAppMenuEvent";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
 import { HelpInfoStore, useHelpInfo } from "@foxglove/studio-base/providers/HelpInfoProvider";
@@ -185,13 +187,19 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
 
   const [enableOpenDialog] = useAppConfigurationValue(AppSetting.OPEN_DIALOG);
 
+  const { currentUser } = useCurrentUser();
+
+  const { currentUserRequired } = useInitialDeepLinkState(props.deepLinks ?? []);
+
   const [showOpenDialogOnStartup = true] = useAppConfigurationValue<boolean>(
     AppSetting.SHOW_OPEN_DIALOG_ON_STARTUP,
   );
 
+  const showSignInForm = currentUserRequired && currentUser == undefined;
+
   const [showOpenDialog, setShowOpenDialog] = useState<
     { view: OpenDialogViews; activeDataSource?: IDataSourceFactory } | undefined
-  >(isPlayerPresent || !showOpenDialogOnStartup ? undefined : { view: "start" });
+  >(isPlayerPresent || !showOpenDialogOnStartup || showSignInForm ? undefined : { view: "start" });
 
   const [selectedSidebarItem, setSelectedSidebarItem] = useState<SidebarItemKey | undefined>(() => {
     // When using the open dialog ui - we always start with the connection sidebar open.
@@ -478,8 +486,6 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
     [selectedSidebarItem, supportsAccountSettings],
   );
 
-  const { currentUser } = useCurrentUser();
-
   // Since the _component_ field of a sidebar item entry is a component and accepts no additional
   // props we need to wrap our DataSourceSidebar component to connect the open data source action to
   // open the data source dialog.
@@ -556,6 +562,7 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
         /* eslint-enable react/jsx-key */
       ]}
     >
+      {showSignInForm && <SignInFormModal />}
       {enableOpenDialog === true && showOpenDialog != undefined && (
         <OpenDialog
           activeView={showOpenDialog.view}
@@ -568,7 +575,7 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
           <div className={classes.dropzone}>Drop a file here</div>
         </DropOverlay>
       </DocumentDropListener>
-      <URLStateSyncAdapter deepLinks={props.deepLinks ?? []} />
+      <URLStateSyncAdapter />
       <div className={classes.container} ref={containerRef} tabIndex={0}>
         <Sidebar
           items={sidebarItems}
--- packages/studio-base/src/hooks/useInitialDeepLinkState.ts
+++ packages/studio-base/src/hooks/useInitialDeepLinkState.ts
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import Log from "@foxglove/log";
 import { AppSetting } from "@foxglove/studio-base/AppSetting";
@@ -11,11 +11,12 @@ import {
   useMessagePipeline,
 } from "@foxglove/studio-base/components/MessagePipeline";
 import { useCurrentLayoutActions } from "@foxglove/studio-base/context/CurrentLayoutContext";
+import { useCurrentUser } from "@foxglove/studio-base/context/CurrentUserContext";
 import { usePlayerSelection } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import useDeepMemo from "@foxglove/studio-base/hooks/useDeepMemo";
 import { useSessionStorageValue } from "@foxglove/studio-base/hooks/useSessionStorageValue";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
-import { AppURLState, parseAppURLState } from "@foxglove/studio-base/util/appURLState";
+import { parseAppURLState } from "@foxglove/studio-base/util/appURLState";
 import isDesktopApp from "@foxglove/studio-base/util/isDesktopApp";
 
 const selectPlayerPresence = (ctx: MessagePipelineContext) => ctx.playerState.presence;
@@ -27,7 +28,9 @@ const log = Log.getLogger(__filename);
 /**
  * Restores our session state from any deep link we were passed on startup.
  */
-export function useInitialDeepLinkState(deepLinks: string[]): void {
+export function useInitialDeepLinkState(deepLinks: readonly string[]): {
+  currentUserRequired: boolean;
+} {
   const stableUrlState = useDeepMemo(useMessagePipeline(selectUrlState));
   const { selectSource } = usePlayerSelection();
   const { setSelectedLayoutId } = useCurrentLayoutActions();
@@ -36,20 +39,21 @@ export function useInitialDeepLinkState(deepLinks: string[]): void {
   );
   const seekPlayback = useMessagePipeline(selectSeek);
   const playerPresence = useMessagePipeline(selectPlayerPresence);
+  const { currentUser } = useCurrentUser();
 
-  const appUrlRef = useRef<AppURLState | undefined>();
-  if (!appUrlRef.current) {
-    const firstLink = deepLinks[0];
-    if (firstLink) {
-      try {
-        appUrlRef.current = parseAppURLState(new URL(firstLink));
-      } catch (error) {
-        log.error(error);
-      }
-    }
-  }
+  const targetUrlState = useMemo(
+    () => (deepLinks[0] ? parseAppURLState(new URL(deepLinks[0])) : undefined),
+    [deepLinks],
+  );
 
-  const shouldSeekTimeRef = useRef(false);
+  // Maybe this should be abstracted somewhere but that would require a
+  // more intimate interface with this hook and the player selection logic.
+  const currentUserRequired = targetUrlState?.ds === "foxglove-data-platform";
+
+  // Tracks what portions of the URL state we have yet to apply to the current session.
+  const [unappliedUrlState, setUnappliedUrlState] = useState(
+    targetUrlState ? { ...targetUrlState } : undefined,
+  );
 
   // Set a sessionStorage preference for web if we have a stable URL state.
   // This allows us to avoid asking for the preference immediately on
@@ -64,50 +68,76 @@ export function useInitialDeepLinkState(deepLinks: string[]): void {
     }
   }, [launchPreference, setLaunchPreference, stableUrlState]);
 
+  // Load data source from URL.
   useEffect(() => {
-    const urlState = appUrlRef.current;
-    if (!urlState) {
+    if (!unappliedUrlState) {
+      return;
+    }
+
+    // Wait for current user session if one is required for this source.
+    if (currentUserRequired && !currentUser) {
       return;
     }
 
     // Apply any available datasource args
-    if (urlState.ds) {
-      log.debug("Initialising source from url", urlState);
-      selectSource(urlState.ds, { type: "connection", params: urlState.dsParams });
-      urlState.ds = undefined;
-      urlState.dsParams = undefined;
-      shouldSeekTimeRef.current = true;
+    if (unappliedUrlState.ds) {
+      log.debug("Initialising source from url", unappliedUrlState);
+      selectSource(unappliedUrlState.ds, {
+        type: "connection",
+        params: unappliedUrlState.dsParams,
+      });
+      setUnappliedUrlState((oldState) => ({ ...oldState, ds: undefined, dsParams: undefined }));
     }
+  }, [currentUser, currentUserRequired, selectSource, unappliedUrlState]);
 
-    // Apply any available layout id
-    if (urlState.layoutId != undefined) {
-      log.debug(`Initializing layout from url: ${urlState.layoutId}`);
-      setSelectedLayoutId(urlState.layoutId);
-      urlState.layoutId = undefined;
-    }
-  }, [selectSource, setSelectedLayoutId]);
-
+  // Select layout from URL.
   useEffect(() => {
+    if (!unappliedUrlState) {
+      return;
+    }
+
+    // Wait for current user session if one is required for this source.
+    if (currentUserRequired && !currentUser) {
+      return;
+    }
+
+    // If we have a target DS then wait until the player has loaded until
+    // we try to select the layout. This also handles the case where the
+    // data source requires a logged in user since the player won't start
+    // until we have a current user.
+    if (targetUrlState?.ds && playerPresence !== PlayerPresence.PRESENT) {
+      return;
+    }
+
+    if (unappliedUrlState.layoutId != undefined) {
+      log.debug(`Initializing layout from url: ${unappliedUrlState.layoutId}`);
+      setSelectedLayoutId(unappliedUrlState.layoutId);
+      setUnappliedUrlState((oldState) => ({ ...oldState, layoutId: undefined }));
+    }
+  }, [
+    currentUser,
+    currentUserRequired,
+    playerPresence,
+    setSelectedLayoutId,
+    targetUrlState,
+    unappliedUrlState,
+  ]);
+
+  // Seek to time in URL.
+  useEffect(() => {
+    if (unappliedUrlState?.time == undefined || !seekPlayback) {
+      return;
+    }
+
     // Wait until player is ready before we try to seek.
     if (playerPresence !== PlayerPresence.PRESENT) {
       return;
     }
 
-    const urlState = appUrlRef.current;
-    if (urlState?.time == undefined || !seekPlayback) {
-      return;
-    }
+    log.debug(`Seeking to url time:`, unappliedUrlState.time);
+    seekPlayback(unappliedUrlState.time);
+    setUnappliedUrlState((oldState) => ({ ...oldState, time: undefined }));
+  }, [playerPresence, seekPlayback, unappliedUrlState]);
 
-    if (!shouldSeekTimeRef.current) {
-      log.debug("Clearing urlState time");
-      urlState.time = undefined;
-      return;
-    }
-
-    shouldSeekTimeRef.current = false;
-
-    log.debug(`Seeking to url time:`, urlState.time);
-    seekPlayback(urlState.time);
-    urlState.time = undefined;
-  }, [playerPresence, seekPlayback]);
+  return useMemo(() => ({ currentUserRequired }), [currentUserRequired]);
 }
