144a2b7bc9756088e7894caf70db8d465e42e25c
John Hurliman <jhurliman@jhurliman.org>
2022-05-23 15:55:04 -0700

    [New3D] Fix glTF loading for DRACO-compressed meshes

--- packages/studio-base/src/panels/ThreeDeeRender/ModelCache.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/ModelCache.ts
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
@@ -13,7 +14,11 @@ export class ModelCache {
   private _gltfLoader = new GLTFLoader();
   private _models = new Map<string, Promise<GLTF | undefined>>();
 
-  constructor(private loadModelOptions: LoadModelOptions) {}
+  constructor(private loadModelOptions: LoadModelOptions) {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+    this._gltfLoader.setDRACOLoader(dracoLoader);
+  }
 
   async load(url: string, reportError: (_: Error) => void): Promise<GLTF | undefined> {
     let promise = this._models.get(url);
@@ -33,7 +38,9 @@ export class ModelCache {
     // TODO: Support other model formats
     const gltf = await this._gltfLoader.loadAsync(url);
 
-    // Y-up to Z-up
+    // THREE.js uses Y-up, while we follow the ROS [REP-0103](https://www.ros.org/reps/rep-0103.html)
+    // convention of Z-up
+    gltf.scene.rotateZ(Math.PI / 2);
     gltf.scene.rotateX(Math.PI / 2);
 
     return gltf;
