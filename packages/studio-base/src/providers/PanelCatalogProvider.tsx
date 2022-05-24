03f65aeb7c938350453201afe873ee21a269dbbc
Miles Egan <miles@foxglove.dev>
2022-05-24 09:27:45 -0500

    Remove redundant toolbar on extension panels

--- packages/studio-base/src/providers/PanelCatalogProvider.tsx
+++ packages/studio-base/src/providers/PanelCatalogProvider.tsx
@@ -43,7 +43,6 @@ export default function PanelCatalogProvider(
       const PanelWrapper = (panelProps: PanelProps) => {
         return (
           <>
-            <PanelToolbar />
             <PanelExtensionAdapter
               config={panelProps.config}
               saveConfig={panelProps.saveConfig}
