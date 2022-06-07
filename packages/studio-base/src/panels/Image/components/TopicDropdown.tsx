c25199e0b72b83c5fa273322c41f8a619875dd05
Miles Egan <miles@foxglove.dev>
2022-06-07 12:15:00 -0500

    Keep most of topic picker styling

--- packages/studio-base/src/panels/Image/components/TopicDropdown.tsx
+++ packages/studio-base/src/panels/Image/components/TopicDropdown.tsx
@@ -2,7 +2,14 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+  MenuItem,
+  Select,
+  ListItemText,
+  SelectChangeEvent,
+  SelectProps,
+  MenuProps,
+} from "@mui/material";
 import { useMemo } from "react";
 
 type TopicDropdownItem = {
@@ -13,18 +20,15 @@ type TopicDropdownItem = {
 type Props = {
   title: string;
   items: TopicDropdownItem[];
+  size?: SelectProps["size"];
   open?: boolean;
+  anchorEl?: Element | ReactNull;
+
   onChange: (activeTopics: string[]) => void;
 };
 
-const menuProps = {
-  MenuListProps: {
-    dense: true,
-  },
-};
-
 export function TopicDropdown(props: Props): JSX.Element {
-  const { items, onChange, title } = props;
+  const { items, onChange, title, size = "small" } = props;
 
   const selectedTopics = useMemo<string[]>(() => {
     return items.filter((item) => item.selected).map((item) => item.name);
@@ -37,28 +41,43 @@ export function TopicDropdown(props: Props): JSX.Element {
     onChange(typeof value === "string" ? [value] : value);
   };
 
+  const menuProps: Partial<MenuProps> = {
+    MenuListProps: {
+      dense: true,
+    },
+  };
+
+  // We avoid setting the anchorEl property unless it is specified.
+  // The underlying menu component treats the presence of the property (even if undefined or null)
+  // as a value.
+  if ("anchorEl" in props) {
+    menuProps.anchorEl = props.anchorEl;
+  }
+
   return (
     <>
       <Select
         value={selectedTopics}
         disabled={items.length === 0}
         displayEmpty
-        title={title}
         renderValue={(_selected) => title}
-        size="small"
-        variant="filled"
+        title={title}
+        size={size}
         onChange={handleChange}
         open={props.open}
         MenuProps={menuProps}
       >
         {items.length === 0 && (
           <MenuItem disabled value="">
-            No camera topics
+            <ListItemText
+              primary="No topics"
+              primaryTypographyProps={{ variant: "inherit", color: "text.secondary" }}
+            />
           </MenuItem>
         )}
         {items.map((item) => (
           <MenuItem key={item.name} value={item.name}>
-            {item.name}
+            <ListItemText primary={item.name} primaryTypographyProps={{ variant: "inherit" }} />
           </MenuItem>
         ))}
       </Select>
