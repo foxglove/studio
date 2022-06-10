905643a05c0843c6c5ebe899cb8a79b80d61d3c2
John Hurliman <jhurliman@jhurliman.org>
2022-06-10 11:29:26 -0700

    lint

--- packages/studio-base/src/panels/ThreeDeeRender/stories/common.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/stories/common.ts
@@ -66,15 +66,6 @@ export function makeColor(hex: string, alpha?: number): ColorRGBA {
   return color;
 }
 
-export function rgba(r: number, g: number, b: number, a: number): number {
-  return (
-    (Math.trunc(r * 255) << 24) |
-    (Math.trunc(g * 255) << 16) |
-    (Math.trunc(b * 255) << 8) |
-    Math.trunc(a * 255)
-  );
-}
-
 // ts-prune-ignore-next
 export function makePass({
   id,
