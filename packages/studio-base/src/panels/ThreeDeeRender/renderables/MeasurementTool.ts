de4710a7b70177fd66081aef9cddd829883040d8
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-07-15 11:03:14 -0700

    too much srgb

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/MeasurementTool.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/MeasurementTool.ts
@@ -36,7 +36,7 @@ class FixedSizeMeshMaterial extends THREE.ShaderMaterial {
       fragmentShader: /* glsl */ `
         uniform vec3 color;
         void main() {
-          gl_FragColor = LinearTosRGB(vec4(color, 1.0));
+          gl_FragColor = vec4(color, 1.0);
         }
       `,
       uniforms: {
