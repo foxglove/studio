24235b73d27de84dfe7a34e270c50269e3d7bb2a
Andrew Carr <andrew@foxglove.dev>
2022-07-15 13:17:27 +1000

    Refactor copy action to use MUI button

--- packages/studio-base/src/panels/RawMessages/Value.tsx
+++ packages/studio-base/src/panels/RawMessages/Value.tsx
@@ -2,10 +2,13 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import ClipboardOutlineIcon from "@mdi/svg/svg/clipboard-outline.svg";
+import CheckIcon from "@mui/icons-material/Check";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
+import { styled as muiStyled, Tooltip } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { useCallback, useState } from "react";
 
-import Icon from "@foxglove/studio-base/components/Icon";
+import HoverableIconButton from "@foxglove/studio-base/components/HoverableIconButton";
 import { OpenSiblingPanel } from "@foxglove/studio-base/types/panels";
 import clipboard from "@foxglove/studio-base/util/clipboard";
 
@@ -15,20 +18,27 @@ import { copyMessageReplacer } from "./copyMessageReplacer";
 import { ValueAction } from "./getValueActionForValue";
 
 const useStyles = makeStyles({
-  icon: {
-    "> svg": {
-      verticalAlign: "top !important",
-    },
-  },
   iconBox: {
     display: "inline-block",
     whiteSpace: "nowrap",
-    width: "0px",
-    height: "0px",
+    width: 0,
+    height: 0,
     position: "relative",
-    left: "6px",
+    left: 6,
   },
 });
+
+const StyledIconButton = muiStyled(HoverableIconButton)`
+  padding: 0;
+  font-size: ${({ theme }) => theme.typography.pxToRem(16)};
+
+  &:hover {
+    background-color: transparent;
+  }
+  &:not(:hover) {
+    opacity: 0.6;
+  }
+`;
 
 export default function Value({
   arrLabel,
@@ -48,22 +58,31 @@ export default function Value({
   openSiblingPanel: OpenSiblingPanel;
 }): JSX.Element {
   const classes = useStyles();
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = useCallback((value: string) => {
+    void clipboard.copy(value).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1500);
+    });
+  }, []);
+
   return (
     <span>
       <HighlightedValue itemLabel={itemLabel} />
       {arrLabel.length !== 0 && (
         <>
           {arrLabel}
-          <Icon
-            fade
-            className={classes.icon}
-            onClick={() => {
-              void clipboard.copy(JSON.stringify(itemValue, copyMessageReplacer, 2) ?? "");
-            }}
-            tooltip="Copy"
-          >
-            <ClipboardOutlineIcon />
-          </Icon>
+          <Tooltip arrow title={copied ? "Copied" : "Copy to Clipboard"}>
+            <StyledIconButton
+              size="small"
+              aria-label={copied ? "Copied" : "Copy to Clipboard"}
+              activeColor={copied ? "success" : "primary"}
+              onClick={() => handleCopy(JSON.stringify(itemValue, copyMessageReplacer, 2) ?? "")}
+              color="inherit"
+              icon={copied ? <CheckIcon fontSize="inherit" /> : <CopyAllIcon fontSize="inherit" />}
+            />
+          </Tooltip>
         </>
       )}
       <span className={classes.iconBox}>
