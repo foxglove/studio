87d1b4d4d0e61cbbda5b1b8347b372da80603cfe
Miles Egan <miles@foxglove.dev>
2022-07-19 12:51:28 -0600

    fix

--- packages/studio-base/src/players/UserNodePlayer/index.ts
+++ packages/studio-base/src/players/UserNodePlayer/index.ts
@@ -11,7 +11,7 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { isEqual, pick, uniq } from "lodash";
+import { isEqual, uniq } from "lodash";
 import memoizeWeak from "memoize-weak";
 import shallowequal from "shallowequal";
 import { v4 as uuidv4 } from "uuid";
