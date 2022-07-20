a6aa05342627b7a38aeef4fd48440e5cff8f36b8
Andrew Carr <andrew@foxglove.dev>
2022-07-20 14:31:13 +1000

    Update PlaybackHoverTicks to avoid muiStyled

--- packages/studio-base/src/components/PlaybackControls/PlaybackBarHoverTicks.tsx
+++ packages/studio-base/src/components/PlaybackControls/PlaybackBarHoverTicks.tsx
@@ -11,9 +11,10 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { styled as muiStyled } from "@mui/material";
-import { CSSProperties, useMemo } from "react";
+import { Typography } from "@mui/material";
+import { useMemo } from "react";
 import { useResizeDetector } from "react-resize-detector";
+import { makeStyles } from "tss-react/mui";
 
 import { add, fromSec, toSec } from "@foxglove/rostime";
 import { RpcScales } from "@foxglove/studio-base/components/Chart/types";
@@ -27,38 +28,31 @@ import { useHoverValue } from "@foxglove/studio-base/context/HoverValueContext";
 import { useAppTimeFormat } from "@foxglove/studio-base/hooks";
 import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
-const sharedTickStyles: CSSProperties = {
-  position: "absolute",
-  left: 0,
-  width: 0,
-  height: 0,
-  borderLeft: "5px solid transparent",
-  borderRight: "5px solid transparent",
-  marginLeft: -5,
-};
-
-const TopTick = muiStyled("div")(({ theme }) => ({
-  top: 8,
-  borderTop: `5px solid ${theme.palette.warning.main}`,
-  ...sharedTickStyles,
+const useStyles = makeStyles()((theme) => ({
+  tick: {
+    position: "absolute",
+    left: 0,
+    width: 0,
+    height: 0,
+    borderLeft: "5px solid transparent",
+    borderRight: "5px solid transparent",
+    marginLeft: -5,
+  },
+  tickTop: {
+    top: 8,
+    borderTop: `5px solid ${theme.palette.warning.main}`,
+  },
+  tickBottom: {
+    bottom: 8,
+    borderBottom: `5px solid ${theme.palette.warning.main}`,
+  },
+  label: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    transform: "translate(-50%, -50%)",
+  },
 }));
-
-const BottomTick = muiStyled("div")(({ theme }) => ({
-  bottom: 8,
-  borderBottom: `5px solid ${theme.palette.warning.main}`,
-  ...sharedTickStyles,
-}));
-
-const TimeLabel = muiStyled("div")`
-  position: absolute;
-  left: 0;
-  top: 0;
-  font-family: ${fonts.MONOSPACE};
-  font-size: ${({ theme }) => theme.typography.caption.fontSize};
-  color: ${({ theme }) => theme.palette.warning.main};
-  transform: translate(-50%, -50%);
-  white-space: nowrap;
-`;
 
 function getStartTime(ctx: MessagePipelineContext) {
   return ctx.playerState.activeData?.startTime;
@@ -74,6 +68,7 @@ type Props = {
 
 export default function PlaybackBarHoverTicks(props: Props): JSX.Element {
   const { componentId } = props;
+  const { classes, cx } = useStyles();
 
   const startTime = useMessagePipeline(getStartTime);
   const endTime = useMessagePipeline(getEndTime);
@@ -124,9 +119,20 @@ export default function PlaybackBarHoverTicks(props: Props): JSX.Element {
     <Stack ref={ref} flex="auto">
       {scaleBounds && (
         <HoverBar componentId={componentId} scales={scaleBounds} isTimestampScale>
-          {displayHoverTime && <TimeLabel>{hoverTimeDisplay}</TimeLabel>}
-          <TopTick />
-          <BottomTick />
+          {displayHoverTime && (
+            <Typography
+              className={classes.label}
+              align="center"
+              variant="caption"
+              color="warning.main"
+              fontFamily={fonts.MONOSPACE}
+              noWrap
+            >
+              {hoverTimeDisplay}
+            </Typography>
+          )}
+          <div className={cx(classes.tick, classes.tickTop)} />
+          <div className={cx(classes.tick, classes.tickBottom)} />
         </HoverBar>
       )}
     </Stack>
