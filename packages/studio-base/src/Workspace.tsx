47dec1634db2d372f60de0685c6b20ee50c03a12
Miles Egan <miles@foxglove.dev>
2022-07-26 17:06:32 -0600

    prev default in handler

--- packages/studio-base/src/Workspace.tsx
+++ packages/studio-base/src/Workspace.tsx
@@ -573,6 +573,7 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
           return;
         }
 
+        ev.preventDefault();
         setSelectedSidebarItem(undefined);
       },
     }),
