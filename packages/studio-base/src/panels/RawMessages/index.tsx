4280f429357d2a334c6296dbf4385fadbc6f5d2a
Andrew Carr <andrew@foxglove.dev>
2022-08-02 11:59:51 +1000

    Update raw message value to match current formatting

--- packages/studio-base/src/panels/RawMessages/index.tsx
+++ packages/studio-base/src/panels/RawMessages/index.tsx
@@ -112,7 +112,12 @@ const useStyles = makeStyles()((theme) => ({
   },
   topic: {
     fontFamily: fonts.SANS_SERIF,
-    fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, "zero" !important`,
+    fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, "zero"`,
+  },
+  big: {
+    "&.MuiTypography-root": {
+      fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, "zero"`,
+    },
   },
   hoverObserver: {
     display: "inline-flex",
@@ -441,6 +446,7 @@ function RawMessages(props: Props) {
         />
         {shouldDisplaySingleVal ? (
           <Typography
+            className={classes.big}
             variant="h1"
             fontWeight="bold"
             whiteSpace="pre-line"
@@ -627,6 +633,7 @@ function RawMessages(props: Props) {
   }, [
     autoExpandPaths,
     baseItem,
+    classes.big,
     classes.topic,
     diffEnabled,
     diffItem,
