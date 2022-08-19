d626431afb7cfd36206739577c05d69eb9595d18
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-08-19 15:12:44 -0700

    fix import order

--- packages/studio-base/src/components/CssBaseline.tsx
+++ packages/studio-base/src/components/CssBaseline.tsx
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { alpha } from "@mui/material";
 import { PropsWithChildren } from "react";
 import { makeStyles } from "tss-react/mui";
 
@@ -9,8 +10,6 @@ import "@foxglove/studio-base/styles/assets/inter.css";
 import "@foxglove/studio-base/styles/assets/plex-mono.css";
 
 import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
-
-import { alpha } from "@mui/material";
 
 const useStyles = makeStyles()(({ palette, typography }) => ({
   root: {
