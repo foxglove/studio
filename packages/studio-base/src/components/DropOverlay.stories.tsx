222267764cfdddfe286e8e3096be75d7a8dba4d7
Andrew Carr <andrew@foxglove.dev>
2022-08-25 13:43:39 +1000

    Update storybook stories to show dark/light variants

--- packages/studio-base/src/components/DropOverlay.stories.tsx
+++ packages/studio-base/src/components/DropOverlay.stories.tsx
@@ -1,20 +1,19 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//   Copyright 2018-2021 Cruise LLC
-//
-//   This source code is licensed under the Apache License, Version 2.0,
-//   found at http://www.apache.org/licenses/LICENSE-2.0
-//   You may not use this file except in compliance with the License.
-
-import { storiesOf } from "@storybook/react";
 
 import DropOverlay from "@foxglove/studio-base/components/DropOverlay";
 
-storiesOf("components/DropOverlay", module).add("default", () => {
+export default {
+  title: "components/DropOverlay",
+};
+
+export function Dark(): JSX.Element {
   return <DropOverlay open>Some DropOverlay</DropOverlay>;
-});
+}
+Dark.parameters = { colorScheme: "dark" };
+
+export function Light(): JSX.Element {
+  return <DropOverlay open>Some DropOverlay</DropOverlay>;
+}
+Light.parameters = { colorScheme: "light" };
