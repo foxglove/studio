1169bc768400a59b4ba2280eedbc0c0a401b256d
Andrew Carr <andrew@foxglove.dev>
2022-08-26 10:35:40 +1000

    Remove global style tweaks to backdrop

--- packages/studio-base/src/theme/muiComponents.ts
+++ packages/studio-base/src/theme/muiComponents.ts
@@ -91,14 +91,6 @@ export default function muiComponents(theme: Theme): ThemeOptions["components"] 
         variant: "standard",
       },
     },
-    MuiBackdrop: {
-      styleOverrides: {
-        root: {
-          zIndex: theme.zIndex.modal,
-          color: theme.palette.getContrastText(theme.palette.background.paper),
-        },
-      },
-    },
     MuiBadge: {
       styleOverrides: {
         badge: {
