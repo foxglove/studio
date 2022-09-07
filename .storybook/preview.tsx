d58907a42801510216d3b51b24d46d5614154809
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-09-06 17:54:18 -0700

    fix stories?

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -11,6 +11,10 @@ import GlobalCss from "@foxglove/studio-base/components/GlobalCss";
 import MultiProvider from "@foxglove/studio-base/components/MultiProvider";
 import StudioToastProvider from "@foxglove/studio-base/components/StudioToastProvider";
 import AppConfigurationContext from "@foxglove/studio-base/context/AppConfigurationContext";
+import PanelCatalogContext, {
+  PanelCatalog,
+  PanelInfo,
+} from "@foxglove/studio-base/context/PanelCatalogContext";
 import { UserNodeStateProvider } from "@foxglove/studio-base/context/UserNodeStateContext";
 import TimelineInteractionStateProvider from "@foxglove/studio-base/providers/TimelineInteractionStateProvider";
 import ReadySignalContext from "@foxglove/studio-base/stories/ReadySignalContext";
@@ -50,6 +54,15 @@ function useCombinedReadySignal(
   }, [readySignal]);
 }
 
+class MockPanelCatalog implements PanelCatalog {
+  public getPanels(): readonly PanelInfo[] {
+    return [];
+  }
+  public getPanelByType(_type: string): PanelInfo | undefined {
+    return undefined;
+  }
+}
+
 function StudioContextProviders({
   children,
   ctx,
@@ -79,6 +92,7 @@ function StudioContextProviders({
     <StudioToastProvider />,
     <TimelineInteractionStateProvider />,
     <UserNodeStateProvider />,
+    <PanelCatalogContext.Provider value={new MockPanelCatalog()} />,
     /* eslint-enable react/jsx-key */
   ];
   return (
