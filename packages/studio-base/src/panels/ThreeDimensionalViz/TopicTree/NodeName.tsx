fddb9401221c0b5257dec658b83ebcae4a6b47c6
snosenzo <sam@foxglove.dev>
2022-09-12 11:13:12 -0400

    Remove ThreeDimensionalViz imports from New3D panel except globalvariables Fixes #4381

--- packages/studio-base/src/panels/ThreeDimensionalViz/TopicTree/NodeName.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/TopicTree/NodeName.tsx
@@ -15,8 +15,8 @@ import styled from "styled-components";
 
 import Tooltip from "@foxglove/studio-base/components/Tooltip";
 
+import TextMiddleTruncate from "../../../components/TextMiddleTruncate";
 import TextHighlight from "./TextHighlight";
-import TextMiddleTruncate from "./TextMiddleTruncate";
 
 // Extra text length to make sure text such as `1000 visible topics` don't get truncated.
 const DEFAULT_END_TEXT_LENGTH = 22;
