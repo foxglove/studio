3a1f3412569a337b4ccbbfa65c4677381f0f802b
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-09-13 10:21:19 -0700

    comment

--- packages/studio-base/src/components/MosaicPathContext.ts
+++ packages/studio-base/src/components/MosaicPathContext.ts
@@ -5,4 +5,9 @@
 import { createContext } from "react";
 import { MosaicPath } from "react-mosaic-component";
 
+/**
+ * Exposes the mosaic path at which a panel is located. Unlike calling
+ * `mosaicWindowActions.getPath()` during render, subscribing to this context will trigger a
+ * re-render when the path changes.
+ */
 export const MosaicPathContext = createContext<MosaicPath | undefined>(undefined);
