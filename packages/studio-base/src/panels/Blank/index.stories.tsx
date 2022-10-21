bf3361e92d2ea130cb888f02a8093f4cffde82cf
Andrew Carr <andrew@foxglove.dev>
2022-10-22 05:54:03 +1100

    Update return types on stories

--- packages/studio-base/src/panels/Blank/index.stories.tsx
+++ packages/studio-base/src/panels/Blank/index.stories.tsx
@@ -2,9 +2,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { Story, StoryContext } from "@storybook/react";
+
 import Blank from "@foxglove/studio-base/panels/Blank";
 import PanelSetup from "@foxglove/studio-base/stories/PanelSetup";
-import { Story, StoryContext } from "@storybook/react";
 
 export default {
   title: "panels/Blank",
@@ -20,21 +21,17 @@ export default {
   ],
 };
 
-export const Default = () => <Blank />;
+export function Default(): JSX.Element {
+  return <Blank />;
+}
+Default.parameters = { colorScheme: "both-column" };
 
-Default.parameters = {
-  colorScheme: "both-column",
-};
+export function NoLogo(): JSX.Element {
+  return <Blank />;
+}
+NoLogo.parameters = { colorScheme: "both-column" };
 
-export const NoLogo = () => <Blank />;
-
-NoLogo.parameters = {
-  colorScheme: "both-column",
-};
-
-export const WithSettings = () => <Blank />;
-
-WithSettings.parameters = {
-  colorScheme: "both-column",
-  includeSettings: true,
-};
+export function WithSettings(): JSX.Element {
+  return <Blank />;
+}
+WithSettings.parameters = { colorScheme: "both-column", includeSettings: true };
