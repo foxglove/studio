827034317d826cb14ed322bc1f870dbb91d8261f
Esther Weon <esther.s.weon@gmail.com>
2022-11-02 11:54:04 -0700

    Don't show warning when there is a disabled data source

--- packages/studio-base/src/components/OpenDialog/Connection.tsx
+++ packages/studio-base/src/components/OpenDialog/Connection.tsx
@@ -130,7 +130,9 @@ export default function Connection(props: ConnectionProps): JSX.Element {
           </Tabs>
         </Stack>
         <Stack key={selectedSource?.id} flex="1 0" gap={2}>
-          {selectedSource?.warning && <Alert severity="warning">{selectedSource.warning}</Alert>}
+          {selectedSource?.disabledReason == undefined && selectedSource?.warning && (
+            <Alert severity="warning">{selectedSource.warning}</Alert>
+          )}
           {selectedSource?.disabledReason != undefined && (
             <Alert severity="warning">{selectedSource.disabledReason}</Alert>
           )}
