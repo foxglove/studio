41b8dc33921e4bc65cdbb3ba5c7845ad39421df1
Miles Egan <miles@foxglove.dev>
2022-12-23 07:35:28 +0900

    cleaner test

--- packages/studio-base/src/hooks/useTopicPublishFrequences.test.tsx
+++ packages/studio-base/src/hooks/useTopicPublishFrequences.test.tsx
@@ -4,58 +4,53 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { renderHook } from "@testing-library/react";
-import { DeepPartial } from "ts-essentials";
 
-import {
-  MessagePipelineContext,
-  useMessagePipeline,
-} from "@foxglove/studio-base/components/MessagePipeline";
+import MockMessagePipelineProvider from "@foxglove/studio-base/components/MessagePipeline/MockMessagePipelineProvider";
 import { useTopicPublishFrequencies } from "@foxglove/studio-base/hooks/useTopicPublishFrequences";
 import { PlayerCapabilities, PlayerState } from "@foxglove/studio-base/players/types";
 
-jest.mock("@foxglove/studio-base/components/MessagePipeline");
-
 describe("useSynchronousMountedState", () => {
   it("calculates frequences for a static source", () => {
-    (useMessagePipeline as jest.Mock).mockImplementation(
-      (selector: (ctx: DeepPartial<MessagePipelineContext>) => unknown) =>
-        selector({
-          playerState: {
-            activeData: {
-              currentTime: { sec: 2, nsec: 0 },
-              endTime: { sec: 10, nsec: 0 },
-              startTime: { sec: 0, nsec: 0 },
-              topicStats: new Map([
-                [
-                  "topic_a",
-                  {
-                    numMessages: 10,
-                    firstMessageTime: { sec: 1, nsec: 0 },
-                    lastMessageTime: { sec: 5, nsec: 0 },
-                  },
-                ],
-                [
-                  "topic_b",
-                  {
-                    numMessages: 20,
-                    firstMessageTime: { sec: 2, nsec: 0 },
-                    lastMessageTime: { sec: 7, nsec: 0 },
-                  },
-                ],
-              ]),
-            },
-            capabilities: [PlayerCapabilities.playbackControl],
+    const activeData: Partial<PlayerState["activeData"]> = {
+      currentTime: { sec: 2, nsec: 0 },
+      endTime: { sec: 10, nsec: 0 },
+      startTime: { sec: 0, nsec: 0 },
+      topicStats: new Map([
+        [
+          "topic_a",
+          {
+            numMessages: 10,
+            firstMessageTime: { sec: 1, nsec: 0 },
+            lastMessageTime: { sec: 5, nsec: 0 },
           },
-        }),
-    );
+        ],
+        [
+          "topic_b",
+          {
+            numMessages: 20,
+            firstMessageTime: { sec: 2, nsec: 0 },
+            lastMessageTime: { sec: 7, nsec: 0 },
+          },
+        ],
+      ]),
+    };
 
-    const { result } = renderHook(useTopicPublishFrequencies);
+    const { result } = renderHook(useTopicPublishFrequencies, {
+      wrapper: ({ children }) => (
+        <MockMessagePipelineProvider
+          activeData={activeData}
+          capabilities={[PlayerCapabilities.playbackControl]}
+        >
+          {children}
+        </MockMessagePipelineProvider>
+      ),
+    });
 
     expect(result.current).toStrictEqual({ topic_a: 2.25, topic_b: 3.8 });
   });
 
   it("updates frequences for a live source", () => {
-    const activeData: DeepPartial<PlayerState["activeData"]> = {
+    let activeData: Partial<PlayerState["activeData"]> = {
       currentTime: { sec: 2, nsec: 0 },
       endTime: { sec: 10, nsec: 0 },
       startTime: { sec: 0, nsec: 0 },
@@ -65,25 +60,25 @@ describe("useSynchronousMountedState", () => {
       ]),
     };
 
-    (useMessagePipeline as jest.Mock).mockImplementation(
-      (selector: (ctx: DeepPartial<MessagePipelineContext>) => unknown) =>
-        selector({
-          playerState: {
-            activeData,
-            capabilities: [],
-          },
-        }),
-    );
-
-    const { result, rerender } = renderHook(useTopicPublishFrequencies);
+    const { result, rerender } = renderHook(useTopicPublishFrequencies, {
+      wrapper: ({ children }) => (
+        <MockMessagePipelineProvider activeData={activeData}>
+          {children}
+        </MockMessagePipelineProvider>
+      ),
+    });
 
     expect(result.current).toStrictEqual({});
 
-    activeData.currentTime = { sec: 3, nsec: 0 };
-    activeData.topicStats = new Map([
-      ["topic_a", { numMessages: 20 }],
-      ["topic_b", { numMessages: 40 }],
-    ]);
+    activeData = {
+      currentTime: { sec: 3, nsec: 0 },
+      endTime: { sec: 10, nsec: 0 },
+      startTime: { sec: 0, nsec: 0 },
+      topicStats: new Map([
+        ["topic_a", { numMessages: 20 }],
+        ["topic_b", { numMessages: 40 }],
+      ]),
+    };
     rerender();
 
     expect(result.current).toStrictEqual({ topic_a: 10, topic_b: 20 });
