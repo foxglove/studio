60892ce463c592bc678a30adbca527cbbceca546
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-01-17 17:53:59 -0800

    WIP: convert Image panel to extension

--- packages/studio-base/src/panels/Image/hooks/useImagePanelMessages.ts
+++ packages/studio-base/src/panels/Image/hooks/useImagePanelMessages.ts
@@ -2,11 +2,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { flatten } from "lodash";
-import { useCallback, useMemo } from "react";
+import { useState } from "react";
+import { createStore, useStore } from "zustand";
 
 import { AVLTree } from "@foxglove/avl";
-import { useShallowMemo } from "@foxglove/hooks";
 import {
   Time,
   compare as compareTime,
@@ -14,30 +13,43 @@ import {
   toNanoSec,
   fromNanoSec,
 } from "@foxglove/rostime";
-import { MessageEvent } from "@foxglove/studio";
-import { useMessageReducer } from "@foxglove/studio-base/PanelAPI";
+import { MessageEvent, RenderState } from "@foxglove/studio";
 
 import { normalizeAnnotations } from "../lib/normalizeAnnotations";
 import { normalizeImageMessage } from "../lib/normalizeMessage";
 import { Annotation, NormalizedImageMessage } from "../types";
-import { useDatatypesByTopic } from "./useDatatypesByTopic";
 
-export type ImagePanelMessages = {
+type ImagePanelState = {
+  imageTopic?: string;
+  annotationTopics: string[];
   image?: NormalizedImageMessage;
-  annotations?: Annotation[];
+  annotationsByTopic: Map<string, Annotation[]>;
+  tree: AVLTree<Time, SynchronizationItem>;
+  synchronize: boolean;
+
+  actions: {
+    frame(currentFrame: NonNullable<RenderState["currentFrame"]>): void;
+    clear(): void;
+    setSynchronize(synchronize: boolean): void;
+    setImageTopic(newTopic: string): void;
+    setAnnotationTopics(newTopics: string[]): void;
+  };
 };
+
+type PublicState = {
+  image?: NormalizedImageMessage;
+  annotations: readonly Annotation[];
+} & ImagePanelState["actions"];
+
+const selectPublicState = (state: ImagePanelState): PublicState => ({
+  ...state.actions,
+  image: state.image,
+  annotations: ([] as Annotation[]).concat(...state.annotationsByTopic.values()),
+});
 
 export type SynchronizationItem = {
   image?: NormalizedImageMessage;
   annotationsByTopic: Map<string, Annotation[]>;
-};
-
-type ReducerState = {
-  imageTopic?: string;
-  image?: NormalizedImageMessage;
-  annotationsByTopic: Map<string, Annotation[]>;
-
-  tree: AVLTree<Time, SynchronizationItem>;
 };
 
 export const ANNOTATION_DATATYPES = [
@@ -61,198 +73,177 @@ export const ANNOTATION_DATATYPES = [
   "foxglove.ImageAnnotations",
 ] as const;
 
-type Options = {
-  imageTopic: string;
-  annotationTopics: string[];
-  synchronize: boolean;
-};
+export function synchronizedAddMessages(
+  state: ImagePanelState,
+  messageEvents: readonly MessageEvent<unknown>[],
+): Partial<ImagePanelState> {
+  for (const messageEvent of messageEvents) {
+    const image = normalizeImageMessage(messageEvent, messageEvent.schemaName);
+    const annotations = normalizeAnnotations(messageEvent, messageEvent.schemaName);
+    if (!image && !annotations) {
+      continue;
+    }
 
-export function synchronizedAddMessage(
-  state: ReducerState,
-  args: { datatype: string; event: MessageEvent<unknown>; annotationTopics: string[] },
-): ReducerState {
-  const {
-    datatype,
-    annotationTopics,
-    event: { topic, message },
-  } = args;
-
-  const image = normalizeImageMessage(message, datatype);
-  const annotations = normalizeAnnotations(message, datatype);
-
-  if (!image && !annotations) {
-    return state;
-  }
-
-  // Update the image at the stamp time
-  if (image) {
-    const item = state.tree.get(image.stamp) ?? {
-      image: undefined,
-      annotationsByTopic: new Map(),
-    };
-    item.image = image;
-    state.tree.set(image.stamp, item);
-  }
-
-  // Update annotations at the stamp time
-  if (annotations) {
-    // If we know all the annotations are the same timestamp, we can shortcut
-    // and set the annotations by topic directly for the single stamp
-    let sameStamp = annotations[0]?.stamp;
-    for (const annotation of annotations) {
-      if (!sameStamp || compareTime(annotation.stamp, sameStamp) !== 0) {
-        sameStamp = undefined;
-        break;
+    if (image) {
+      // Update the image at the stamp time
+      const item = state.tree.get(image.stamp);
+      if (item) {
+        item.image = image;
+      } else {
+        state.tree.set(image.stamp, {
+          image,
+          annotationsByTopic: new Map(),
+        });
       }
     }
 
-    if (sameStamp) {
-      const item = state.tree.get(sameStamp) ?? {
-        image: undefined,
-        annotationsByTopic: new Map(),
-      };
-      item.annotationsByTopic.set(topic, annotations);
-      state.tree.set(sameStamp, item);
-    } else {
-      // Annotations have different timestamps. This means we have to group them by stamp.
-      // Then for each stamp, we update the annotations by topic at that stamp.
-
+    if (annotations) {
+      // Group annotations by timestamp, then update the annotations by topic at each stamp
       const groups = new Map<bigint, Annotation[]>();
       for (const annotation of annotations) {
         const key = toNanoSec(annotation.stamp);
-        const arr = groups.get(key) ?? [];
-        arr.push(annotation);
-        groups.set(key, arr);
+        const arr = groups.get(key);
+        if (arr) {
+          arr.push(annotation);
+        } else {
+          groups.set(key, [annotation]);
+        }
       }
 
-      for (const entry of groups.entries()) {
-        const stamp = fromNanoSec(entry[0]);
-        const item = state.tree.get(stamp) ?? {
-          image: undefined,
-          annotationsByTopic: new Map(),
-        };
-        item.annotationsByTopic.set(topic, entry[1]);
-        state.tree.set(stamp, item);
+      for (const [stampNsec, group] of groups) {
+        const stamp = fromNanoSec(stampNsec);
+        let item = state.tree.get(stamp);
+        if (!item) {
+          item = {
+            image: undefined,
+            annotationsByTopic: new Map(),
+          };
+          state.tree.set(stamp, item);
+        }
+        item.annotationsByTopic.set(messageEvent.topic, group);
       }
     }
-  }
 
-  // Find the oldest entry where we have everything synchronized
-  let validEntry: [Time, SynchronizationItem] | undefined = undefined;
-  for (const entry of state.tree.entries()) {
-    const messageState = entry[1];
-    // If we have an image and all the messages for annotation topics then we have a synchronized set.
-    if (messageState.image && messageState.annotationsByTopic.size === annotationTopics.length) {
-      validEntry = entry;
-    }
-  }
-
-  // We've got a set of synchronized messages, remove any older items from the tree
-  if (validEntry) {
-    let minKey = state.tree.minKey();
-    while (minKey && isLessThan(minKey, validEntry[0])) {
-      state.tree.shift();
-
-      minKey = state.tree.minKey();
+    // Find the oldest entry where we have everything synchronized
+    let validEntry: [Time, SynchronizationItem] | undefined = undefined;
+    for (const entry of state.tree.entries()) {
+      const messageState = entry[1];
+      // If we have an image and all the messages for annotation topics then we have a synchronized set.
+      if (
+        messageState.image &&
+        messageState.annotationsByTopic.size === state.annotationTopics.length
+      ) {
+        validEntry = entry;
+      }
     }
 
-    return {
-      imageTopic: state.imageTopic,
-      image: validEntry[1].image,
-      annotationsByTopic: validEntry[1].annotationsByTopic,
-      tree: state.tree,
-    };
+    // We've got a set of synchronized messages, remove any older items from the tree
+    if (validEntry) {
+      let minKey = state.tree.minKey();
+      while (minKey && isLessThan(minKey, validEntry[0])) {
+        state.tree.shift();
+
+        minKey = state.tree.minKey();
+      }
+
+      return {
+        imageTopic: state.imageTopic,
+        image: validEntry[1].image,
+        annotationsByTopic: validEntry[1].annotationsByTopic,
+        tree: state.tree,
+      };
+    }
   }
 
   // with no valid entry, we keep the previous state
   return state;
 }
 
-function useImagePanelMessages(options?: Options): ImagePanelMessages {
-  const { imageTopic, annotationTopics, synchronize = false } = options ?? {};
+function addMessages(
+  state: ImagePanelState,
+  messageEvents: readonly MessageEvent<unknown>[],
+): Partial<ImagePanelState> {
+  if (state.synchronize && state.annotationTopics.length > 0) {
+    return synchronizedAddMessages(state, messageEvents);
+  }
 
-  const topics = useMemo(() => {
-    const out: string[] = [];
-    if (imageTopic) {
-      out.push(imageTopic);
+  let newState: Pick<ImagePanelState, "image" | "annotationsByTopic"> | undefined;
+  for (const event of messageEvents) {
+    const normalizedImage = normalizeImageMessage(event.message, event.schemaName);
+    const normalizedAnnotations = normalizeAnnotations(event.message, event.schemaName);
+    if (!normalizedImage && !normalizedAnnotations) {
+      continue;
     }
-    if (annotationTopics) {
-      out.push(...annotationTopics);
-    }
-    return out;
-  }, [annotationTopics, imageTopic]);
 
-  const shallowTopics = useShallowMemo(topics);
-
-  const datatypesByTopic = useDatatypesByTopic();
-
-  const restore = useCallback(
-    (state?: ReducerState) => {
-      // When changing image topics, clear the image and any annotations
-      if (!state || state.imageTopic !== imageTopic) {
-        return {
-          annotationsByTopic: new Map(),
-          tree: new AVLTree<Time, SynchronizationItem>(compareTime),
-        };
-      }
-      return state;
-    },
-    [imageTopic],
-  );
-
-  const addMessage = useCallback(
-    (state: ReducerState, event: MessageEvent<unknown>): ReducerState => {
-      // A datatype is required to normalize the message
-      const datatype = datatypesByTopic.get(event.topic);
-      if (!datatype) {
-        return state;
-      }
-
-      if (synchronize && annotationTopics) {
-        return synchronizedAddMessage(state, {
-          annotationTopics,
-          datatype,
-          event,
-        });
-      }
-
-      const normalizedImage = normalizeImageMessage(event.message, datatype);
-      const normalizedAnnotations = normalizeAnnotations(event.message, datatype);
-
-      if (!normalizedImage && !normalizedAnnotations) {
-        return state;
-      }
-
-      let annotationsByTopic = state.annotationsByTopic;
-
-      if (normalizedAnnotations) {
-        annotationsByTopic.set(event.topic, normalizedAnnotations);
-        annotationsByTopic = new Map(annotationsByTopic);
-      }
-
-      return {
-        imageTopic: state.imageTopic,
-        image: normalizedImage ?? state.image,
-        annotationsByTopic,
-        tree: state.tree,
+    if (!newState) {
+      newState = {
+        image: state.image,
+        annotationsByTopic: new Map(state.annotationsByTopic),
       };
-    },
-    [annotationTopics, datatypesByTopic, synchronize],
+    }
+
+    if (normalizedImage) {
+      newState.image = normalizedImage;
+    }
+    if (normalizedAnnotations) {
+      newState.annotationsByTopic.set(event.topic, normalizedAnnotations);
+    }
+  }
+
+  return newState ?? state;
+}
+
+function useImagePanelMessages(initialState: {
+  imageTopic: string;
+  annotationTopics: string[];
+  synchronize: boolean;
+}): ImagePanelState["actions"] {
+  const [store] = useState(() =>
+    createStore<ImagePanelState>((set) => ({
+      imageTopic: initialState.imageTopic,
+      annotationTopics: initialState.annotationTopics,
+      annotationsByTopic: new Map(),
+      tree: new AVLTree(compareTime),
+      synchronize: initialState.synchronize,
+
+      actions: {
+        setImageTopic(imageTopic) {
+          // When changing image topics, clear the image and any annotations
+          set((old) =>
+            old.imageTopic !== imageTopic
+              ? {
+                  annotationsByTopic: new Map(),
+                  tree: new AVLTree(compareTime),
+                }
+              : old,
+          );
+        },
+        setAnnotationTopics(annotationTopics) {
+          set({
+            annotationTopics,
+            // FIXME - what if synchronize is enabled?
+          });
+        },
+        setSynchronize(synchronize) {
+          set({
+            synchronize,
+            // FIXME - how to switch modes?
+          });
+        },
+        clear() {
+          set({
+            annotationsByTopic: new Map(),
+            tree: new AVLTree(compareTime),
+          });
+        },
+        frame(currentFrame) {
+          set((old) => addMessages(old, currentFrame));
+        },
+      },
+    })),
   );
 
-  const { image, annotationsByTopic } = useMessageReducer({
-    topics: shallowTopics,
-    restore,
-    addMessage,
-  });
-
-  return useMemo(() => {
-    const annotations = flatten(Array.from(annotationsByTopic.values()));
-    return {
-      image,
-      annotations,
-    };
-  }, [annotationsByTopic, image]);
+  return useStore(store, selectPublicState);
 }
 
 export { useImagePanelMessages };
