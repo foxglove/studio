5968f452b8fed25bcda5b533246602e1984dcb41
snosenzo <sam@foxglove.dev>
2023-01-23 15:23:12 -0500

    - account for binary search complement and add tests

--- packages/den/collection/ArrayMap.ts
+++ packages/den/collection/ArrayMap.ts
@@ -63,7 +63,8 @@ export class ArrayMap<K, V> {
   /** Removes all elements with keys less than the given key. */
   public removeBefore(key: K): void {
     const index = this.binarySearch(key);
-    this._list.splice(0, index);
+    const lessThanIndex = index >= 0 ? index : ~index;
+    this._list.splice(0, lessThanIndex);
   }
 
   /** Access the first key/value tuple in the list, without modifying the list. */
