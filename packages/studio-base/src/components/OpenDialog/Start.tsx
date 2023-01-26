f4cc8bbf247a63526fac49a80906aefea8dceb39
Adrian Macneil <adrian@foxglove.dev>
2023-01-26 14:27:47 -0800

    Update packages/studio-base/src/components/OpenDialog/Start.tsx
    Co-authored-by: Esther S. Weon <esther.s.weon@gmail.com>

--- packages/studio-base/src/components/OpenDialog/Start.tsx
+++ packages/studio-base/src/components/OpenDialog/Start.tsx
@@ -419,7 +419,7 @@ export default function Start(props: IStartProps): JSX.Element {
       {
         key: "open-local-file",
         text: "Open local file",
-        secondaryText: "Visualize directly from your local filesystem.",
+        secondaryText: "Visualize data directly from your local filesystem.",
         icon: (
           <SvgIcon fontSize="large" color="primary" viewBox="0 0 2048 2048">
             <path d="M1955 1533l-163-162v677h-128v-677l-163 162-90-90 317-317 317 317-90 90zM256 1920h1280v128H128V0h1115l549 549v475h-128V640h-512V128H256v1792zM1280 512h293l-293-293v293z" />
