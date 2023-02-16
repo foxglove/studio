94d4691430bc627fc8bdac3696209faf38ac7916
Roman Shtylman <roman@foxglove.dev>
2023-02-16 07:45:32 -0800

    Remove unused FOXGLOVE_USER_AGENT

--- packages/studio-base/webpack.ts
+++ packages/studio-base/webpack.ts
@@ -239,9 +239,6 @@ export function makeConfig(
         // Should match webpack-defines.d.ts
         ReactNull: null, // eslint-disable-line no-restricted-syntax
         FOXGLOVE_STUDIO_VERSION: JSON.stringify(packageJson.version),
-        FOXGLOVE_USER_AGENT: JSON.stringify(
-          `studio/${packageJson.version} (commit ${commitHash ?? "??"})`,
-        ),
       }),
       // https://webpack.js.org/plugins/ignore-plugin/#example-of-ignoring-moment-locales
       new webpack.IgnorePlugin({
