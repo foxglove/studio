59a102975d23adc762ca347d50b3aa461b58309b
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-02-27 16:09:21 -0800

    fix menu modal blocking drop targets

--- packages/studio-base/src/components/AppBar/Preferences.tsx
+++ packages/studio-base/src/components/AppBar/Preferences.tsx
@@ -174,7 +174,7 @@ export const aboutItems: Map<
 
 export function PreferencesIconButton(props: IconButtonProps): JSX.Element {
   return (
-    <IconButton {...props} className={classes.iconButton}>
+    <IconButton {...props}>
       <Settings24Regular />
     </IconButton>
   );
