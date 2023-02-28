2580cf087309f4a0e0dd307ed5dce409f038cb03
Roman Shtylman <roman@foxglove.dev>
2023-02-28 13:31:47 -0800

    feedback

--- packages/studio-base/src/components/CaptureErrorBoundary.tsx
+++ packages/studio-base/src/components/CaptureErrorBoundary.tsx
@@ -7,13 +7,27 @@ type Props = {
   onError: (err: Error) => void;
 };
 
+type State = {
+  hadError: boolean;
+};
+
 /** An error boundary that calls an onError function when it captures an error */
-export class CaptureErrorBoundary extends Component<PropsWithChildren<Props>, unknown> {
+export class CaptureErrorBoundary extends Component<PropsWithChildren<Props>, State> {
+  public override state: State = {
+    hadError: false,
+  };
+
   public override componentDidCatch(error: Error): void {
+    this.setState({ hadError: true });
     this.props.onError(error);
   }
 
   public override render(): ReactNode {
+    // Avoid rendering children since the children are what caused the error
+    if (this.state.hadError) {
+      return <></>;
+    }
+
     return this.props.children;
   }
 }
