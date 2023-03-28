2e7fe44fc7a17d52f31f937a6894841cddd16e0e
Andrew Carr <andrew@foxglove.dev>
2023-03-28 14:48:43 +1100

    Restyle data source info

--- packages/studio-base/src/components/AppBar/DataSource.tsx
+++ packages/studio-base/src/components/AppBar/DataSource.tsx
@@ -2,23 +2,13 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { ErrorCircle20Filled, ChevronDown12Filled } from "@fluentui/react-icons";
-import {
-  ButtonBase,
-  CircularProgress,
-  Divider,
-  IconButton,
-  Menu,
-  MenuItem,
-  Tooltip,
-} from "@mui/material";
-import { useState, useRef, MouseEvent } from "react";
-import tinycolor from "tinycolor2";
+import { ErrorCircle20Filled, ChevronDown12Filled, Open16Filled } from "@fluentui/react-icons";
+import { ButtonBase, CircularProgress, IconButton, Tooltip } from "@mui/material";
+import { useState, useRef } from "react";
 import { makeStyles } from "tss-react/mui";
 
 import {
   APP_BAR_PRIMARY_COLOR,
-  APP_BAR_FOREGROUND_COLOR,
   APP_BAR_HEIGHT,
 } from "@foxglove/studio-base/components/AppBar/constants";
 import { ProblemsList } from "@foxglove/studio-base/components/DataSourceSidebar/ProblemsList";
@@ -34,7 +24,7 @@ import { LayoutMenu } from "./LayoutMenu";
 
 const LEFT_ICON_SIZE = 19;
 
-const useStyles = makeStyles<void, "adornmentError">()((theme, _params, classes) => ({
+const useStyles = makeStyles<void, "adornmentError" | "openIcon">()((theme, _params, classes) => ({
   button: {
     font: "inherit",
     display: "flex",
@@ -44,19 +34,16 @@ const useStyles = makeStyles<void, "adornmentError">()((theme, _params, classes)
     minHeight: APP_BAR_HEIGHT,
     minWidth: 0,
 
-    "&:hover": {
-      backgroundColor: tinycolor(APP_BAR_FOREGROUND_COLOR).setAlpha(0.08).toRgbString(),
-    },
-    "&.Mui-selected": {
-      backgroundColor: APP_BAR_PRIMARY_COLOR,
-
-      [`.${classes.adornmentError}`]: {
-        color: tinycolor(APP_BAR_PRIMARY_COLOR).lighten(15).toString(),
-      },
-    },
     "&.Mui-disabled": {
       color: "currentColor",
       opacity: theme.palette.action.disabledOpacity,
+    },
+    ":not(:hover)": {
+      opacity: 0.6,
+
+      [`.${classes.openIcon}`]: {
+        visibility: "hidden",
+      },
     },
   },
   adornment: {
@@ -78,6 +65,16 @@ const useStyles = makeStyles<void, "adornmentError">()((theme, _params, classes)
     left: 0,
     bottom: 0,
     margin: "auto",
+  },
+  divider: {
+    opacity: 0.6,
+  },
+  playerName: {
+    maxWidth: "20vw",
+    overflow: "hidden",
+  },
+  openIcon: {
+    opacity: 0.6,
   },
   iconButton: {
     padding: 0,
@@ -114,16 +111,6 @@ export function DataSource({
 }): JSX.Element {
   const { classes, cx } = useStyles();
 
-  const [anchorEl, setAnchorEl] = useState<undefined | HTMLElement>(undefined);
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(undefined);
-  };
-
   const playerName = useMessagePipeline(selectPlayerName);
   const playerPresence = useMessagePipeline(selectPlayerPresence);
   const playerProblems = useMessagePipeline(selectPlayerProblems) ?? [];
@@ -155,39 +142,47 @@ export function DataSource({
     <>
       {problemModal}
       <Stack direction="row" alignItems="center">
-        <ButtonBase className={cx(classes.button, { "Mui-selected": open })} onClick={handleClick}>
-          <div className={cx(classes.adornment, { [classes.adornmentError]: error })}>
-            {loading && (
-              <CircularProgress
-                size={LEFT_ICON_SIZE}
-                color="inherit"
-                className={classes.spinner}
-                variant="indeterminate"
-              />
-            )}
-            {error && (
-              <Tooltip
-                arrow={false}
-                disableHoverListener={initializing}
-                disableFocusListener={initializing}
-                classes={{ tooltip: classes.tooltip }}
-                placement="bottom"
-                title={<ProblemsList problems={playerProblems} setProblemModal={setProblemModal} />}
-              >
-                <IconButton
+        <ButtonBase className={classes.button} onClick={onSelectDataSourceAction}>
+          {loading || error ? (
+            <div className={cx(classes.adornment, { [classes.adornmentError]: error })}>
+              {loading && (
+                <CircularProgress
+                  size={LEFT_ICON_SIZE}
                   color="inherit"
-                  className={cx(classes.iconButton, classes.errorIconButton)}
+                  className={classes.spinner}
+                  variant="indeterminate"
+                />
+              )}
+              {error && (
+                <Tooltip
+                  arrow={false}
+                  disableHoverListener={initializing}
+                  disableFocusListener={initializing}
+                  classes={{ tooltip: classes.tooltip }}
+                  placement="bottom"
+                  title={
+                    <ProblemsList problems={playerProblems} setProblemModal={setProblemModal} />
+                  }
                 >
-                  <ErrorCircle20Filled />
-                </IconButton>
-              </Tooltip>
-            )}
+                  <IconButton
+                    color="inherit"
+                    className={cx(classes.iconButton, classes.errorIconButton)}
+                  >
+                    <ErrorCircle20Filled />
+                  </IconButton>
+                </Tooltip>
+              )}
+            </div>
+          ) : (
+            <Open16Filled className={classes.openIcon} />
+          )}
+          <div className={classes.playerName}>
+            <TextMiddleTruncate text={playerDisplayName ?? "<unknown>"} />
           </div>
-          <TextMiddleTruncate text={playerDisplayName ?? "<unknown>"} />
         </ButtonBase>
-        <Divider flexItem orientation="vertical" style={{ marginBlock: 12, opacity: 0.5 }} />
+        <span className={classes.divider}>/</span>
         <ButtonBase
-          className={cx(classes.button, { "Mui-selected": layoutMenuOpen })}
+          className={classes.button}
           ref={layoutButtonRef}
           id="layout-button"
           title="Layouts"
@@ -202,100 +197,6 @@ export function DataSource({
           <ChevronDown12Filled />
         </ButtonBase>
       </Stack>
-      {/* <Stack direction="row" alignItems="center" paddingRight={1.25}>
-        <div className={cx(classes.adornment, { [classes.adornmentError]: error })}>
-          {loading && (
-            <CircularProgress
-              size={LEFT_ICON_SIZE}
-              color="inherit"
-              className={classes.spinner}
-              variant="indeterminate"
-            />
-          )}
-          {error && (
-            <Tooltip
-              arrow={false}
-              disableHoverListener={initializing}
-              disableFocusListener={initializing}
-              classes={{ tooltip: classes.tooltip }}
-              placement="bottom"
-              title={<ProblemsList problems={playerProblems} setProblemModal={setProblemModal} />}
-            >
-              <IconButton
-                color="inherit"
-                className={cx(classes.iconButton, classes.errorIconButton)}
-              >
-                <ErrorCircle20Filled />
-              </IconButton>
-            </Tooltip>
-          )}
-          {!loading && !error && (
-            <Tooltip
-              arrow={false}
-              disableHoverListener={initializing}
-              disableFocusListener={initializing}
-              classes={{ tooltip: classes.tooltip }}
-              placement="bottom"
-              title={
-                <Stack gap={1} padding={1}>
-                  <DataSourceInfoView />
-                </Stack>
-              }
-            >
-              <IconButton
-                className={cx(classes.iconButton, classes.infoIconButton)}
-                color="inherit"
-              >
-                <Info16Regular />
-              </IconButton>
-            </Tooltip>
-          )}
-        </div>
-        <Stack direction="row" alignItems="center" gap={0.25} overflow="hidden">
-          <Link
-            className={classes.link}
-            color="inherit"
-            underline="none"
-            noWrap
-            variant="body2"
-            component="span"
-            onClick={onSelectDataSourceAction}
-          >
-            <TextMiddleTruncate
-              className={classes.zeroMinWidth}
-              text={playerDisplayName ?? "<unknown>"}
-            />
-          </Link>
-          <span className={classes.separator}>/</span>
-          <Link
-            className={classes.link}
-            color="inherit"
-            underline="none"
-            variant="body2"
-            component="span"
-            ref={layoutButtonRef}
-            id="layout-button"
-            title="Layouts"
-            aria-controls={layoutMenuOpen ? "layout-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={layoutMenuOpen ? "true" : undefined}
-            onClick={() => {
-              setLayoutMenuOpen(true);
-            }}
-          >
-            <TextMiddleTruncate className={classes.zeroMinWidth} text="Layout name" />
-            <IconButton
-              color="inherit"
-              className={cx(classes.iconButton, classes.layoutIconButton)}
-            >
-              <ChevronDown12Filled />
-            </IconButton>
-          </Link>
-        </Stack>
-      </Stack> */}
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        <MenuItem onClick={onSelectDataSourceAction}>Open Data Source</MenuItem>
-      </Menu>
       <LayoutMenu
         anchorEl={layoutAnchorEl ?? undefined}
         open={layoutMenuOpen}
