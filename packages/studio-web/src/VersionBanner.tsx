368393b7342208bb4b2c5106422a566acc11dec9
Roman Shtylman <roman@foxglove.dev>
2023-04-05 07:54:36 -0700

    Update VersionBanner.tsx
    Point the link to discussions rather than the old issue.

--- packages/studio-web/src/VersionBanner.tsx
+++ packages/studio-web/src/VersionBanner.tsx
@@ -98,10 +98,10 @@ const VersionBanner = function ({
 
             {!isChrome && (
               <Typography align="center" variant="subtitle1">
-                Check out our cross-browser support progress in GitHub issue{" "}
+                Check out our cross-browser support progress in GitHub discussion{" "}
                 <Link
                   color="inherit"
-                  href="https://github.com/foxglove/studio/issues/1511"
+                  href="https://github.com/orgs/foxglove/discussions/174"
                   target="_blank"
                 >
                   #1511
