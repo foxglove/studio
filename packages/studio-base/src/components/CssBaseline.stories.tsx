Lichtblick-Suite/lichtblick

commit a2d1c90cd2c79955640a48adbff3c310240aeb2f

Increase scrollbar width to 6px (#5680)
While working on https://github.com/foxglove/studio/pull/5678, I noticed
that in some instances our highlighted panel border will protrude into
the active panel.

Making it harder to select the scrollbar to scroll. Changing this border
behavior is challenging and has other gotchas with react mosaic. To ease
the pain of using a scrollbar in an active panel I am increasing the
width to 6px from 4px. I've also removed the border radius since our app
has a more boxy design.

---------

Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

packages/studio-base/src/components/CssBaseline.tsx
@@ -51,8 +51,8 @@ const useStyles = makeStyles()(({ palette, typography }) => ({
     },
     div: {
       "::-webkit-scrollbar": {
-        width: 4,
-        height: 4,
+        width: 6,
+        height: 0,
       },
       "::-webkit-scrollbar-corner": {
         background: "transparent",
@@ -62,7 +62,7 @@ const useStyles = makeStyles()(({ palette, typography }) => ({
       },
       "::-webkit-scrollbar-thumb": {
         background: palette.action.focus,
-        borderRadius: 2,
+        borderRadius: 0,
       },
     },
     "p:not([class^='Mui')": {

foxglove/studio

commit 6911837f1d9f059db9924cdd13feb51c8e20b5bc

Fix horizontal scrollbar visibility
Broken in: https://github.com/foxglove/studio/pull/5680

packages/studio-base/src/components/CssBaseline.stories.tsx
@@ -1,0 +1,27 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { Story } from "@storybook/react";
+
+import CssBaseline from "./CssBaseline";
+
+export default {
+  component: CssBaseline,
+  title: "components/CssBaseline",
+  parameters: {
+    colorScheme: "light",
+  },
+};
+
+export const Scrollbars: Story = () => {
+  return (
+    <CssBaseline>
+      <div
+        style={{ width: "200px", height: "200px", border: "1px solid black", overflow: "scroll" }}
+      >
+        <div style={{ width: "400px", height: "400px" }}>Should have both scrollbars</div>
+      </div>
+    </CssBaseline>
+  );
+};

packages/studio-base/src/components/CssBaseline.tsx
@@ -52,7 +52,7 @@ const useStyles = makeStyles()(({ palette, typography }) => ({
     div: {
       "::-webkit-scrollbar": {
         width: 6,
-        height: 0,
+        height: 6,
       },
       "::-webkit-scrollbar-corner": {
         background: "transparent",