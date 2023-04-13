af292a3b5b4760e3f5b79ede8560c5023a1ac531
Andrew Carr <andrew@foxglove.dev>
2023-04-13 15:53:17 +1000

    Rename preferences to settings without refactoring all component naming

--- packages/studio-base/src/i18n/en/preferences.ts
+++ packages/studio-base/src/i18n/en/preferences.ts
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 export const preferences = {
-  preferences: "Preferences",
+  preferences: "Settings",
   colorScheme: "Color scheme",
   dark: "Dark",
   light: "Light",
