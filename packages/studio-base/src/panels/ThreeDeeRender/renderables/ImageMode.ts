709c0af546857b10a8a45c26a54603778156323b
snosenzo <sam@foxglove.dev>
2023-04-18 14:22:44 -0400

    - keep matrix ignore

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode.ts
@@ -433,24 +433,14 @@ export class ImageMode extends SceneExtension implements ICameraHandler {
     const near = this.cameraState.near;
     const far = this.cameraState.far;
 
-    const matrix = new THREE.Matrix4().set(
-      ((2.0 * fx) / width) * zoomX,
-      0,
-      2.0 * (0.5 - cx / width) * zoomX,
-      0,
-      0,
-      ((2.0 * fy) / height) * zoomY,
-      2.0 * (cy / height - 0.5) * zoomY,
-      0,
-      0,
-      0,
-      -(far + near) / (far - near),
-      (-2.0 * far * near) / (far - near),
-      0,
-      0,
-      -1.0,
-      0,
-    );
+    // prettier-ignore
+    const matrix = new THREE.Matrix4()
+        .set(
+          2.0*fx/width * zoomX, 0, 2.0*(0.5 - cx/width) * zoomX, 0,
+          0, 2.0*fy/height * zoomY, 2.0*(cy/height-0.5) * zoomY, 0,
+          0, 0, -(far+near)/(far-near), -2.0*far*near/(far-near),
+          0, 0, -1.0, 0,
+        );
 
     return matrix;
   }
