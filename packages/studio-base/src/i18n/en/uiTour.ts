2242d8dc25f32d20d65096135044cdba1dbdd6e3
Andrew Carr <andrew@foxglove.dev>
2023-05-04 12:36:18 +1000

    More translation strings

--- packages/studio-base/src/i18n/en/uiTour.ts
+++ packages/studio-base/src/i18n/en/uiTour.ts
@@ -3,6 +3,18 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 export const uiTour = {
+  navigation: {
+    next: "Next",
+    back: "Back",
+    done: "Done",
+    dismiss: "Dismiss",
+  },
+  callToAction: {
+    title: "Try our new user experience",
+    content: "We’ve redesigned our navigation to make Studio faster and easier to use.",
+    readMore: "Read more",
+    tryIt: "Try it now",
+  },
   appBar: {
     title: "Top bar",
     content: "All of your high-level information and controls now live in the top bar.",
