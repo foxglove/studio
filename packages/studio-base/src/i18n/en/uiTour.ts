ee9e6e99b1ce20a59872b4f6751bd613a9f79ced
Andrew Carr <andrew@foxglove.dev>
2023-05-04 12:23:38 +1000

    Add translation strings for uiTour

---
+++ packages/studio-base/src/i18n/en/uiTour.ts
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+export const uiTour = {
+  appBar: {
+    title: "Top bar",
+    content: "All of your high-level information and controls now live in the top bar.",
+  },
+  appMenu: {
+    title: "App menu",
+    content: "Quickly open new and recent connections via the App menu",
+  },
+  addPanelMenu: {
+    title: "Add panel",
+    content: "Easily panels by dragging them into the layout.",
+  },
+  layoutMenu: {
+    title: "Layout menu",
+    content: "Browse your private and shared layouts.",
+  },
+};
