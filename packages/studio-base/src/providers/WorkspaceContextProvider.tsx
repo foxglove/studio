1945e00b5218a0155cd4254a24ddc9861214d2b5
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2023-05-08 12:55:07 +0700

    Organize workspace context items into slices

--- packages/studio-base/src/providers/WorkspaceContextProvider.tsx
+++ packages/studio-base/src/providers/WorkspaceContextProvider.tsx
@@ -7,10 +7,84 @@ import { ReactNode, useState } from "react";
 import { StoreApi, createStore } from "zustand";
 import { persist } from "zustand/middleware";
 
+import { AppSettingsTab } from "@foxglove/studio-base/components/AppSettingsDialog/AppSettingsDialog";
+import { DataSourceDialogItem } from "@foxglove/studio-base/components/DataSourceDialog";
+import { IDataSourceFactory } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import {
+  LeftSidebarItemKey,
+  RightSidebarItemKey,
+  SidebarItemKey,
   WorkspaceContext,
   WorkspaceContextStore,
-} from "@foxglove/studio-base/context/WorkspaceContext";
+} from "@foxglove/studio-base/context/Workspace/WorkspaceContext";
+
+// Type of version 0 store, used for migration.
+type WorkspaceContextStoreV0 = {
+  dataSourceDialog: {
+    activeDataSource: undefined | IDataSourceFactory;
+    item: undefined | DataSourceDialogItem;
+    open: boolean;
+  };
+  featureTours: {
+    active: undefined | string;
+    shown: string[];
+  };
+  leftSidebarOpen: boolean;
+  rightSidebarOpen: boolean;
+  leftSidebarItem: undefined | LeftSidebarItemKey;
+  leftSidebarSize: undefined | number;
+  rightSidebarItem: undefined | RightSidebarItemKey;
+  rightSidebarSize: undefined | number;
+  playbackControls: {
+    repeat: boolean;
+  };
+  prefsDialogState: {
+    initialTab: undefined | AppSettingsTab;
+    open: boolean;
+  };
+  sidebarItem: undefined | SidebarItemKey;
+};
+
+function migrateV0State(oldState: unknown, _version: number): WorkspaceContextStore {
+  // Currently v0 is the only obsolete state. If we do more migrations this
+  // needs to consider the version number.
+  const v0State = oldState as WorkspaceContextStoreV0;
+  return {
+    dialogs: {
+      dataSource: {
+        activeDataSource: v0State.dataSourceDialog.activeDataSource,
+        item: v0State.dataSourceDialog.item,
+        open: v0State.dataSourceDialog.open,
+      },
+      preferences: {
+        initialTab: undefined,
+        open: false,
+      },
+    },
+    featureTours: {
+      active: v0State.featureTours.active,
+      shown: v0State.featureTours.shown,
+    },
+    sidebars: {
+      legacy: {
+        item: "connection",
+      },
+      left: {
+        item: v0State.leftSidebarItem,
+        open: v0State.leftSidebarOpen,
+        size: v0State.leftSidebarSize,
+      },
+      right: {
+        item: v0State.rightSidebarItem,
+        open: v0State.rightSidebarOpen,
+        size: v0State.rightSidebarSize,
+      },
+    },
+    playbackControls: {
+      repeat: v0State.playbackControls.repeat,
+    },
+  };
+}
 
 function createWorkspaceContextStore(
   initialState?: Partial<WorkspaceContextStore>,
@@ -19,49 +93,52 @@ function createWorkspaceContextStore(
     persist(
       () => {
         const store: WorkspaceContextStore = {
-          dataSourceDialog: {
-            activeDataSource: undefined,
-            item: undefined,
-            open: false,
+          dialogs: {
+            dataSource: {
+              activeDataSource: undefined,
+              item: undefined,
+              open: false,
+            },
+            preferences: {
+              initialTab: undefined,
+              open: false,
+            },
           },
           featureTours: {
             active: undefined,
             shown: [],
           },
-          leftSidebarItem: "panel-settings",
-          leftSidebarOpen: true,
-          leftSidebarSize: undefined,
+          sidebars: {
+            legacy: {
+              item: "connection",
+            },
+            left: {
+              item: "panel-settings",
+              open: true,
+              size: undefined,
+            },
+            right: {
+              item: undefined,
+              open: false,
+              size: undefined,
+            },
+          },
           playbackControls: {
             repeat: false,
           },
-          prefsDialogState: {
-            initialTab: undefined,
-            open: false,
-          },
-          rightSidebarItem: undefined,
-          rightSidebarOpen: false,
-          rightSidebarSize: undefined,
-          sidebarItem: "connection",
+
           ...initialState,
         };
         return store;
       },
       {
         name: "fox.workspace",
+        version: 1,
+        migrate: migrateV0State,
         partialize: (value) => {
           // Note that this is a list of keys from the store that we include and restore
           // when persisting to and from localStorage.
-          return pick(value, [
-            "featureTours",
-            "leftSidebarItem",
-            "leftSidebarOpen",
-            "leftSidebarSize",
-            "playbackControls",
-            "rightSidebarItem",
-            "rightSidebarOpen",
-            "rightSidebarSize",
-            "sidebarItem",
-          ]);
+          return pick(value, ["featureTours", "playbackControls", "sidebars"]);
         },
       },
     ),
