c963ac41b876de685a17e3d9785fb720f3330b4a
Sam Nosenzo <samnosenzo@gmail.com>
2023-05-15 12:04:26 -0400

    - renderable lines should now dispose properly and not make new buffer geometries every render

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/primitives/RenderableLines.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/primitives/RenderableLines.ts
@@ -7,6 +7,7 @@ import { Line2 } from "three/examples/jsm/lines/Line2";
 import { LineGeometry } from "three/examples/jsm/lines/LineGeometry";
 import { LineSegments2 } from "three/examples/jsm/lines/LineSegments2";
 import { LineSegmentsGeometry } from "three/examples/jsm/lines/LineSegmentsGeometry";
+import { assert } from "ts-essentials";
 
 import { toNanoSec } from "@foxglove/rostime";
 import { LinePrimitive, LineType, SceneEntity } from "@foxglove/schemas";
@@ -20,145 +21,53 @@ import { LayerSettingsEntity } from "../../settings";
 const tempRgba = makeRgba();
 
 export class RenderableLines extends RenderablePrimitive {
-  #lines: LineSegments2[] = [];
+  #lines: LineGroup[] = [];
   public constructor(renderer: IRenderer) {
     super("", renderer);
   }
 
   #updateLines(lines: LinePrimitive[]) {
     this.clear();
-    this.#lines.length = 0;
 
-    for (const primitive of lines) {
+    let idx = 0;
+    for (idx; idx < lines.length; idx++) {
+      const primitive = lines[idx]!;
       if (primitive.points.length === 0) {
         continue;
       }
-      const line = this.#makeLine(primitive);
-      const group = new THREE.Group().add(line);
-      group.position.set(
+      let lineGroup = this.#lines[idx];
+      if (lineGroup == undefined) {
+        lineGroup = new LineGroup(primitive, this.renderer.input.canvasSize);
+        this.#lines.push(lineGroup);
+      }
+      lineGroup.setPrimitive(primitive);
+      lineGroup.setSettings(this.userData.settings);
+      lineGroup.setCanvasSize(this.renderer.input.canvasSize);
+
+      lineGroup.update();
+
+      lineGroup.position.set(
         primitive.pose.position.x,
         primitive.pose.position.y,
         primitive.pose.position.z,
       );
-      group.quaternion.set(
+      lineGroup.quaternion.set(
         primitive.pose.orientation.x,
         primitive.pose.orientation.y,
         primitive.pose.orientation.z,
         primitive.pose.orientation.w,
       );
-      this.add(group);
-      this.#lines.push(line);
+      this.add(lineGroup);
     }
-  }
-
-  #makeLine(primitive: LinePrimitive) {
-    let geometry: LineSegmentsGeometry;
-    const isSegments = primitive.type === LineType.LINE_LIST;
-    const isLoop = primitive.type === LineType.LINE_LOOP;
-
-    const transparent = true;
-    const material = new LineMaterial({
-      worldUnits: !primitive.scale_invariant,
-      linewidth: primitive.thickness,
-      transparent,
-      depthWrite: !transparent,
-      resolution: this.renderer.input.canvasSize,
-    });
-    material.lineWidth = primitive.thickness; // Fix for THREE.js type annotations
-
-    const pickingMaterial = new THREE.ShaderMaterial({
-      vertexShader: material.vertexShader,
-      fragmentShader: /* glsl */ `
-      uniform vec4 objectId;
-      void main() {
-        gl_FragColor = objectId;
-      }
-    `,
-      clipping: true,
-      uniforms: {
-        objectId: { value: [NaN, NaN, NaN, NaN] },
-        linewidth: { value: primitive.thickness },
-        resolution: { value: this.renderer.input.canvasSize },
-        dashOffset: { value: 0 },
-        dashScale: { value: 1 },
-        dashSize: { value: 1 },
-        gapSize: { value: 1 },
-      },
-      defines: !primitive.scale_invariant ? { WORLD_UNITS: "" } : {},
-    });
-
-    let line: LineSegments2;
-
-    switch (primitive.type) {
-      case LineType.LINE_STRIP:
-      case LineType.LINE_LOOP: {
-        const lineGeometry = new LineGeometry(); // separate variable to work around typescript refinement
-        geometry = lineGeometry;
-        line = new Line2(lineGeometry, material);
-        break;
-      }
-      case LineType.LINE_LIST: {
-        geometry = new LineSegmentsGeometry();
-        line = new LineSegments2(geometry, material);
-        break;
-      }
-    }
-
-    const positions = getPositions(primitive);
-    // setPosition requires the position array to be >= 6 length or else it will error
-    // we skip primitives with empty points before calling this function
-    geometry.setPositions(positions);
-
-    const singleColor = this.userData.settings.color
-      ? stringToRgba(tempRgba, this.userData.settings.color)
-      : primitive.colors.length === 0
-      ? primitive.color
-      : undefined;
-    if (singleColor == undefined) {
-      material.vertexColors = true;
-      material.opacity = 1;
-      material.uniforms.opacity!.value = 1;
-      const colors = getColors(primitive);
-      const instanceColorBuffer = new THREE.InstancedInterleavedBuffer(
-        colors,
-        isSegments ? 8 : 4,
-        1,
-      );
-      geometry.setAttribute(
-        "instanceColorStart",
-        new THREE.InterleavedBufferAttribute(instanceColorBuffer, 4, 0),
-      );
-      geometry.setAttribute(
-        "instanceColorEnd",
-        new THREE.InterleavedBufferAttribute(instanceColorBuffer, 4, 4),
-      );
-    } else {
-      material.vertexColors = false;
-      material.color = rgbToThreeColor(new THREE.Color(), singleColor);
-      // material.opacity = singleColor.a; // does not work for some reason
-      material.uniforms.opacity!.value = singleColor.a;
-    }
-
-    // Set an explicit instance count, because three.js ignores attribute offsets when
-    // automatically computing the instance count (and results differ across browsers because they
-    // depend on the key iteration order, since three.js derives the count from the first
-    // instanced interleaved attribute it sees).
-    geometry.instanceCount = isSegments
-      ? primitive.points.length >>> 1
-      : isLoop
-      ? primitive.points.length
-      : Math.max(primitive.points.length - 1, 0);
-
-    line.userData.pickingMaterial = pickingMaterial;
-    return line;
   }
 
   public override dispose(): void {
     for (const line of this.#lines) {
-      line.geometry.dispose();
-      line.material.dispose();
-      line.userData.pickingMaterial.dispose();
+      line.dispose();
+      line.removeFromParent();
     }
+    this.clear();
+    this.#lines.length = 0;
   }
 
   public override update(
@@ -180,85 +89,289 @@ export class RenderableLines extends RenderablePrimitive {
   }
 }
 
+class LineGroup extends THREE.Group {
+  #geometry: LineSegmentsGeometry | LineGeometry | undefined;
+  #positionBuffer: Float32Array | undefined;
+  #colorBuffer: Float32Array | undefined;
+
+  #material: LineMaterial;
+  #pickingMaterial: THREE.ShaderMaterial;
+  #transparent: boolean = true;
+  #line: LineSegments2 | Line2 | undefined;
+
+  #canvasSizeChanged: boolean = true;
+  #canvasSize: THREE.Vector2;
+
+  #primitiveChanged: boolean = true;
+  #primitive?: LinePrimitive;
+
+  // Settings
+  #colorChanged: boolean = true;
+  #color: string | undefined;
+
+  public constructor(primitive: LinePrimitive, canvasSize: THREE.Vector2) {
+    super();
+
+    this.#canvasSize = canvasSize;
+    this.#material = new LineMaterial({
+      worldUnits: !primitive.scale_invariant,
+      linewidth: primitive.thickness,
+      transparent: this.#transparent,
+      depthWrite: !this.#transparent,
+      resolution: canvasSize,
+    });
+    this.#material.lineWidth = primitive.thickness; // Fix for THREE.js type annotations
+
+    this.#pickingMaterial = new THREE.ShaderMaterial({
+      vertexShader: this.#material.vertexShader,
+      fragmentShader: /* glsl */ `
+      uniform vec4 objectId;
+      void main() {
+        gl_FragColor = objectId;
+      }
+    `,
+      clipping: true,
+      uniforms: {
+        objectId: { value: [NaN, NaN, NaN, NaN] },
+        linewidth: { value: primitive.thickness },
+        resolution: { value: canvasSize },
+        dashOffset: { value: 0 },
+        dashScale: { value: 1 },
+        dashSize: { value: 1 },
+        gapSize: { value: 1 },
+      },
+      defines: !primitive.scale_invariant ? { WORLD_UNITS: "" } : {},
+    });
+  }
+
+  public setSettings(settings: LayerSettingsEntity): void {
+    this.#colorChanged = this.#color !== settings.color;
+    this.#color = settings.color;
+  }
+
+  public setPrimitive(primitive: LinePrimitive): void {
+    this.#primitiveChanged = this.#primitive !== primitive;
+    this.#primitive = primitive;
+  }
+
+  public setCanvasSize(canvasSize: THREE.Vector2): void {
+    this.#canvasSizeChanged =
+      this.#canvasSize.x !== canvasSize.x || this.#canvasSize.y !== canvasSize.y;
+    this.#canvasSize = canvasSize;
+  }
+
+  public update(): void {
+    if (this.#primitive == undefined) {
+      this.visible = false;
+      return;
+    }
+    this.visible = true;
+    const isLoop = this.#primitive.type === LineType.LINE_LOOP;
+    const isSegments = this.#primitive.type === LineType.LINE_LIST;
+    const pointsLength = isLoop ? this.#primitive.points.length + 1 : this.#primitive.points.length;
+    if (this.#primitiveChanged) {
+      const geometryNeedsRecreated =
+        this.#geometry == undefined ||
+        !this.#positionBuffer ||
+        this.#positionBuffer.length < pointsLength;
+      if (geometryNeedsRecreated) {
+        if (this.#geometry != undefined) {
+          this.#geometry.dispose();
+        }
+        if (this.#line != undefined) {
+          this.#line.removeFromParent();
+        }
+
+        switch (this.#primitive.type) {
+          case LineType.LINE_STRIP:
+          case LineType.LINE_LOOP: {
+            const lineGeometry = new LineGeometry(); // separate variable to work around typescript refinement
+            this.#geometry = lineGeometry;
+            this.#positionBuffer = new Float32Array(pointsLength * 3);
+            this.#line = new Line2(lineGeometry, this.#material);
+            break;
+          }
+          case LineType.LINE_LIST: {
+            this.#geometry = new LineSegmentsGeometry();
+            this.#positionBuffer = new Float32Array(pointsLength * 3);
+            this.#colorBuffer = new Float32Array(pointsLength * 2 * 4);
+            this.#line = new LineSegments2(this.#geometry, this.#material);
+            break;
+          }
+        }
+        this.#line.userData.pickingMaterial = this.#pickingMaterial;
+        this.userData.pickingMaterial = this.#pickingMaterial;
+        this.add(this.#line);
+      }
+
+      assert(this.#positionBuffer, "Position buffer must be initialized");
+      assert(this.#geometry, "Geometry must be initialized");
+
+      this.#geometry.instanceCount = getLineInstanceCount({
+        pointsLength,
+        isSegments,
+        isLoop,
+      });
+
+      getPositions(this.#positionBuffer, this.#primitive);
+      this.#geometry.setPositions(this.#positionBuffer);
+    }
+
+    if (this.#colorChanged || this.#primitiveChanged) {
+      const singleColor = this.#color
+        ? stringToRgba(tempRgba, this.#color)
+        : this.#primitive.colors.length === 0
+        ? this.#primitive.color
+        : undefined;
+
+      if (singleColor == undefined) {
+        assert(this.#geometry, "Line Group geometry must exist");
+        assert(this.#colorBuffer);
+        this.#material.vertexColors = true;
+        this.#material.opacity = 1;
+        this.#material.uniforms.opacity!.value = 1;
+        const colors = getColors(this.#colorBuffer, this.#primitive);
+        const instanceColorBuffer = new THREE.InstancedInterleavedBuffer(
+          colors,
+          isSegments ? 8 : 4,
+          1,
+        );
+        this.#geometry.setAttribute(
+          "instanceColorStart",
+          new THREE.InterleavedBufferAttribute(instanceColorBuffer, 4, 0),
+        );
+        this.#geometry.setAttribute(
+          "instanceColorEnd",
+          new THREE.InterleavedBufferAttribute(instanceColorBuffer, 4, 4),
+        );
+      } else {
+        this.#material.vertexColors = false;
+        const color = this.#material.color as THREE.Color;
+        rgbToThreeColor(color, singleColor);
+        // material.opacity = singleColor.a; // does not work for some reason
+        this.#material.uniforms.opacity!.value = singleColor.a;
+      }
+    }
+
+    if (this.#canvasSizeChanged || this.#colorChanged || this.#primitiveChanged) {
+      this.#updateMaterial();
+    }
+    this.#primitiveChanged = false;
+    this.#canvasSizeChanged = false;
+    this.#colorChanged = false;
+  }
+
+  #updateMaterial() {
+    if (this.#primitive == undefined) {
+      return;
+    }
+    this.#material.linewidth = this.#primitive.thickness;
+    this.#material.resolution.copy(this.#canvasSize);
+    this.#material.transparent = this.#transparent;
+
+    this.#material.needsUpdate = true;
+
+    this.#pickingMaterial.uniforms.linewidth!.value = this.#primitive.thickness;
+    this.#pickingMaterial.uniforms.resolution!.value = this.#canvasSize;
+    const scaleInvariantDisabled = this.#pickingMaterial.defines.WORLD_UNITS === "";
+    if (scaleInvariantDisabled && this.#primitive.scale_invariant) {
+      this.#pickingMaterial.defines.WORLD_UNITS = "";
+    } else {
+      this.#pickingMaterial.defines = {};
+    }
+    this.#pickingMaterial.uniformsNeedUpdate = true;
+    this.#pickingMaterial.needsUpdate = true;
+  }
+
+  public dispose(): void {
+    this.remove(this.#line!);
+    this.#geometry?.dispose();
+    this.#material.dispose();
+    this.#pickingMaterial.dispose();
+  }
+}
+
 /**
  * Converts (x,y,z) values specified in `primitive.points` (and possibly `primitive.indices`) into
  * vertices for LineGeometry or LineSegmentsGeometry.
  */
-function getPositions(primitive: LinePrimitive): Float32Array {
+function getPositions(positionsOut: Float32Array, primitive: LinePrimitive): Float32Array {
   const isLoop = primitive.type === LineType.LINE_LOOP;
-  let positions: Float32Array;
   const indices = primitive.indices;
   if (indices.length > 0) {
-    positions = new Float32Array((indices.length + (isLoop ? 1 : 0)) * 3);
-
     let i = 0;
     for (const idx of indices) {
       const { x, y, z } = primitive.points[idx]!;
-      positions[i++] = x;
-      positions[i++] = y;
-      positions[i++] = z;
+      positionsOut[i++] = x;
+      positionsOut[i++] = y;
+      positionsOut[i++] = z;
     }
   } else {
-    positions = new Float32Array((primitive.points.length + (isLoop ? 1 : 0)) * 3);
-
     let i = 0;
     for (const { x, y, z } of primitive.points) {
-      positions[i++] = x;
-      positions[i++] = y;
-      positions[i++] = z;
+      positionsOut[i++] = x;
+      positionsOut[i++] = y;
+      positionsOut[i++] = z;
     }
   }
-  if (isLoop && positions.length > 3) {
-    positions[positions.length - 3] = positions[0]!;
-    positions[positions.length - 2] = positions[1]!;
-    positions[positions.length - 1] = positions[2]!;
+  if (isLoop && positionsOut.length > 3) {
+    positionsOut[positionsOut.length - 3] = positionsOut[0]!;
+    positionsOut[positionsOut.length - 2] = positionsOut[1]!;
+    positionsOut[positionsOut.length - 1] = positionsOut[2]!;
   }
-  return positions;
+  return positionsOut;
 }
 
 /**
  * Converts RGBA colors specified in `primitive.colors` (and possibly `primitive.indices`) into
  * vertices for LineGeometry or LineSegmentsGeometry.
  */
-function getColors(primitive: LinePrimitive): Float32Array {
+function getColors(colorsOut: Float32Array, primitive: LinePrimitive): Float32Array {
   const isLoop = primitive.type === LineType.LINE_LOOP;
-  let colors: Float32Array;
   const indices = primitive.indices;
   if (indices.length > 0) {
-    colors = new Float32Array((indices.length + (isLoop ? 1 : 0)) * 4);
-
     if (primitive.colors.length > 0) {
       let i = 0;
       for (const idx of indices) {
         const { r, g, b, a } = primitive.colors[idx]!;
-        colors[i++] = SRGBToLinear(r);
-        colors[i++] = SRGBToLinear(g);
-        colors[i++] = SRGBToLinear(b);
-        colors[i++] = a;
+        colorsOut[i++] = SRGBToLinear(r);
+        colorsOut[i++] = SRGBToLinear(g);
+        colorsOut[i++] = SRGBToLinear(b);
+        colorsOut[i++] = a;
       }
     } else {
       throw new Error("invariant: expected not to be using vertex colors");
     }
   } else {
-    colors = new Float32Array((primitive.points.length + (isLoop ? 1 : 0)) * 4);
-
     if (primitive.colors.length > 0) {
       let i = 0;
       for (const { r, g, b, a } of primitive.colors) {
-        colors[i++] = SRGBToLinear(r);
-        colors[i++] = SRGBToLinear(g);
-        colors[i++] = SRGBToLinear(b);
-        colors[i++] = a;
+        colorsOut[i++] = SRGBToLinear(r);
+        colorsOut[i++] = SRGBToLinear(g);
+        colorsOut[i++] = SRGBToLinear(b);
+        colorsOut[i++] = a;
       }
     } else {
       throw new Error("invariant: expected not to be using vertex colors");
     }
   }
-  if (isLoop && colors.length > 4) {
-    colors[colors.length - 4] = colors[0]!;
-    colors[colors.length - 3] = colors[1]!;
-    colors[colors.length - 2] = colors[2]!;
-    colors[colors.length - 1] = colors[3]!;
+  if (isLoop && colorsOut.length > 4) {
+    colorsOut[colorsOut.length - 4] = colorsOut[0]!;
+    colorsOut[colorsOut.length - 3] = colorsOut[1]!;
+    colorsOut[colorsOut.length - 2] = colorsOut[2]!;
+    colorsOut[colorsOut.length - 1] = colorsOut[3]!;
   }
-  return colors;
+  return colorsOut;
+}
+
+function getLineInstanceCount({
+  pointsLength,
+  isLoop,
+  isSegments,
+}: {
+  pointsLength: number;
+  isLoop: boolean;
+  isSegments: boolean;
+}) {
+  return isSegments ? pointsLength >>> 1 : isLoop ? pointsLength : Math.max(pointsLength - 1, 0);
 }
