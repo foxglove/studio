e4328ccb0173656334585dd88f5744a6897c8c03
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-05-22 15:21:07 -0500

    [Image Beta] Use toggle control for rotation setting

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode/ImageMode.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode/ImageMode.ts
@@ -417,7 +417,7 @@ export class ImageMode
     //   value: flipVertical,
     // };
     fields.rotation = {
-      input: "select",
+      input: "toggle",
       label: "Rotation",
       value: config.imageMode.rotation ?? 0,
       options: [
