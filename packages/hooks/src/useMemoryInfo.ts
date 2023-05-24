1ca45edf142b7aabec869f927288f568cd20bbad
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-05-24 14:18:00 -0700

    fixes?

--- packages/hooks/src/useMemoryInfo.ts
+++ packages/hooks/src/useMemoryInfo.ts
@@ -2,6 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// foxglove-depcheck-used: @types/foxglove__web
+
 import { useEffect, useState } from "react";
 
 import Logger from "@foxglove/log";
