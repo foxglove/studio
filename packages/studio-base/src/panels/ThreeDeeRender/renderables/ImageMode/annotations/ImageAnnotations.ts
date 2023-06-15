b7f2d4c7aab341208571c60ec91d8685cdedc4ea
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-06-15 13:35:53 -0700

    feedback

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode/annotations/ImageAnnotations.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/ImageMode/annotations/ImageAnnotations.ts
@@ -39,16 +39,20 @@ interface ImageAnnotationsContext {
   messageHandler: MessageHandler;
 }
 
-const ALL_SUPPORTED_SCHEMAS = new Set([
-  ...IMAGE_ANNOTATIONS_DATATYPES,
-  ...IMAGE_MARKER_DATATYPES,
-  ...IMAGE_MARKER_ARRAY_DATATYPES,
-  // For backwards compatibility with older studio versions and webviz
+/** For backwards compatibility with older studio versions and webviz */
+const LEGACY_ANNOTATION_SCHEMAS = new Set([
   "foxglove_msgs/ImageMarkerArray",
   "foxglove_msgs/msg/ImageMarkerArray",
   "studio_msgs/ImageMarkerArray",
   "studio_msgs/msg/ImageMarkerArray",
   "webviz_msgs/ImageMarkerArray",
+]);
+
+const ALL_SUPPORTED_SCHEMAS = new Set([
+  ...IMAGE_ANNOTATIONS_DATATYPES,
+  ...IMAGE_MARKER_DATATYPES,
+  ...IMAGE_MARKER_ARRAY_DATATYPES,
+  ...LEGACY_ANNOTATION_SCHEMAS,
 ]);
 
 /**
