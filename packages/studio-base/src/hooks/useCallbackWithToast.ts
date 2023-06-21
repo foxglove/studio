cb59f10316f54b37845da480be836dd24dcf816a
Roman Shtylman <roman@foxglove.dev>
2023-06-21 16:39:48 -0700

    Remove layout browser

--- packages/studio-base/src/hooks/useCallbackWithToast.ts
+++ packages/studio-base/src/hooks/useCallbackWithToast.ts
@@ -11,6 +11,8 @@ const log = Logger.getLogger(__filename);
 
 /**
  * A version of React.useCallback() displaying any errors thrown from the function as toast notifications.
+ *
+ * ts-prune-ignore-next
  */
 export default function useCallbackWithToast<Args extends unknown[]>(
   callback: (...args: Args) => Promise<void> | void,
