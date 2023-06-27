5fef785ec316a8c25d57f7343012c0e17cb53a1c
Bryan Fox <bryan@foxglove.dev>
2023-06-27 08:42:51 -0400

    Fix lint

--- packages/studio-base/src/panels/Map/getAccuracy.ts
+++ packages/studio-base/src/panels/Map/getAccuracy.ts
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { MathNumericType, atan2, eigs, isNumber, unit } from "mathjs";
+import { MathNumericType, atan2, eigs, isNumber } from "mathjs";
 
 import {
   NavSatFixMsg,
