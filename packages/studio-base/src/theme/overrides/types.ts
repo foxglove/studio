4434a0cfffc7b07997fae1a92dfde43fc5fced7e
Andrew Carr <andrew@foxglove.dev>
2023-08-10 20:10:09 +1000

    Fix duplicate import

--- packages/studio-base/src/theme/overrides/types.ts
+++ packages/studio-base/src/theme/overrides/types.ts
@@ -2,8 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Components } from "@mui/material";
-import { Theme } from "@mui/material";
+import { Components, Theme } from "@mui/material";
 
 export type OverrideComponentReturn<T extends keyof Components<Theme>> = Required<
   Pick<Components<Theme>, T>
