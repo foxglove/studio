547c8e69adcd68953f7d924927e8931f452e5f9d
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-08-16 17:09:59 -0700

    lint harder

--- packages/studio-base/src/players/IterablePlayer/Mcap/McapIterableSource.test.ts
+++ packages/studio-base/src/players/IterablePlayer/Mcap/McapIterableSource.test.ts
@@ -1,7 +1,13 @@
-import { TempBuffer } from "@foxglove/mcap-support";
-import { McapIterableSource } from "./McapIterableSource";
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 import { McapWriter } from "@mcap/core";
 import { Blob } from "node:buffer";
+
+import { TempBuffer } from "@foxglove/mcap-support";
+
+import { McapIterableSource } from "./McapIterableSource";
 
 describe("McapIterableSource", () => {
   it("returns an appropriate error message for an empty MCAP file", async () => {
--- packages/studio-desktop/src/quicklook/getMcapInfo.test.ts
+++ packages/studio-desktop/src/quicklook/getMcapInfo.test.ts
@@ -1,7 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 import { McapWriter } from "@mcap/core";
 import { Blob } from "node:buffer";
-import { getMcapInfo } from "./getMcapInfo";
+
 import { TempBuffer } from "@foxglove/mcap-support";
+
+import { getMcapInfo } from "./getMcapInfo";
 
 describe("getMcapInfo", () => {
   it("returns correct values for an empty MCAP file", async () => {
