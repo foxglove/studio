cdbaa1af4931e3e65512392b40c8bdc95f729d6e
Caleb Foust <cfoust@sqweebloid.com>
2023-08-25 12:52:08 -0700

    fix: downsample test

--- packages/studio-base/src/components/TimeBasedChart/downsample.test.ts
+++ packages/studio-base/src/components/TimeBasedChart/downsample.test.ts
@@ -15,15 +15,14 @@ describe("downsampleTimeseries", () => {
 
   it("merges nearby points", () => {
     const result = downsampleTimeseries(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: 0, y: 0, value: 0 },
         { x: 10, y: 0, value: 0 },
         { x: 20, y: 0, value: 0 },
         { x: 20, y: 1, value: 1 },
         { x: 20, y: 10, value: 10 },
         { x: 20, y: 20, value: 20 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([0, 1, 2, 5]);
@@ -31,15 +30,14 @@ describe("downsampleTimeseries", () => {
 
   it("preserves distinctly labeled segments", () => {
     const result = downsampleTimeseries(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: 0, y: 0, value: 0, label: "1" },
         { x: 10, y: 0, value: 0, label: "2" },
         { x: 20, y: 0, value: 0 },
         { x: 20, y: 1, value: 1 },
         { x: 20, y: 10, value: 10 },
         { x: 20, y: 20, value: 20 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([0, 1, 2, 5]);
@@ -47,13 +45,12 @@ describe("downsampleTimeseries", () => {
 
   it("should keep the min/max values within an interval", () => {
     const result = downsampleTimeseries(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: 0, y: 0, value: 0 },
         { x: 0, y: 100, value: 100 },
         { x: 0, y: -20, value: -20 },
         { x: 0, y: 4, value: 4 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([0, 2, 1, 3]);
@@ -61,14 +58,13 @@ describe("downsampleTimeseries", () => {
 
   it("should keep entry/exit datum to an interval", () => {
     const result = downsampleTimeseries(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: 0, y: 0, value: 0 },
         { x: 1, y: 0, value: 0 },
         { x: 1, y: 100, value: 100 },
         { x: 1, y: 4, value: 4 },
         { x: 2, y: 5, value: 5 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([0, 1, 2, 3, 4]);
@@ -84,13 +80,12 @@ describe("downsampleScatter", () => {
 
   it("ignores out of bounds points", () => {
     const result = downsampleScatter(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: -1, y: 0, value: 0 },
         { x: 0, y: -1, value: -1 },
         { x: 200, y: 0, value: 0 },
         { x: 0, y: 200, value: 200 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([1, 3]);
@@ -98,13 +93,12 @@ describe("downsampleScatter", () => {
 
   it("merges nearby points", () => {
     const result = downsampleScatter(
-      iterateObjects,
-      [
+      iterateObjects([
         { x: 0, y: 0, value: 0 },
         { x: 0, y: 0.4, value: 0.4 },
         { x: 0, y: 0.8, value: 0.8 },
         { x: 0, y: 1, value: 1 },
-      ],
+      ]),
       bounds,
     );
     expect(result).toEqual([0, 2]);
