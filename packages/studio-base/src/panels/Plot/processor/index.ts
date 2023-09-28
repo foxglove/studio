cf1c6625659a822129e486f2933a2c8557442f66
Caleb Foust <cfoust@sqweebloid.com>
2023-09-28 15:32:40 -0400

    feat: minimize processor exports

--- packages/studio-base/src/panels/Plot/processor/index.ts
+++ packages/studio-base/src/panels/Plot/processor/index.ts
@@ -2,7 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-export * from "./state";
+export type { State, StateAndEffects } from "./state";
+export { init, SideEffectType, findClient, setLive } from "./state";
 export {
   register,
   unregister,
