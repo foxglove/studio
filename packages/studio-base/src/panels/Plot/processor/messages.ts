19989ee68089fc2c6f658ebe7912687313242350
Caleb Foust <cfoust@sqweebloid.com>
2023-09-28 12:45:16 -0400

    fix: several bugs in messages.ts

--- packages/studio-base/src/panels/Plot/processor/messages.ts
+++ packages/studio-base/src/panels/Plot/processor/messages.ts
@@ -10,7 +10,7 @@ import { Topic, MessageEvent } from "@foxglove/studio-base/players/types";
 import { RosDatatypes } from "@foxglove/studio-base/types/RosDatatypes";
 import { enumValuesByDatatypeAndField } from "@foxglove/studio-base/util/enums";
 
-import { initAccumulated, accumulate, getNewMessages, buildPlot } from "./accumulate";
+import { initAccumulated, accumulate, buildPlot } from "./accumulate";
 import {
   State,
   StateAndEffects,
@@ -18,14 +18,12 @@ import {
   SideEffects,
   rebuildClient,
   sendData,
-  sendPartial,
   mapClients,
   noEffects,
   keepEffects,
 } from "./state";
 import { Messages } from "../internalTypes";
 import { isSingleMessage } from "../params";
-import { appendPlotData } from "../plotData";
 
 export function receiveMetadata(
   topics: readonly Topic[],
@@ -71,7 +69,7 @@ export function addBlock(block: Messages, resetTopics: string[], state: State): 
     )(blocks),
   };
 
-  return mapClients((client): [Client, SideEffects] => {
+  return mapClients((client, { blocks: newBlocks }): [Client, SideEffects] => {
     const { id, params } = client;
     const relevantTopics = R.intersection(topics, client.topics);
     const shouldReset = R.intersection(relevantTopics, resetTopics).length > 0;
@@ -87,7 +85,7 @@ export function addBlock(block: Messages, resetTopics: string[], state: State): 
           globalVariables,
           shouldReset ? initAccumulated(client.topics) : client.blocks,
           params,
-          blocks,
+          newBlocks,
         ),
       },
       [rebuildClient(id)],
@@ -95,98 +93,42 @@ export function addBlock(block: Messages, resetTopics: string[], state: State): 
   })(newState);
 }
 
-const updateLiveClients = mapClients((client, state): [Client, SideEffects] => {
-  const { current, metadata, globalVariables } = state;
-  const { id, params } = client;
-  if (params == undefined) {
-    return noEffects(client);
-  }
-
-  if (isSingleMessage(params)) {
-    const plotData = buildPlot(
-      metadata,
-      globalVariables,
-      params,
-      R.map((messages) => messages.slice(-1), current),
-    );
-
-    return [client, [sendData(id, plotData)]];
-  }
-
-  return [
-    {
-      ...client,
-      current: accumulate(metadata, globalVariables, client.current, params, current),
-    },
-    [rebuildClient(id)],
-  ];
-});
-
-const updateRecordedClients = mapClients((client, state): [Client, SideEffects] => {
-  const { current, metadata, globalVariables } = state;
-  const { id, params, current: previous } = client;
-  if (params == undefined) {
-    return noEffects(client);
-  }
-
-  const { cursors: oldCursors, data: oldData } = previous;
-  const [newCursors, newMessages] = getNewMessages(oldCursors, current);
-
-  if (isSingleMessage(params)) {
-    return [
-      client,
-      [
-        sendData(
-          id,
-          buildPlot(
-            metadata,
-            globalVariables,
-            params,
-            R.map((messages) => messages.slice(-1), current),
-          ),
-        ),
-      ],
-    ];
-  }
-
-  if (R.isEmpty(newMessages)) {
-    return noEffects(client);
-  }
-
-  const newData = buildPlot(metadata, globalVariables, params, newMessages);
-
-  return [
-    {
-      ...client,
-      current: {
-        cursors: newCursors,
-        data: appendPlotData(oldData, newData),
-      },
-    },
-    [sendPartial(id, newData)],
-  ];
-});
-
 export function addCurrent(events: readonly MessageEvent[], state: State): StateAndEffects {
-  const { current, isLive } = state;
-
-  const newCurrent = R.map((v) => v, current);
-
-  for (const message of events) {
-    const { topic } = message;
-    if (newCurrent[topic] == undefined) {
-      newCurrent[topic] = [];
-    }
-    newCurrent[topic]?.push(message);
-  }
-
+  const { current } = state;
   const newState = {
     ...state,
-    current: newCurrent,
+    current: R.pipe(
+      R.groupBy((v: MessageEvent) => v.topic),
+      R.mergeWith(R.concat, current),
+    )(events),
   };
 
   return R.pipe(
-    isLive ? updateLiveClients : updateRecordedClients,
+    mapClients((client, state): [Client, SideEffects] => {
+      const { current, metadata, globalVariables } = state;
+      const { id, params } = client;
+      if (params == undefined) {
+        return noEffects(client);
+      }
+
+      if (isSingleMessage(params)) {
+        const plotData = buildPlot(
+          metadata,
+          globalVariables,
+          params,
+          R.map((messages) => messages.slice(-1), current),
+        );
+        return [client, [sendData(id, plotData)]];
+      }
+
+      return [
+        {
+          ...client,
+          current: accumulate(metadata, globalVariables, client.current, params, current),
+        },
+        [rebuildClient(id)],
+      ];
+    }),
     keepEffects(evictCache),
   )(newState);
 }
