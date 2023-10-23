9136e36a6827cfe1a916d4dd644351f5a79e9b16
James Smith <james@foxglove.dev>
2023-10-24 06:20:35 +1100

    ensure overwritten fetch parameters are in sync with underlying

--- packages/studio-base/src/util/overwriteFetch.ts
+++ packages/studio-base/src/util/overwriteFetch.ts
@@ -11,15 +11,18 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
+type InputType = Parameters<typeof fetch>[0];
+type InitType = Parameters<typeof fetch>[1];
+
 // Overwrite the default fetch error handler with one that catches one message: "Failed to fetch". We see this often
 // in our logs and want the logs to more fully reflect the error message.
 export default function overwriteFetch(): void {
   const originalFetch = global.fetch;
-  global.fetch = async (url: RequestInfo | URL, init?: RequestInit) => {
+  global.fetch = async (input: InputType, init?: InitType) => {
     // Use this replacement error instead of the original one, because this one will have the correct stack trace.
     // eslint-disable-next-line @typescript-eslint/no-base-to-string
-    const replacementError = new TypeError(`Failed to fetch: ${url}`);
-    return await originalFetch(url, init).catch((error) => {
+    const replacementError = new TypeError(`Failed to fetch: ${input}`);
+    return await originalFetch(input, init).catch((error) => {
       if (error.message === "Failed to fetch") {
         throw replacementError;
       }
