b8e0aed9a857d724a8bc282868b2a9e58822b151
Andrew Carr <andrew@foxglove.dev>
2024-01-12 16:06:37 +1100

    Add global enterDelay to tooltips

--- packages/theme/src/components/MuiTooltip.ts
+++ packages/theme/src/components/MuiTooltip.ts
@@ -11,6 +11,7 @@ export const MuiTooltip: OverrideComponentReturn<"MuiTooltip"> = {
   defaultProps: {
     arrow: true,
     TransitionComponent: Fade,
+    enterDelay: 500,
   },
   styleOverrides: {
     arrow: ({ theme }) => ({
