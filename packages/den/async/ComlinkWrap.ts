b31c3fc10ae614e8e90c37236e3f9f50daa8af43
Sam Nosenzo <samnosenzo@gmail.com>
2024-01-23 17:07:58 +0100

    comlinkwrap comment

--- packages/den/async/ComlinkWrap.ts
+++ packages/den/async/ComlinkWrap.ts
@@ -4,6 +4,17 @@
 
 import * as Comlink from "comlink";
 
+/**
+ * Wraps an instantiated `Worker` and exposes its API in the same way that `Comlink.wrap` does
+ * but it also provides a `dispose` function to terminate the worker and release the comlink proxy.
+ * This can help prevent memory leaks when the comlink proxy is unable to garbage collect itself due to
+ * unresolved promises which can occur if the worker is terminated while processing a request.
+ * This should be used instead of `Comlink.wrap` where possible.
+ *
+ * @param worker - worker to be wrapped by comlink
+ * @returns remote - API for worker wrapped by comlink. What is normally received from Comlink.wrap
+ * @returns dispose - function to release the comlink proxy and to terminate the worker
+ */
 export function ComlinkWrap<T>(worker: Worker): { remote: Comlink.Remote<T>; dispose: () => void } {
   const remote = Comlink.wrap<T>(worker);
 
