87720ebf52ad89bad51a594f3959b23bf5e0c9a8
Roman Shtylman <roman@foxglove.dev>
2024-01-23 16:03:07 -0800

    deps

--- packages/den/testing/makeComlinkWorkerMock.ts
+++ packages/den/testing/makeComlinkWorkerMock.ts
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import * as Comlink from "comlink";
-import EventEmitter from "eventemitter3";
+import { EventEmitter } from "node:events";
 
 type ComlinkWorkerConstructor = new () => Comlink.Endpoint;
 
