597f994f9a2da2b83566b22d567719a25d8d8f23
Manning <manning@foxglove.dev>
2024-02-08 11:55:05 -0600

    (chore) update default playback speed

--- packages/studio-base/src/providers/WorkspaceContextProvider.tsx
+++ packages/studio-base/src/providers/WorkspaceContextProvider.tsx
@@ -47,7 +47,7 @@ export function makeWorkspaceContextInitialState(): WorkspaceContextStore {
     },
     playbackControls: {
       repeat: false,
-      speed: 0.2,
+      speed: 1,
     },
   };
 }
